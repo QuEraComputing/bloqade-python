fc57f5b53f7eb1435e915a7262280ab8dd0a2f72
Phillip Weinberg <weinbe58@gmail.com>
2023-04-19 13:05:56 -0400

    changing magic methods to static methods.

--- src/bloqade/ir/scalar.py
+++ src/bloqade/ir/scalar.py
@@ -19,11 +19,13 @@ def merge_dicts(lhs: Dict, rhs: Dict):
 
 
 class ScalarLang:
-    def __add__(self, other):
-        if not isinstance(other, ScalarLang):
+    
+    @staticmethod
+    def add(lhs, rhs):
+        if not isinstance(rhs, ScalarLang):
             raise ValueError("Cannot add non-ScalarLang objects.")
 
-        match (self, other):
+        match (lhs, rhs):
             case ((scalar, Scalar(Literal(0.0))) | (Scalar(Literal(0.0)), scalar)):
                 return scalar
 
@@ -34,7 +36,7 @@ class ScalarLang:
                 return Scalar(value=Literal(value=0.0))
 
             case (Negative(lhs), Negative(rhs)):
-                return -(lhs + rhs)
+                return ScalarLang.negative(ScalarLang.add(lhs, rhs))
 
             case (Reduce(head="+") as lhs, Reduce(head="+") as rhs):
                 new_args = merge_dicts(lhs.args, rhs.args)
@@ -88,12 +90,13 @@ class ScalarLang:
                 return Reduce(head="+", literal=0, args={lhs: 2})
 
             case _:
-                return Reduce(head="+", literal=0, args={self: 1, other: 1})
+                return Reduce(head="+", literal=0, args={lhs: 1, rhs: 1})
 
-    def __neg__(self):
-        match self:
+    @staticmethod(function)
+    def negative(scalar):
+        match scalar:
             case Scalar(Literal(0.0)):
-                return self
+                return scalar
 
             # pass negative through there variants makes for less canoicalization in addition
             case Scalar(Literal(value)):
@@ -108,28 +111,31 @@ class ScalarLang:
                 return value
 
             case _:
-                return Negative(self)
+                return Negative(scalar)
 
-    def __sub__(self, other):
-        return self + (-other)
+    @staticmethod
+    def subtract(lhs, rhs):
+        ScalarLang.add(lhs, ScalarLang.negative(rhs))
+
+
 
 
 @dataclass(frozen=True)
 class Scalar(ScalarLang):
     value: Real
 
-    def julia_adt(self):
+    def julia_adt(lhs):
         return NotImplemented
-        # return ir_types.Scalar(self.value.julia_adt())
+        # return ir_types.Scalar(lhs.value.julia_adt())
 
 
 @dataclass(frozen=True)
 class Negative(ScalarLang):
     value: ScalarLang
 
-    def julia_adt(self):
+    def julia_adt(lhs):
         return NotImplemented
-        # return ir_types.Negative(self.value.julia_adt())
+        # return ir_types.Negative(lhs.value.julia_adt())
 
 
 @dataclass(frozen=True)
@@ -137,9 +143,9 @@ class Default(ScalarLang):
     var: Variable
     value: float
 
-    def julia_adt(self):
+    def julia_adt(lhs):
         return NotImplemented
-        # return ir_types.Default(self.var.julia_adt(), self.value)
+        # return ir_types.Default(lhs.var.julia_adt(), lhs.value)
 
 
 @dataclass(frozen=True)
@@ -148,12 +154,12 @@ class Reduce(ScalarLang):
     literal: float
     args: Dict[ScalarLang, int]
 
-    def julia_adt(self):
+    def julia_adt(lhs):
         return NotImplemented
         # return ir_types.Reduce(
-        #     self.head,
-        #     self.literal,
-        #     jl.DictValue(k.julia_adt():v for k,v in self.args.items())
+        #     lhs.head,
+        #     lhs.literal,
+        #     jl.DictValue(k.julia_adt():v for k,v in lhs.args.items())
         # )
 
 
@@ -162,11 +168,11 @@ class Interval(ScalarLang):
     first: ScalarLang
     last: ScalarLang
 
-    def julia_adt(self):
+    def julia_adt(lhs):
         return NotImplemented
         # return ir_types.Interval(
-        #     self.first.julia_adt(),
-        #     self.last.julia_adt()
+        #     lhs.first.julia_adt(),
+        #     lhs.last.julia_adt()
         # )
 
 
@@ -175,9 +181,9 @@ class Slice(ScalarLang):
     duration: ScalarLang
     interval: Interval
 
-    def julia_adt(self):
+    def julia_adt(lhs):
         return NotImplemented
         # return ir_types.Slice(
-        #     self.duration.julia_adt(),
-        #     self.interval.julia_adt()
+        #     lhs.duration.julia_adt(),
+        #     lhs.interval.julia_adt()
         # )
