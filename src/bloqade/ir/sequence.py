8812b2612f1d2d3e94c25c82e700adbd717e806f
Xiu-zhe (Roger) Luo <rogerluo.rl18@gmail.com>
2023-04-28 11:04:58 -0400

    remove ToJulia trait from IR (#8)

--- src/bloqade/ir/sequence.py
+++ src/bloqade/ir/sequence.py
@@ -1,33 +1,27 @@
-from bloqade.julia.prelude import *
 from bloqade.ir.pulse import Pulse
 from bloqade.ir.scalar import Interval
 
 from pydantic.dataclasses import dataclass
 from enum import Enum
 from typing import List
-from ..julia.prelude import *
-from juliacall import AnyValue  # type: ignore
-
 
 @dataclass(frozen=True)
-class LevelCoupling(ToJulia):
+class LevelCoupling:
     pass
 
 
 @dataclass(frozen=True)
 class Rydberg(LevelCoupling):
-    def julia(self):
-        return IRTypes.Rydberg
+    pass
 
 
 @dataclass(frozen=True)
 class Hyperfine(LevelCoupling):
-    def julia(self):
-        return IRTypes.Hyperfine
+    pass
 
 
 @dataclass(frozen=True)
-class Sequence(ToJulia):
+class Sequence:
     pass
 
 
@@ -35,18 +29,10 @@ class Sequence(ToJulia):
 class Append(Sequence):
     value: List[Sequence]
 
-    def julia(self) -> AnyValue:
-        return IRTypes.SequenceLang.Append(Vector[IRTypes.SequenceLang](self.value))
-
 
 @dataclass(frozen=True)
 class Instruction(Sequence):
     value: dict[LevelCoupling, Pulse]
-
-    def julia(self) -> AnyValue:
-        return IRTypes.SequenceLang.Instruction(
-            Dict[IRTypes.LevelCoupling, IRTypes.PulseLang](self.value)
-        )
 
 
 @dataclass(frozen=True)
@@ -54,20 +40,8 @@ class NamedSequence(Sequence):
     sequence: Sequence
     name: str
 
-    def julia(self) -> AnyValue:
-        return IRTypes.SequenceLang.Name(
-            self.sequence.julia(),
-            IRTypes.String(self.name),
-        )
-
 
 @dataclass(frozen=True)
 class Slice(Sequence):
     sequence: Sequence
     interval: Interval
-
-    def julia(self) -> AnyValue:
-        return IRTypes.SequenceLang.Slice(
-            self.sequence.julia(),
-            self.interval.julia(),
-        )
