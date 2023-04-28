3c942a010c68ee731605ee6581b85e24fe33fcf9
Roger-luo <rogerluo.rl18@gmail.com>
2023-04-28 17:49:51 -0400

    polish demo

--- src/bloqade/task.py
+++ src/bloqade/task.py
@@ -1,6 +1,9 @@
 # we have to put these objects in one file because of circular imports
+# TODO: figure out how to remove the circular imports & split the file
+
 from .ir.prelude import *
 from pandas import DataFrame
+import numpy as np
 
 class Task:
 
@@ -24,6 +27,12 @@ class QuEraTask(Task):
         super().__init__(prog, nshots)
         # custom config goes here
 
+class SimuTask(Task):
+    
+    def __init__(self, prog: "Program", nshots: int) -> None:
+        super().__init__(prog, nshots)
+        # custom config goes here
+
 class TaskResult:
 
     def report(self) -> "TaskReport":
@@ -37,6 +46,7 @@ class TaskReport:
     def __init__(self, result: TaskResult) -> None:
         self.result = result
         self._dataframe = None # df cache
+        self._bitstring = None # bitstring cache
 
     @property
     def dataframe(self) -> DataFrame:
@@ -45,6 +55,16 @@ class TaskReport:
         self._dataframe = DataFrame()
         return self._dataframe
 
+    @property
+    def bitstring(self) -> np.array:
+        if self._bitstring:
+            return self._bitstring
+        self._bitstring = np.array([])
+        return self._bitstring
+
+    @property
+    def markdown(self) -> str:
+        return self.dataframe.to_markdown()
 
 class Program:
 
@@ -55,5 +75,8 @@ class Program:
     def braket(self, *args, **kwargs) -> BraketTask:
         return BraketTask(self, *args, **kwargs)
 
-    def quera(self, nshots: int) -> QuEraTask:
-        pass
+    def quera(self, *args, **kwargs) -> QuEraTask:
+        return QuEraTask(self, *args, **kwargs)
+
+    def simu(self, *args, **kwargs) -> SimuTask:
+        return SimuTask(self, *args, **kwargs)
