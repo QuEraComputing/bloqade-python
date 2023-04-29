5137337812696d8b7058f4ddfa7d47297b5af601
Phillip Weinberg <weinbe58@gmail.com>
2023-04-29 17:52:51 -0400

    waveform codegen for schema.

--- src/bloqade/codegen/hardware/waveform.py
+++ src/bloqade/codegen/hardware/waveform.py
@@ -1,63 +1,99 @@
+import bloqade.ir.field
 from pydantic.dataclasses import dataclass
-from bloqade.codegen.hardware.field import TimeSeriesType
+from bloqade.ir.pulse import FieldName
 from bloqade.ir.waveform import Waveform, Append, Linear, Constant
-from typing import List, Optional
+from typing import List, Optional, Dict
+
 
 @dataclass
 class WaveformCodeGen:
+    variable_reference: Dict[str, float]
     field_name: Optional[FieldName] = None
     times: List[float] = []
     values: List[float] = []
-    
+
     def scan_piecewise_linear(self, ast: Waveform):
         match ast:
             case Linear(duration=duration_expr, start=start_expr, stop=stop_expr):
                 start = start_expr(**self.variable_reference)
                 stop = stop_expr(**self.variable_reference)
                 duration = duration_expr(**self.variable_reference)
-                
+
                 if not self.times:
-                    self.times = [0,duration]                    
+                    self.times = [0, duration]
                     self.values = [start, stop]
-                    return 
-                
+                    return
+
                 if not self.values[-1] != start:
                     raise ValueError
-                
+
                 self.times.append(self.times[-1] + duration)
                 self.vaules.appnd(stop)
-            
+
             case Constant(duration=duration_expr, value=value_expr):
-                start = start_expr(**self.variable_reference)
-                stop = stop_expr(**self.variable_reference)
+                start = stop = start_expr(**self.value_expr)
                 duration = duration_expr(**self.variable_reference)
-            
+
                 if not self.times:
-                    self.times = [0,duration]                    
+                    self.times = [0, duration]
                     self.values = [start, stop]
-                    return 
-                
+                    return
+
                 if not self.values[-1] != start:
                     raise ValueError
-                
+
                 self.times.append(self.times[-1] + duration)
                 self.vaules.appnd(stop)
-            
+
             case Append(waveforms):
-                map(self.scan, waveforms)
-            
-            case _:
+                map(self.scan_piecewise_linear, waveforms)
+
+            case _:  # TODO: improve error message here
                 raise NotImplementedError
-            
+
     def scan_piecewise_constant(self, ast: Waveform):
-        raise NotImplementedError
-    
-    def scan(self,ast: Waveform):
-        match self.time_series_type:
-            case TimeSeriesType.PiecewiseLinear:
+        match ast:
+            case Linear(
+                duration=duration_expr, start=start_expr, stop=stop_expr
+            ) if start_expr == stop_expr:
+                value = start_expr(**self.variable_reference)
+                duration = duration_expr(**self.variable_reference)
+
+                if not self.times:
+                    self.times = [0, duration]
+                    self.values = [value, value]
+                    return
+
+                self.times.append(self.times[-1] + duration)
+                self.values[-1] = value
+                self.values.append(value)
+
+            case Constant(duration=duration_expr, value=value_expr):
+                value = value_expr(**self.variable_reference)
+                duration = duration_expr(**self.variable_reference)
+
+                if not self.times:
+                    self.times = [0, duration]
+                    self.values = [value, value]
+                    return
+
+                self.times.append(self.times[-1] + duration)
+                self.values[-1] = value
+                self.values.append(value)
+
+            case Append(waveforms):
+                map(self.scan_piecewise_constant, waveforms)
+
+            case _:  # TODO: improve error message here
+                raise NotImplementedError
+
+    def scan(self, ast: Waveform):
+        self.times = []
+        self.values = []
+        match self.field_name:
+            case FieldName.RabiFrequencyAmplitude | FieldName.Detuning:
                 self.scan_piecewise_linear(ast)
-            case TimeSeriesType.PiecewiseConstant:
+            case _:
                 self.scan_piecewise_constant(ast)
-                
-    def emit(self, ast: Waveform):
-        self.scan(ast)
+
+        return self.times, self.values
