ec53412ffe7cd996dbaec8f9f200d9a68bfc73db
Phillip Weinberg <weinbe58@gmail.com>
2023-04-29 21:00:46 -0400

    sequence codegen

--- src/bloqade/codegen/hardware/sequence.py
+++ src/bloqade/codegen/hardware/sequence.py
@@ -1,21 +1,30 @@
 from pydantic.dataclasses import dataclass
-from bloqade.ir.sequence import Sequence, 
-from typing import Dict
+from bloqade.ir.sequence import SequenceExpr, Sequence, NamedSequence, LevelCoupling
 from bloqade.codegen.hardware.pulse import PulseCodeGen
-from quera_ahs_utils.quera_ir.task_specification import RydbergHamiltonian
+from  quera_ahs_utils.quera_ir.task_specification import EffectiveHamiltonian, RydbergHamiltonian
+from typing import Optional
 
 @dataclass
 class SequenceCodeGen(PulseCodeGen):
+    rydberg: Optional[RydbergHamiltonian] = None
     
-    # override dataclass __init__ to give required arguments for codegen
-    def __init__(self, variable_reference: Dict[str, float], n_atoms: int):
-        super().__init__(variable_reference=variable_reference, n_atoms=n_atoms)
-    
+    def scan(self, ast: SequenceExpr):
+        match ast:
+            case Sequence(value):
+                
+                self.level_coupling = LevelCoupling.Rydberg
+                if self.level_coupling in value:
+                    self.rydberg = PulseCodeGen.emit(self, value[self.level_coupling])
+                    
+            case NamedSequence(sub_sequence,_):
+                self.scan(sub_sequence)
+                
+            case _:
+                # TODO: Inprove error message. 
+                raise ValueError()
 
-    def scan_hardware(self, ast: Sequence):
-        raise NotImplementedError
-    
-    def emit_hardware(self, ast: Sequence):
-        raise NotImplementedError
-    
-    
+    def emit(self, ast: SequenceExpr):
+        self.scan(ast)
+        return EffectiveHamiltonian(rydberg=self.rydberg)
+
+
