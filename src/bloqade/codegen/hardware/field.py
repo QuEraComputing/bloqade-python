f7a4094d7c65270bb6a360d4c5ff1688129d178e
Phillip Weinberg <weinbe58@gmail.com>
2023-04-29 21:06:35 -0400

    field codegen.

--- src/bloqade/codegen/hardware/field.py
+++ src/bloqade/codegen/hardware/field.py
@@ -1,23 +1,85 @@
 from pydantic.dataclasses import dataclass
-from bloqade.ir.field import Field
-
+from bloqade.ir.field import Field, GlobalModulation, Global
+from bloqade.ir.pulse import FieldName
 from typing import List, Optional
 from bloqade.codegen.hardware.spatial_modulation import SpatialModulationCodeGen
+from bloqade.codegen.hardware.waveform import WaveformCodeGen
 
+from quera_ahs_utils.quera_ir.task_specification import (
+    GlobalField,
+    LocalField,
+    RabiFrequencyAmplitude,
+    RabiFrequencyPhase,
+    Detuning,
+)
+from exmaples.mwe_adiabatic import lattice
 
-class TimeSeriesType(str, Enum):
-    PiecewiseLinear = "piecewise_linear"
-    PiecewiseConstant = "piecewise_constant"
 
 @dataclass
-class FieldCodeGen(SpatialModulationCodeGen):
-    
+class FieldCodeGen(WaveformCodeGen, SpatialModulationCodeGen):
+    global_: Optional[GlobalField] = None
+    local: Optional[LocalField] = None
+
     def scan(self, ast: Field):
-        raise NotImplementedError
-            
+        terms = dict(ast.value)
+        match self.field_name:
+            case FieldName.RabiFrequencyAmplitude if len(
+                terms
+            ) == 1 and GlobalModulation() in terms:
+                times, values = WaveformCodeGen.emit(self, terms.pop(Global))
+                self.global_ = GlobalField(times=times, values=values)
+
+            case FieldName.RabiFrequencyPhase if len(
+                terms
+            ) == 1 and GlobalModulation() in terms:
+                times, values = WaveformCodeGen.emit(self, terms.pop(Global))
+                self.global_ = GlobalField(times=times, values=values)
+
+            case FieldName.Detuning if len(terms) == 1 and GlobalModulation() in terms:
+                times, values = WaveformCodeGen.emit(self, terms.pop(Global))
+                self.global_ = GlobalField(times=times, values=values)
+
+            case FieldName.Detuning if len(terms) == 1:
+                ((spatial_modulation, waveform),) = terms.items()
+                times, values = WaveformCodeGen.emit(self, waveform)
+                lattice_site_coefficients = SpatialModulationCodeGen.emit(
+                    self, spatial_modulation
+                )
+
+                self.local = LocalField(
+                    times=times,
+                    values=values,
+                    lattice_site_coefficients=lattice_site_coefficients,
+                )
+
+            case FieldName.Detuning if len(terms) == 2 and GlobalModulation() in terms:
+                times, values = WaveformCodeGen.emit(self, terms.pop(Global))
+                self.global_ = GlobalField(times=times, values=values)
+                ((spatial_modulation, waveform),) = terms.items()
+                times, values = WaveformCodeGen.emit(self, waveform)
+                lattice_site_coefficients = SpatialModulationCodeGen.emit(
+                    self, spatial_modulation
+                )
+
+                self.global_ = GlobalField(times=times, values=values)
+                self.local = LocalField(
+                    times=times,
+                    values=values,
+                    lattice_site_coefficients=lattice_site_coefficients,
+                )
+
+            case _:  # TODO: improve error message here
+                raise NotImplementedError
+
     def emit(self, ast: Field):
-        raise NotImplementedError
+        self.global_ = None
+        self.local = None
+        self.scan(ast)
 
-    
-    
-
+        match self.field_name:
+            case FieldName.RabiFrequencyAmplitude:
+                return RabiFrequencyAmplitude(global_=self.global_)
+            case FieldName.RabiFrequencyPhase:
+                return RabiFrequencyPhase(global_=self.global_)
+            case FieldName.Detuning:
+                return Detuning(global_=self.global_, local=self.local)
