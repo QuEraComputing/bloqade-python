effcf0d34a965604fa4d2f4fafc54178b643fb0a
Phillip Weinberg <weinbe58@gmail.com>
2023-04-30 21:59:48 -0400

    update name.

--- src/bloqade/emulator/sparse_operator.py
+++ src/bloqade/emulator/sparse_operator.py
@@ -1,7 +1,6 @@
 from pydantic.dataclasses import dataclass
 import numpy as np
 from numpy.typing import NDArray
-import pydantic.dataclasses
 from typing import Union
 
 
@@ -16,12 +15,12 @@ class Diagonal:  # used to represent detuning + Rydberg
 # use csr_matrix for rabi-terms that span multiple sites.
 # use PermMatrix for local rabi-terms
 @dataclass
-class PermMatrix:
+class IndexMapping:
     input_indices: Union[slice, NDArray]
     output_indices: Union[slice, NDArray] = slice(-1)
 
     def ajoint(self):
-        return PermMatrix(self.output_indices, self.input_indices)
+        return IndexMapping(self.output_indices, self.input_indices)
 
     def dot(self, other, out=None):
         result = np.zeros_like(other)
