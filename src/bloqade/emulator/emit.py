154e757c14a60fd04339ffb8034a1f44755da767
Phillip Weinberg <weinbe58@gmail.com>
2023-05-01 00:04:55 -0400

    refactor based on new space.

--- src/bloqade/emulator/emit.py
+++ src/bloqade/emulator/emit.py
@@ -1,12 +1,13 @@
 from bloqade.ir.sequence import LevelCoupling
 from bloqade.emulator.sparse_operator import IndexMapping, Diagonal
-from bloqade.emulator.space import Space, FullSpace, SubSpace, LocalHilbertSpace
+from bloqade.emulator.space import Space, SpaceType, LocalHilbertSpace
 from pydantic.dataclasses import dataclass
 from scipy.sparse import csr_matrix
 from enum import Enum
 import numpy as np
 from typing import Dict
 from numbers import Number
+from src.bloqade.emulator.emit import emit_rabi_index_map
 
 
 class RabiOperatorType(str, Enum):
@@ -22,30 +23,47 @@ class RabiInfo:
     space: Space
 
 
-def local_rabi_matrix(info: RabiInfo):
-    shape = (info.space.size, info.space.size)
-    dtype = np.common_type(list(info.target_atoms.values()))
-
+def emit_rabi_index_map(info: RabiInfo) -> IndexMapping:
     match info:
         case RabiInfo(
             op_type=RabiOperatorType.RealValued,
             level_coupling=LevelCoupling.Rydberg,
             target_atoms=target_atoms,
-            space=FullSpace(n_level=LocalHilbertSpace.TwoLevel) as space,
-        ) if len(target_atoms) == 1:
-            configurations = np.arange(space.size, dtype=space.state_type)
+            space=Space(
+                space_type=SpaceType.FullSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
+            ),
+        ):
             (atom_index,) = target_atoms.keys()
             input_indices = configurations ^ (1 << atom_index)
             return IndexMapping(input_indices)
 
         case RabiInfo(
+            op_type=RabiOperatorType.ComplexValued,
+            level_coupling=LevelCoupling.Rydberg,
+            target_atoms=target_atoms,
+            space=Space(
+                space_type=SpaceType.FullSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
+            ),
+        ):
+            (atom_index,) = target_atoms.keys()
+            output_indices = (configurations >> atom_index) & 1 == 1
+            input_indices = configurations[output_indices] ^ (1 << atom_index)
+            return IndexMapping(input_indices, output_indices)
+
+        case RabiInfo(
             op_type=RabiOperatorType.RealValued,
             level_coupling=LevelCoupling.Rydberg,
             target_atoms=target_atoms,
-            space=SubSpace(
-                n_level=LocalHilbertSpace.TwoLevel, configurations=configurations
-            ) as space,
-        ) if len(target_atoms) == 1:
+            space=Space(
+                space_type=SpaceType.SubSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
+            ),
+        ):
             (atom_index,) = target_atoms.keys()
             new_configurations = configurations ^ (1 << atom_index)
             input_indices = np.searchsorted(configurations, new_configurations)
@@ -55,13 +73,41 @@ def local_rabi_matrix(info: RabiInfo):
             return IndexMapping(input_indices, output_indices)
 
         case RabiInfo(
+            op_type=RabiOperatorType.ComplexValued,
+            level_coupling=LevelCoupling.Rydberg,
+            target_atoms=target_atoms,
+            space=Space(
+                space_type=SpaceType.SubSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
+            ),
+        ):
+            (atom_index,) = target_atoms.keys()
+            rydberg_states = (configurations >> atom_index) & 1 == 1
+            new_configurations = configurations[rydberg_states] ^ (1 << atom_index)
+            input_indices = np.searchsorted(configurations, new_configurations)
+
+            nonzero_indices = input_indices < configurations.size
+            output_indices = np.logical_and(rydberg_states, nonzero_indices)
+            input_indices = input_indices[nonzero_indices]
+            return IndexMapping(input_indices, output_indices)
+
+
+def emit_rabi_csr_matrix(info: RabiInfo) -> csr_matrix:
+    shape = (info.space.size, info.space.size)
+    dtype = np.common_type(list(info.target_atoms.values()))
+    match info:
+        case RabiInfo(
             op_type=RabiOperatorType.RealValued,
             level_coupling=LevelCoupling.Rydberg,
             target_atoms=target_atoms,
-            space=FullSpace(n_level=LocalHilbertSpace.TwoLevel) as space,
+            space=Space(
+                space_type=SpaceType.FullSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
+            ) as space,
         ):
-            configurations = np.arange(space.size, dtype=space.state_type)
-            indptr = np.zeros(configurations.size, dtype=space.index_type)
+            indptr = np.zeros(space.size + 1, dtype=space.index_type)
             indptr[1:] = len(target_atoms)
 
             np.cumsum(indptr, out=indptr)
@@ -82,8 +128,10 @@ def local_rabi_matrix(info: RabiInfo):
             op_type=RabiOperatorType.RealValued,
             level_coupling=LevelCoupling.Rydberg,
             target_atoms=target_atoms,
-            space=SubSpace(
-                n_level=LocalHilbertSpace.TwoLevel, configurations=configurations
+            space=Space(
+                space_type=SpaceType.SubSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
             ) as space,
         ):
             indptr = np.zeros(configurations.size, dtype=space.index_type)
@@ -104,7 +152,7 @@ def local_rabi_matrix(info: RabiInfo):
                 new_configurations = configurations ^ (1 << atom_index)
                 input_indices = np.searchsorted(configurations, new_configurations)
 
-                input_indices = input_indices[output_indices < configurations.size]
+                input_indices = input_indices[input_indices < configurations.size]
                 nonzero_index = index[input_indices]
 
                 indices[nonzero_index] = input_indices
@@ -119,40 +167,13 @@ def local_rabi_matrix(info: RabiInfo):
             op_type=RabiOperatorType.ComplexValued,
             level_coupling=LevelCoupling.Rydberg,
             target_atoms=target_atoms,
-            space=FullSpace(n_level=LocalHilbertSpace.TwoLevel) as space,
-        ) if len(target_atoms) == 1:
-            configurations = np.arange(space.size, dtype=space.state_type)
-            (atom_index,) = target_atoms.keys()
-            output_indices = (configurations >> atom_index) & 1 == 1
-            input_indices = configurations[output_indices] ^ (1 << atom_index)
-            return IndexMapping(input_indices, output_indices)
-
-        case RabiInfo(
-            op_type=RabiOperatorType.ComplexValued,
-            level_coupling=LevelCoupling.Rydberg,
-            target_atoms=target_atoms,
-            space=SubSpace(
-                n_level=LocalHilbertSpace.TwoLevel, configurations=configurations
+            space=Space(
+                space_type=SpaceType.FullSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
             ) as space,
-        ) if len(target_atoms) == 1:
-            (atom_index,) = target_atoms.keys()
-            rydberg_states = (configurations >> atom_index) & 1 == 1
-            new_configurations = configurations[rydberg_states] ^ (1 << atom_index)
-            input_indices = np.searchsorted(configurations, new_configurations)
-
-            nonzero_indices = input_indices < configurations.size
-            output_indices = np.logical_and(rydberg_states, nonzero_indices)
-            input_indices = input_indices[nonzero_indices]
-            return IndexMapping(input_indices, output_indices)
-
-        case RabiInfo(
-            op_type=RabiOperatorType.ComplexValued,
-            level_coupling=LevelCoupling.Rydberg,
-            target_atoms=target_atoms,
-            space=FullSpace(n_level=LocalHilbertSpace.TwoLevel) as space,
         ):
-            configurations = np.arange(space.size, dtype=space.state_type)
-            indptr = np.zeros(configurations.size, dtype=space.index_type)
+            indptr = np.zeros(space.size + 1, dtype=space.index_type)
 
             nnz_values = indptr[1:]
             for atom_index in target_atoms.keys():
@@ -181,11 +202,13 @@ def local_rabi_matrix(info: RabiInfo):
             op_type=RabiOperatorType.ComplexValued,
             level_coupling=LevelCoupling.Rydberg,
             target_atoms=target_atoms,
-            space=SubSpace(
-                n_level=LocalHilbertSpace.TwoLevel, configurations=configurations
+            space=Space(
+                space_type=SpaceType.SubSpace,
+                n_level=LocalHilbertSpace.TwoLevel,
+                configurations=configurations,
             ) as space,
         ):
-            indptr = np.zeros(configurations.size, dtype=space.index_type)
+            indptr = np.zeros(space.size + 1, dtype=space.index_type)
 
             nnz_values = indptr[1:]
             for atom_index, value in target_atoms.items():
