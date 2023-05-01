5c4177b382da3ea07c2588d159031e1b7b82c2bd
Roger-luo <rogerluo.rl18@gmail.com>
2023-04-30 22:06:47 -0400

    add dict interface

--- exmaples/adiabatic.py
+++ exmaples/adiabatic.py
@@ -15,3 +15,28 @@ lattice.Square(6).rydberg\
     .braket(nshots=1000)\
     .submit(token="112312312")\
     .report()
+
+
+# dict interface
+
+seq = Sequence(
+    {
+        rydberg: {
+            detuning: {
+                Global: Constant('initial_detuning', 'up_time')\
+                    .append(Linear('initial_detuning', 'final_detuning', 'anneal_time'))\
+                    .append(Constant('final_detuning', 'up_time')),
+            },
+            rabi.amplitude: {
+                Global: Linear(0.0, 'rabi_amplitude_max', 'up_time')\
+                    .append(Constant('rabi_amplitude_max', 'anneal_time'))\
+                    .append(Linear('rabi_amplitude_max', 0.0, 'up_time'))
+            },
+        }
+    }
+)
+
+lattice.Square(6).run(seq)\
+    .braket(nshots=1000)\
+    .submit(token="112312312")\
+    .report()
