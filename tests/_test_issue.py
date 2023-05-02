a271cc00e410bb11a76d6949c7d8d2859c7477f6
Roger-luo <rogerluo.rl18@gmail.com>
2023-05-02 18:03:46 -0400

    skip these with pytest for now

--- tests/_test_issue.py
+++ tests/_test_issue.py
@@ -3,7 +3,7 @@ import bloqade.lattice as lattice
 
 (
     lattice.Square(3)
-    .rydberg.detuning.glob.apply(Linear(start=1.0, stop="x", duration=3.0))
+    .rydberg.detuning.uniform.apply(Linear(start=1.0, stop="x", duration=3.0))
     .location(2)
     .scale(3.0)
     .apply(Linear(start=1.0, stop="x", duration=3.0))
@@ -11,7 +11,7 @@ import bloqade.lattice as lattice
 
 (
     lattice.Square(3)
-    .rydberg.detuning.glob.apply(Linear(start=1.0, stop="x", duration=3.0))
+    .rydberg.detuning.uniform.apply(Linear(start=1.0, stop="x", duration=3.0))
     .location(2)
     .scale(3.0)
     .apply(Linear(start=1.0, stop="x", duration=3.0))
