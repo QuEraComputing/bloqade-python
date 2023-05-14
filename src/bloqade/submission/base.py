d033d70efdbffb98e0526be2bd4541b212f9042d
Phillip Weinberg <weinbe58@gmail.com>
2023-05-13 21:05:59 -0400

    initial unified task implementaiton. (#77)

--- src/bloqade/submission/base.py
+++ src/bloqade/submission/base.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel
 
 
 class SubmissionBackend(BaseModel):
+    def validate_task(self, task_ir: QuantumTaskIR):
+        raise NotImplementedError
+
     def submit_task(self, task_ir: QuantumTaskIR):
         raise NotImplementedError
 
--- src/bloqade/task.py
+++ src/bloqade/task.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel
 from quera_ahs_utils.quera_ir.task_results import QuEraTaskResults
 
 from quera_ahs_utils.ir import quera_task_to_braket_ahs
@@ -21,13 +21,20 @@ import os
 import json
 
 
-class TaskFuture:
-    def report(self) -> "TaskReport":
-        """generate the task report"""
-        return TaskReport(self)
+class Task:
+    def submit(self) -> "TaskFuture":
+        raise NotImplementedError
 
 
-class QuantumTaskFuture(TaskFuture):
+class TaskDataModel(BaseModel):
+    # note that the separate types here are because pydantic
+    # will automatically convert between the types in a Union
+    quera_task_ir: Optional[QuEraTaskSpecification] = None
+    braket_task_ir: Optional[BraketTaskSpecification] = None
+    mock_backend: Optional[DumbMockBackend] = None
+    quera_backend: Optional[QuEraBackend] = None
+    braket_backend: Optional[BraketBackend] = None
+
     def json(self, exclude_none=True, by_alias=True, **json_options):
         return super().json(
             exclude_none=exclude_none, by_alias=by_alias, **json_options
@@ -43,8 +50,141 @@ class QuantumTaskFuture(TaskFuture):
             case _:
                 filename_or_io.write(self.json(**json_options))
 
+    def read_json(self, filename_or_io: Union[str, TextIO]):
+        match filename_or_io:
+            case str(filename):
+                with open(filename, "r") as io:
+                    params = json.load(io)
+            case _:
+                params = json.load(filename_or_io)
+
+        braket_backend = params.get("braket_backend")
+        if braket_backend:
+            self.braket_backend = BraketBackend(**braket_backend)
+
+        quera_backend = params.get("quera_backend")
+        if quera_backend:
+            self.quera_backend = QuEraBackend(**quera_backend)
+
+        mock_backend = params.get("mock_backend")
+        if mock_backend:
+            self.mock_backend = DumbMockBackend(**mock_backend)
+
+        quera_task_ir = params.get("quera_task_ir")
+        if quera_task_ir:
+            self.quera_task_ir = QuEraTaskSpecification(**quera_task_ir)
+
+        braket_task_ir = params.get("braket_task_ir")
+        if braket_task_ir:
+            self.braket_task_ir = BraketTaskSpecification(**braket_task_ir)
+
+    def _validate_fields(self):
+        if self.quera_task_ir is None and self.braket_task_ir is None:
+            raise AttributeError("Missing task_ir.")
+
+        if (
+            self.braket_backend is None
+            and self.mock_backend is None
+            and self.quera_backend is None
+        ):
+            raise AttributeError("No backend found for hardware task.")
+
+
+class HardwareTask(TaskDataModel, Task):
+    def submit(self) -> "HardwareTaskFuture":
+        self._validate_fields()
+
+        if self.braket_backend:
+            task_id = self.braket_backend.submit_task(self.braket_task_ir)
+            return HardwareTaskFuture(
+                task_id=task_id,
+                braket_task_ir=self.braket_task_ir,
+                braket_backend=self.braket_backend,
+            )
+        if self.quera_backend:
+            task_id = self.quera_backend.submit_task(self.quera_task_ir)
+            return HardwareTaskFuture(
+                task_id=task_id,
+                quera_task_ir=self.quera_task_ir,
+                quera_backend=self.quera_backend,
+            )
+        if self.mock_backend:
+            task_id = self.mock_backend.submit_task(self.quera_task_ir)
+            return HardwareTaskFuture(
+                task_id=task_id,
+                quera_task_ir=self.quera_task_ir,
+                mock_backend=self.mock_backend,
+            )
+
+    def validate(self) -> None:
+        self._validate_fields()
+
+        if self.braket_backend:
+            self.braket_backend.validate_task(self.braket_task_ir)
+
+        if self.quera_backend:
+            self.quera_backend.validate_task(self.quera_task_ir)
+
+        if self.mock_backend:
+            self.mock_backend.validate_task(self.quera_task_ir)
+
+
+class TaskFuture:
+    def report(self) -> "TaskReport":
+        """generate the task report"""
+        return TaskReport(self)
+
+
+class TaskFutureDataModel(TaskDataModel):
+    task_id: Optional[str] = None
+
+    def read_json(self, filename_or_io: Union[str, TextIO]):
+        super().read_json(filename_or_io)
+
+        match filename_or_io:
+            case str(filename):
+                with open(filename, "r") as io:
+                    params = json.load(io)
+            case _:
+                params = json.load(filename_or_io)
+
+        self.task_id = params.get("task_id")
+
+        task_result_ir = params.get("task_result_ir")
+        if task_result_ir:
+            self.task_result_ir = QuEraTaskResults(**task_result_ir)
+
+    def _validate_fields(self):
+        super()._validate_fields()
+
+        if self.task_id is None:
+            raise AttributeError("Missing task_id.")
+
+
+class HardwareTaskFuture(TaskFutureDataModel, TaskFuture):
+    def cancel(self) -> None:
+        self._validate_fields()
+
+        if self.braket_backend:
+            self.braket_backend.cancel_task(self.task_id)
+
+        if self.quera_backend:
+            self.quera_backend.cancel_task(self.task_id)
+
+        if self.mock_backend:
+            self.mock_backend.cancel_task(self.task_id)
+
     def fetch(self) -> QuEraTaskResults:
-        raise NotImplementedError
+        self._validate_fields()
+        print("here")
+        if self.braket_backend:
+            return self.braket_backend.task_results(self.task_id)
+
+        if self.quera_backend:
+            return self.quera_backend.task_results(self.task_id)
+
+        if self.mock_backend:
+            return self.mock_backend.task_results(self.task_id)
 
     @property
     def task_results(self) -> QuEraTaskResults:
@@ -52,65 +192,6 @@ class QuantumTaskFuture(TaskFuture):
             self.task_result_ir = self.fetch()
 
         return self.task_result_ir
-
-
-class MockTaskFuture(BaseModel, QuantumTaskFuture):
-    task_ir: QuEraTaskSpecification
-    task_id: str
-    backend: DumbMockBackend
-    task_result_ir: Optional[QuEraTaskResults] = None
-
-    def fetch(self):
-        return self.backend.task_results(self.task_id)
-
-
-class BraketTaskFuture(BaseModel, QuantumTaskFuture):
-    task_ir: BraketTaskSpecification
-    task_id: str
-    backend: BraketBackend
-    task_result_ir: Optional[QuEraTaskResults] = None
-
-    def fetch(self):
-        return self.backend.task_results(self.task_id)
-
-
-class QuEraTaskFuture(BaseModel, QuantumTaskFuture):
-    task_ir: QuEraTaskSpecification
-    task_id: str
-    backend: QuEraBackend
-    task_result_ir: Optional[QuEraTaskResults] = None
-
-    def fetch(self):
-        return self.backend.task_results(self.task_id)
-
-
-def read_future_from_json(
-    filename_or_io: Union[str, TextIO]
-) -> Union[QuantumTaskFuture, BraketTaskFuture, MockTaskFuture]:
-    match filename_or_io:
-        case str(filename):
-            with open(filename, "r") as io:
-                quantum_task_ir = json.load(io)
-
-        case _:
-            quantum_task_ir = json.load(filename_or_io)
-
-    try:
-        return QuEraTaskFuture(**quantum_task_ir)
-    except ValidationError:
-        pass
-
-    try:
-        return BraketTaskFuture(**quantum_task_ir)
-    except ValidationError:
-        pass
-
-    try:
-        return MockTaskFuture(**quantum_task_ir)
-    except ValidationError:
-        pass
-
-    raise ValueError("Unable to parse JSON file to a task future.")
 
 
 # NOTE: this is only the basic report, we should provide
@@ -173,7 +254,7 @@ class Program:
     def simu(self, *args, **kwargs):
         raise NotImplementedError
 
-    def braket(self, nshots: int) -> BraketTaskFuture:
+    def braket(self, nshots: int) -> HardwareTask:
         from bloqade.codegen.quera_hardware import SchemaCodeGen
 
         quera_task_ir = SchemaCodeGen().emit(nshots, self)
@@ -182,12 +263,9 @@ class Program:
             nshots=nshots, program=braket_ahs_program.to_ir()
         )
 
-        backend = BraketBackend()
-        task_id = backend.submit_task(task_ir)
+        return HardwareTask(braket_task_ir=task_ir, braket_backend=BraketBackend())
 
-        return BraketTaskFuture(task_ir=task_ir, task_id=task_id, backend=backend)
-
-    def quera(self, nshots: int, config_file: Optional[str] = None) -> QuEraTaskFuture:
+    def quera(self, nshots: int, config_file: Optional[str] = None) -> HardwareTask:
         from bloqade.codegen.quera_hardware import SchemaCodeGen
 
         task_ir = SchemaCodeGen().emit(nshots, self)
@@ -202,15 +280,12 @@ class Program:
 
             backend = QuEraBackend(**api_config)
 
-        task_id = backend.submit_task(task_ir)
+        return HardwareTask(quera_task_ir=task_ir, quera_backend=backend)
 
-        return QuEraTaskFuture(task_ir=task_ir, task_id=task_id, backend=backend)
-
-    def mock(self, nshots: int, state_file: str = ".mock_state.txt") -> MockTaskFuture:
+    def mock(self, nshots: int, state_file: str = ".mock_state.txt") -> HardwareTask:
         from bloqade.codegen.quera_hardware import SchemaCodeGen
 
         task_ir = SchemaCodeGen().emit(nshots, self)
         backend = DumbMockBackend(state_file=state_file)
 
-        task_id = backend.submit_task(task_ir)
-        return MockTaskFuture(task_ir=task_ir, task_id=task_id, backend=backend)
+        return HardwareTask(quera_task_ir=task_ir, mock_backend=backend)
