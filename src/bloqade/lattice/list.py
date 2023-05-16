52e3fc9b8308bf21d9703808e96f282c86d441ed
Phillip Weinberg <weinbe58@gmail.com>
2023-05-16 13:56:40 -0400

    Adding extend and append to `ListOfPositions` (#94)
    * adding extend and append
    
    * fixing initialization with no atoms.

--- src/bloqade/lattice/list.py
+++ src/bloqade/lattice/list.py
@@ -7,12 +7,23 @@ from typing import List, Tuple
 class ListOfPositions(Lattice):
     value: List[Tuple[float, ...]]
 
-    def __init__(self, value: List[Tuple[float, ...]]):
+    def __init__(self, value: List[Tuple[float, ...]] = []):
         if not all(map(lambda x: len(x) == len(value[0]), value)):
             raise ValueError("all positions must have the same dimension")
+
+        if value:
+            self.__n_atoms = len(value)
+            self.__n_dims = len(value[0])
+        else:
+            self.__n_atoms = 0
+            self.__n_dims = None
         self.value = value
-        self.__n_atoms = len(value)
-        self.__n_dims = len(value[0])
+
+    def append(self, position: Tuple[float, ...]):
+        return ListOfPositions(self.value + [position])
+
+    def extend(self, positions: List[Tuple[float, ...]]):
+        return ListOfPositions(self.value + positions)
 
     @property
     def n_atoms(self):
