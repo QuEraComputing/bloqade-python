b6defedda8e5912ea58290fbc85a8d881c5d9de5
Phillip Weinberg <weinbe58@gmail.com>
2023-05-23 16:23:21 -0400

    rename `multiplex` to `parallel` or `parallelize`.

--- src/bloqade/builder/base.py
+++ src/bloqade/builder/base.py
@@ -1,14 +1,14 @@
 from typing import TYPE_CHECKING, Optional, Union
 
 if TYPE_CHECKING:
-    from bloqade.ir.location.base import AtomArrangement, MultuplexRegister
+    from bloqade.ir.location.base import AtomArrangement, ParallelRegister
 
 
 class Builder:
     def __init__(
         self,
         parent: Optional["Builder"] = None,
-        register: Optional[Union["AtomArrangement", "MultuplexRegister"]] = None,
+        register: Optional[Union["AtomArrangement", "ParallelRegister"]] = None,
     ) -> None:
         self.__parent__ = parent
         self.__register__ = register
