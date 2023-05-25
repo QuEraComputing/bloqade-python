0246c697f1b88a839687a33f7760d56eaee31af4
Phillip Weinberg <weinbe58@gmail.com>
2023-05-25 13:43:48 -0400

    moving parallelize to end of builder.

--- src/bloqade/builder/terminate.py
+++ src/bloqade/builder/terminate.py
@@ -3,7 +3,17 @@ from .coupling import LevelCoupling
 from .field import Rabi
 from .start import ProgramStart
 from .emit import Emit
+from bloqade.ir.location.base import ParallelRegister
+from typing import Any
 
 
 class Terminate(SpatialModulation, LevelCoupling, Rabi, ProgramStart, Emit):
-    pass
+    def parallelize(self, cluster_spacing: Any) -> Emit:
+        parallel_register = ParallelRegister(self.register, cluster_spacing)
+        return Emit(
+            self,
+            assignments=self.__assignments__,
+            batch=self.__batch__,
+            register=parallel_register,
+            sequence=self.__sequence__,
+        )
