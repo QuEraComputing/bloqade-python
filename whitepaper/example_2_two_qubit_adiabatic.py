94d33e54f15282689895570baba0c43782dfca61
John Long <johnzl@protonmail.ch>
2023-06-02 11:10:25 -0700

    added two qubit adiabatic example

---
+++ whitepaper/example_2_two_qubit_adiabatic.py
@@ -0,0 +1,31 @@
+from bloqade import start
+import numpy as np
+from bloqade.ir import Variable, Literal
+
+durations = [1, 2, 1]
+
+two_qubit_adiabatic_program = (
+    start
+    .add_positions([(0, 0), (0, Variable("distance"))])
+    .rydberg
+    .rabi
+    .amplitude
+    .uniform
+    .piecewise_linear(
+        durations = durations,
+        values = [0, 15, 15, 0]
+    )
+    .detuning
+    .uniform
+    .piecewise_linear(
+        durations = durations,
+        values = [-15, -15, 15, 15]
+    )
+)
+
+(
+    two_qubit_adiabatic_program
+    .parallelize(24)
+    .batch_assign(distance=np.around(np.arange(4, 11, 1), 13))
+    .mock(1000)
+)
