438ec9b88a08fd99c78e613dacd4a4ce5f614457
Phillip Weinberg <weinbe58@gmail.com>
2023-06-03 22:19:13 -0400

    running linter

--- whitepaper/example_1a_rabi.py
+++ whitepaper/example_1a_rabi.py
@@ -6,15 +6,16 @@ from bloqade import start
 durations = [Variable("RAMP_TIME"), Variable("run_time"), Variable("RAMP_TIME")]
 
 rabi_oscillations_program = (
-                start.add_position((0,0))
-                .rydberg.rabi.amplitude.uniform.piecewise_linear(
-                    durations = durations,
-                    values = [0, "rabi_value", "rabi_value", 0]
-                ).detuning.uniform.piecewise_linear(
-                    durations = durations,
-                    values = [0, "detuning_value", "detuning_value", 0]
-                )
-            )
+    start.add_position((0, 0))
+    .rydberg.rabi.amplitude.uniform.piecewise_linear(
+        durations=durations, values=[0, "rabi_value", "rabi_value", 0]
+    )
+    .detuning.uniform.piecewise_linear(
+        durations=durations, values=[0, "detuning_value", "detuning_value", 0]
+    )
+)
 
 
-rabi_oscillations_program.parallelize(24).assign(RAMP_TIME = 0.06, rabi_value = 15, detuning_value = 0.0).batch_assign(run_time = np.around(np.arange(0,21,1) * 0.05, 13)).mock(10, "test.txt")
+rabi_oscillations_program.parallelize(24).assign(
+    RAMP_TIME=0.06, rabi_value=15, detuning_value=0.0
+).batch_assign(run_time=np.around(np.arange(0, 21, 1) * 0.05, 13)).mock(10, "test.txt")
