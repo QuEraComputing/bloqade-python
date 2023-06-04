9a0990e576b644f11fc9d437e8b923c4ff376196
Phillip Weinberg <weinbe58@gmail.com>
2023-06-03 22:18:29 -0400

    running linter

--- whitepaper/example_1b_IR_ramsey.py
+++ whitepaper/example_1b_IR_ramsey.py
@@ -3,42 +3,39 @@ from bloqade.ir.waveform import Linear, Constant
 from bloqade import start
 import numpy as np
 
-plateau_time = ((np.pi/2 - 0.625)/12.5)
+plateau_time = (np.pi / 2 - 0.625) / 12.5
 
-# would be nice to have builder functions like what Bloqade has (e.g. piecewise_linear, piecewise_constant, etc.)
+# would be nice to have builder functions like what Bloqade has
+# (e.g. piecewise_linear, piecewise_constant, etc.)
 pi_over_two_pulse = (
-        Linear(start = 0.0, stop = 12.5, duration = 0.5)
-        .append(
-            Linear(start = 12.5, stop = 12.5, duration = plateau_time)
-        ).append(
-            Linear(start = 12.5, stop = 0.0, duration = 0.5)
-        )
+    Linear(start=0.0, stop=12.5, duration=0.5)
+    .append(Linear(start=12.5, stop=12.5, duration=plateau_time))
+    .append(Linear(start=12.5, stop=0.0, duration=0.5))
 )
 
-# would it be possible to just define a constant whose duration is equal to the whole program without
-# needing to pass in that value as a separate argument? Say another IR type that could be "auto-filled" 
+# would it be possible to just define a constant whose
+# duration is equal to the whole program without
+# needing to pass in that value as a separate argument?
+# Say another IR type that could be "auto-filled"
 # upon finishing the construction of the program?
 
-rabi_pulse = (
-    pi_over_two_pulse.append(
-        Constant(value=0, duration=Variable("t_run"))
-    ).append(
-        pi_over_two_pulse
+rabi_pulse = pi_over_two_pulse.append(
+    Constant(value=0, duration=Variable("t_run"))
+).append(pi_over_two_pulse)
+
+
+ramsey_program = (
+    start.add_position([0, 0])
+    .rydberg.rabi.amplitude.uniform.apply(rabi_pulse)
+    .detuning.uniform.constant(
+        value=10.5,
+        # having to manually remember and sum the times versus a variable is not
+        # very pleasant
+        duration=Variable("t_run") + Literal(2 * np.sum([0.5, plateau_time, 0.5])),
     )
-)    
-
-
-ramsey_program = (start.add_position([0,0])
-                  .rydberg.rabi.amplitude.uniform.apply(
-                      rabi_pulse
-                  )
-                  .detuning.uniform.constant(
-                      value = 10.5,
-                      # having to manually remember and sum the times versus a variable is not
-                      # very pleasant
-                      duration = Variable("t_run") + Literal(2 * np.sum([0.5, plateau_time, 0.5]))
-                  )
 )
 
 # does not suffer the same problem as using the plain builder syntax for waveform
-ramsey_program.parallelize(24).batch_assign(t_run = np.around(np.arange(0,30,1) * 0.1, 13)).mock(100)
+ramsey_program.parallelize(24).batch_assign(
+    t_run=np.around(np.arange(0, 30, 1) * 0.1, 13)
+).mock(100)
