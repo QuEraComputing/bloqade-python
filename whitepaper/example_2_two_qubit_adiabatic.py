31258a3c1d72101a8a3a410769338443dfa54ca4
Phillip Weinberg <weinbe58@gmail.com>
2023-06-03 22:15:32 -0400

    running linter

--- whitepaper/example_2_two_qubit_adiabatic.py
+++ whitepaper/example_2_two_qubit_adiabatic.py
@@ -1,31 +1,19 @@
 from bloqade import start
 import numpy as np
-from bloqade.ir import Variable, Literal
+from bloqade.ir import Variable
 
 durations = [1, 2, 1]
 
 two_qubit_adiabatic_program = (
-    start
-    .add_positions([(0, 0), (0, Variable("distance"))])
-    .rydberg
-    .rabi
-    .amplitude
-    .uniform
-    .piecewise_linear(
-        durations = durations,
-        values = [0, 15, 15, 0]
+    start.add_positions([(0, 0), (0, Variable("distance"))])
+    .rydberg.rabi.amplitude.uniform.piecewise_linear(
+        durations=durations, values=[0, 15, 15, 0]
     )
-    .detuning
-    .uniform
-    .piecewise_linear(
-        durations = durations,
-        values = [-15, -15, 15, 15]
-    )
+    .detuning.uniform.piecewise_linear(durations=durations, values=[-15, -15, 15, 15])
 )
 
 (
-    two_qubit_adiabatic_program
-    .parallelize(24)
+    two_qubit_adiabatic_program.parallelize(24)
     .batch_assign(distance=np.around(np.arange(4, 11, 1), 13))
     .mock(1000)
 )
