6cf25264a3f2542e36eb0f46d47cab28b879d340
Phillip Weinberg <weinbe58@gmail.com>
2023-06-05 09:48:25 -0400

    export `Literal`

--- src/bloqade/__init__.py
+++ src/bloqade/__init__.py
@@ -1,4 +1,4 @@
 from bloqade.ir.location import start
-from bloqade.ir import var, cast, Variable
+from bloqade.ir import var, cast, Variable, Literal
 
-__all__ = ["start", "var", "cast", "Variable"]
+__all__ = ["start", "var", "cast", "Variable", "Literal"]
