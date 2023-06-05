b272aee81a6de6f0acca43c92e68d797e92eafac
John Long <johnzl@protonmail.ch>
2023-06-05 10:59:10 -0700

    enforce DRY, remove unnecessary  call

--- whitepaper/example_2_two_qubit_adiabatic.py
+++ whitepaper/example_2_two_qubit_adiabatic.py
@@ -1,17 +1,15 @@
 from bloqade import start
 import numpy as np
-from bloqade.ir import Variable
 
 durations = [1, 2, 1]
 
 two_qubit_adiabatic_program = (
-    start.add_positions([(0, 0), (0, Variable("distance"))])
-    .rydberg.rabi.amplitude.uniform.piecewise_linear(
-        durations=durations, values=[0, 15, 15, 0]
-    )
-    .detuning.uniform.piecewise_linear(durations=durations, values=[-15, -15, 15, 15])
+    start.add_positions([(0, 0), (0, "distance")])
+    .rydberg.rabi.amplitude.uniform.piecewise_linear(durations, [0, 15, 15, 0])
+    .detuning.uniform.piecewise_linear(durations, [-15, -15, 15, 15])
 )
 
+# Submit to actual hardware
 (
     two_qubit_adiabatic_program.parallelize(24)
     .batch_assign(distance=np.around(np.arange(4, 11, 1), 13))
