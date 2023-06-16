3a0d720c3f188b0d28c6c9ac2428bc951270e778
Phillip Weinberg <weinbe58@gmail.com>
2023-06-16 14:39:21 -0400

    adding conversion to csr.

--- src/bloqade/codegen/emulator/sparse_operator.py
+++ src/bloqade/codegen/emulator/sparse_operator.py
@@ -1,4 +1,5 @@
 from pydantic.dataclasses import dataclass
+from scipy.sparse import csr_matrix
 import numpy as np
 from numpy.typing import NDArray
 from typing import Union
@@ -11,6 +12,14 @@ class Diagonal:  # used to represent detuning + Rydberg
     def dot(self, other):
         return self.diagonal * other
 
+    def to_csr(self):
+        indices = np.arange(len(self.diagonal))
+        indptr = np.arange(len(self.diagonal) + 1)
+        data = self.diagonal
+        return csr_matrix(
+            (data, indices, indptr), shape=(len(self.diagonal), len(self.diagonal))
+        )
+
 
 # use csr_matrix for rabi-terms that span multiple sites.
 # use PermMatrix for local rabi-terms
@@ -18,6 +27,7 @@ class Diagonal:  # used to represent detuning + Rydberg
 class IndexMapping:
     input_indices: Union[slice, NDArray]
     output_indices: Union[slice, NDArray] = slice(-1)
+    n_row: int
 
     def ajoint(self):
         return IndexMapping(self.output_indices, self.input_indices)
@@ -25,3 +35,17 @@ class IndexMapping:
     def dot(self, other, out=None):
         result = np.zeros_like(other)
         result[self.output_indices] = other[self.input_indices]
+
+    def to_csr(self):
+        indptr = np.zeros(self.n_row + 1)
+        indptr[1:][self.output_indices] = 1
+        np.cumsum(indptr, out=indptr)
+
+        if isinstance(self.input_indices, slice):
+            indices = np.arange(self.n_row)
+        else:
+            indices = self.input_indices
+
+        data = np.ones_like(indices)
+
+        return csr_matrix((data, indices, indptr), shape=(self.n_row, self.n_row))
