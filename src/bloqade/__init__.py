7f2edad074e30b6afa0c6988ddbe3c6d13a084b6
Phillip Weinberg <weinbe58@gmail.com>
2023-06-20 10:35:18 -0400

    adding factory functions for generating piecewise-linear/constant. exporting them to the bloqade public API.

--- src/bloqade/__init__.py
+++ src/bloqade/__init__.py
@@ -1,4 +1,13 @@
 from bloqade.ir.location import start
 from bloqade.ir import var, cast, Variable, Literal
+from bloqade.builder.factory import piecewise_linear, piecewise_constant
 
-__all__ = ["start", "var", "cast", "Variable", "Literal"]
+__all__ = [
+    "start",
+    "var",
+    "cast",
+    "Variable",
+    "Literal",
+    "piecewise_linear",
+    "piecewise_constant",
+]
---
+++ src/bloqade/builder/factory.py
@@ -0,0 +1,24 @@
+from bloqade.ir.control.waveform import Waveform, Linear, Constant
+from typing import Any, List
+
+
+def piecewise_linear(durations: List[Any], values: List[Any]) -> Waveform:
+    pwl_wf = None
+    for duration, start, stop in zip(durations, values[:-1], values[1:]):
+        if pwl_wf is None:
+            pwl_wf = Linear(start, stop, duration)
+        else:
+            pwl_wf = pwl_wf.append(Linear(start, stop, duration))
+
+    return pwl_wf
+
+
+def piecewise_constant(durations: List[Any], values: List[Any]) -> Waveform:
+    pwc_wf = None
+    for duration, value in zip(durations, values):
+        if pwc_wf is None:
+            pwc_wf = Constant(value, duration)
+        else:
+            pwc_wf = pwc_wf.append(Constant(value, duration))
+
+    return pwc_wf
