50e067be5783a42b30a9c2e54b55eadfc66eeeca
Phillip Weinberg <weinbe58@gmail.com>
2023-06-20 11:32:41 -0400

    Adding method to add a fixed number of defects and renaming functions to refect difference in API.

--- src/bloqade/ir/location/base.py
+++ src/bloqade/ir/location/base.py
@@ -105,12 +105,30 @@ class AtomArrangement(ProgramStart):
     def n_dims(self) -> int:
         raise NotImplementedError
 
-    def add_defects(
+    def apply_defect_count(
+        self, n_defects: int, rng: np.random.Generator = np.random.default_rng()
+    ) -> "ListOfLocations":
+        from .list import ListOfLocations
+
+        location_list = []
+        for location_info in self.enumerate():
+            location_list.append(location_info)
+
+        for _ in range(n_defects):
+            idx = rng.integers(0, len(location_list))
+            location_list[idx] = LocationInfo(
+                location_list[idx].position,
+                (False if location_list[idx].filling is SiteFilling.filled else True),
+            )
+
+        return ListOfLocations(location_list)
+
+    def apply_defect_density(
         self,
         defect_probability: float,
         rng: np.random.Generator = np.random.default_rng(),
     ) -> "ListOfLocations":
-        from bloqade.ir.location.list import ListOfLocations
+        from .list import ListOfLocations
 
         p = min(1, max(0, defect_probability))
         location_list = []
