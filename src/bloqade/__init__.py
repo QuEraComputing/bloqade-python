6dd18fb0f9a4cc091d2f120a7c093532e0cb4e22
Phillip Weinberg <weinbe58@gmail.com>
2023-06-20 15:21:04 -0400

    Factory functions for piecewise-linear/constant.  (#213)
    * adding factory functions for generating piecewise-linear/constant. exporting them to the bloqade public API.
    
    * adding factory functions for `linear` and `constant` to keep the snake-case style.

--- src/bloqade/__init__.py
+++ src/bloqade/__init__.py
@@ -1,4 +1,20 @@
 from bloqade.ir.location import start
 from bloqade.ir import var, cast, Variable, Literal
+from bloqade.builder.factory import (
+    piecewise_linear,
+    piecewise_constant,
+    linear,
+    constant,
+)
 
-__all__ = ["start", "var", "cast", "Variable", "Literal"]
+__all__ = [
+    "start",
+    "var",
+    "cast",
+    "Variable",
+    "Literal",
+    "piecewise_linear",
+    "piecewise_constant",
+    "linear",
+    "constant",
+]
