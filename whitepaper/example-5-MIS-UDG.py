f8db68cf264ae9610783f6adc87e1439882708c0
John Long <johnzl@protonmail.ch>
2023-06-21 19:35:21 -0400

    use newly implemented defect application method

--- whitepaper/example-5-MIS-UDG.py
+++ whitepaper/example-5-MIS-UDG.py
@@ -1,41 +1,19 @@
-from bloqade import start
+from bloqade.ir.location import Square
 import numpy as np
-
-
-# Modified from Aquila whitepaper
-def generate_random_graph(
-    Lx: int, Ly: int, p_filled: float = 0.7, lattice_spacing: float = 5.0, SEED: int = 0
-):
-    randgen = np.random.RandomState(SEED)
-
-    positions = []
-    fillings = []
-
-    for ix in range(Lx):
-        for iy in range(Ly):
-            filling = True if randgen.uniform() < p_filled else False
-
-            x = np.around(ix * lattice_spacing, 13)
-            y = np.around(iy * lattice_spacing, 13)
-
-            positions.append((x, y))
-            fillings.append(filling)
-
-    return positions, fillings
-
-
-# generate positions and fillings, then feed directly into program builder
-positions, filling = generate_random_graph(15, 15)
 
 # durations for rabi and detuning
 durations = [0.3, 1.6, 0.3]
 
 mis_udg_program = (
-    start.add_positions(positions, filling)
+    Square(15, 0.5)
+    .apply_defect_density(0.5)
     .rydberg.rabi.amplitude.uniform.piecewise_linear(durations, [0.0, 15.0, 15.0, 0.0])
     .detuning.uniform.piecewise_linear(
         durations, [-30, -30, "final_detuning", "final_detuning"]
     )
 )
 
+# submit to HW
 mis_udg_program.batch_assign(final_detuning=np.linspace(0, 80, 81)).mock(1000)
+
+# submit to emulator would take too many resources
