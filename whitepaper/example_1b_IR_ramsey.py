06554fe6bc1a4cf722662349170da06e4783f90c
John Long <johnzl@protonmail.ch>
2023-06-21 17:44:44 -0400

    got program to work but spits warning about durations, previous implementation refuses to run over misaligned time points

--- whitepaper/example_1b_IR_ramsey.py
+++ whitepaper/example_1b_IR_ramsey.py
@@ -1,6 +1,5 @@
-from bloqade.ir.scalar import Variable
-from bloqade.ir.waveform import Linear, Constant
-from bloqade import start
+from bloqade.ir import Linear, Constant
+from bloqade import start, var
 import numpy as np
 
 plateau_time = (np.pi / 2 - 0.625) / 12.5
@@ -12,24 +11,31 @@ pi_over_two_pulse = (
     .append(Linear(start=12.5, stop=12.5, duration=plateau_time))
     .append(Linear(start=12.5, stop=0.0, duration=0.5))
 )
-
 rabi_pulse = pi_over_two_pulse.append(
-    Constant(value=0, duration=Variable("t_run"))
+    Constant(value=0, duration="t_run")
 ).append(pi_over_two_pulse)
 
 
 ramsey_program = (
     start.add_position([0, 0])
     .rydberg.rabi.amplitude.uniform.apply(rabi_pulse)
-    .detuning.uniform.constant(
-        value=10.5,
-        # having to manually remember and sum the times versus a variable is not
-        # very pleasant
-        duration=Variable("t_run") + 2 * np.sum([0.5, plateau_time, 0.5]),
+    .detuning.uniform.piecewise_constant(
+        durations = [0.5, plateau_time, 0.5, "t_run", 0.5, plateau_time, 0.5],
+        values = [10.5] * 7,
     )
 )
 
+ramsey_job = (
+    ramsey_program
+    .batch_assign(t_run=np.around(np.arange(0, 30, 1) * 0.1, 13))
+    .braket_local_simulator(10000)
+    .submit()
+    .report()
+    .rydberg_densities()
+)
+
 # does not suffer the same problem as using the plain builder syntax for waveform
-ramsey_program.parallelize(24).batch_assign(
-    t_run=np.around(np.arange(0, 30, 1) * 0.1, 13)
-).mock(100)
+# run on HW
+#ramsey_program.parallelize(24).batch_assign(
+#    t_run=np.around(np.arange(0, 30, 1) * 0.1, 13)
+#).mock(100)
