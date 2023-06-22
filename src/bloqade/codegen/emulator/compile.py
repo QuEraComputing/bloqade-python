4d5982292156b601fdab591890b393e06a56edc4
Phillip Weinberg <weinbe58@gmail.com>
2023-06-22 15:05:08 -0400

    implementing IR compiler to emulator.

--- src/bloqade/codegen/emulator/compile.py
+++ src/bloqade/codegen/emulator/compile.py
@@ -6,10 +6,12 @@ import bloqade.ir.control.sequence as sequence
 import bloqade.ir.control.pulse as pulse
 import bloqade.ir.control.field as field
 import bloqade.ir.control.waveform as waveform
+import bloqade.ir as ir
+from bloqade.ir.location.base import SiteFilling
 
 from pydantic.dataclasses import dataclass
-from numpy.typing import NDArray
-from typing import TYPE_CHECKING, Any, Dict, Callable
+from numpy.typing import NDArray, List, Tuple, Callable
+from typing import TYPE_CHECKING, Any, Dict, Optional
 from numbers import Number
 
 
@@ -17,16 +19,24 @@ if TYPE_CHECKING:
     from bloqade.ir.location.base import AtomArrangement
 
 
-@dataclass(frozen=True)
-class CouplingSpatialModulation:
-    field_name: field.FieldName
-    level_coupling: sequence.LevelCoupling
-    spatial_modulation: field.SpatialModulation
+@dataclass
+class RabiDrive:
+    amplitude: Dict[field.SpatialModulation, Callable[float, float]] = {}
+    phase: Dict[field.SpatialModulation, Callable[float, float]] = {}
+
+
+@dataclass
+class LaserCoupling:
+    detuning: Dict[field.SpatialModulation, Callable[float, float]] = {}
+    rabi: RabiDrive = RabiDrive()
 
 
 @dataclass
 class EmulatorProgram:
-    terms: Dict[CouplingSpatialModulation, Callable]
+    space: Space
+    register: List[Tuple[float, float]]
+    rydberg: Optional[LaserCoupling]
+    hyperfine: Optional[LaserCoupling]
 
 
 class WaveformCompiler(WaveformVisitor):
@@ -45,18 +55,35 @@ class Emulate(ProgramVisitor):
         self.assignments = assignments
         self.psi = psi
         self.space = space
-        self.terms = {}
+        self.current_coupling = None
+        self.current_field = None
+        self.register = None
+        self.rydberg = None
+        self.hyperfine = None
+
+    def visit_program(self, ast: ir.Program):
+        self.visit(ast.register)
+        self.visit(ast.sequence)
 
     def visit_atom_arrangement(self, ast: AtomArrangement):
-        # generate rydberg interaction.
-        pass
+        self.register = [
+            loc_info.position
+            for loc_info in ast.enumerate()
+            if loc_info.filling == SiteFilling.filled
+        ]
 
     def visit_sequence(self, ast: sequence.SequenceExpr):
         match ast:
             case sequence.Sequence(pulses):
                 for level_coupling, sub_pulse in pulses.items():
-                    self.level_coupling = level_coupling
+                    self.current_coupling = LaserCoupling()
                     self.visit(sub_pulse)
+
+                    match level_coupling:
+                        case sequence.rydberg:
+                            self.rydberg = self.current_coupling
+                        case sequence.hyperfine:
+                            self.hyperfine = self.current_coupling
 
             case sequence.NamedSequence(sub_sequence, _):
                 self.visit(sub_sequence)
@@ -68,7 +95,14 @@ class Emulate(ProgramVisitor):
         match ast:
             case pulse.Pulse(fields):
                 for field_name, sub_field in fields.items():
-                    self.field_name = field_name
+                    match field_name:
+                        case pulse.rabi.amplitude:
+                            self.current_field = self.current_coupling.rabi.amplitude
+                        case pulse.rabi.phase:
+                            self.current_field = self.current_coupling.rabi.phase
+                        case pulse.detuning:
+                            self.current_field = self.current_coupling.detuning
+
                     self.visit(sub_field)
 
             case pulse.NamedPulse(sub_pulse, _):
@@ -78,11 +112,12 @@ class Emulate(ProgramVisitor):
                 raise NotImplementedError
 
     def visit_field(self, ast: Field) -> Any:
-        for spatial_modulation, sub_waveform in ast.value.items():
-            compiled_waveform = WaveformCompiler(self.assignments).emit(sub_waveform)
+        for sp_mod, wf in ast.value.items():
+            self.current_field[sp_mod] = WaveformCompiler(self.assignments).emit(wf)
 
-            key = CouplingSpatialModulation(
-                self.field_name, self.level_coupling, spatial_modulation
-            )
+    def emit(self, program: ir.Profram) -> EmulatorProgram:
+        self.visit(program)
 
-            self.terms[key] = compiled_waveform
+        return EmulatorProgram(
+            register=self.register, rydberg=self.rydberg, hyperfine=self.hyperfine
+        )
