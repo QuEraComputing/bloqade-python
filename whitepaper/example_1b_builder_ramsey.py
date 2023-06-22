6d7e7a6bf9e2b63e774b056a16bd15c2a7551a50
John Long <johnzl@protonmail.ch>
2023-06-22 13:29:59 -0400

    more programmatic way of providing piecewise_linear values, even though piecewise constant would be nice here

--- whitepaper/example_1b_builder_ramsey.py
+++ whitepaper/example_1b_builder_ramsey.py
@@ -2,21 +2,14 @@ from bloqade import start
 import numpy as np
 
 plateau_time = (np.pi / 2 - 0.625) / 12.5
-pi_over_two_durations = [0.05, plateau_time, 0.05]
-pi_over_two_values = [0.0, 12.5, 12.5, 0.0]
+wf_durations = [0.05, plateau_time, 0.05, "t_run", 0.05, plateau_time, 0.05]
+rabi_wf_values = [0.0, 12.5, 12.5, 0.0] * 2  # repeat values twice
 
 
 ramsey_program = (
     start.add_position([0, 0])
-    .rydberg.rabi.amplitude.uniform.piecewise_linear(
-        durations=pi_over_two_durations, values=pi_over_two_values
-    )
-    .constant(value=0, duration="t_run")
-    .piecewise_linear(durations=pi_over_two_durations, values=pi_over_two_values)
-    .detuning.uniform.piecewise_constant(
-        durations=pi_over_two_durations + ["t_run"] + pi_over_two_durations,
-        values=[10.5] * 7,
-    )
+    .rydberg.rabi.amplitude.uniform.piecewise_linear(wf_durations, rabi_wf_values)
+    .detuning.uniform.piecewise_linear(wf_durations, [10.5] * (len(wf_durations) + 1))
 )
 
 # run on local emulator
