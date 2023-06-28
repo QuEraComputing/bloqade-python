f5abe7610bfb70147ad2c633161f7634ea8ed27f
John Long <johnzl@protonmail.ch>
2023-06-28 14:48:16 -0400

    now runnable as standalone script, remove HW pipeline due to bug

--- whitepaper/example_1a_rabi.py
+++ whitepaper/example_1a_rabi.py
@@ -3,8 +3,6 @@ from bloqade import start
 import numpy as np
 from bokeh.plotting import figure, show
 
-import os
-
 durations = ["ramp_time", "run_time", "ramp_time"]
 
 rabi_oscillations_program = (
@@ -20,19 +18,6 @@ rabi_oscillations_program = (
 rabi_oscillation_job = rabi_oscillations_program.assign(
     ramp_time=0.06, rabi_value=15, detuning_value=0.0
 ).batch_assign(run_time=np.around(np.arange(0, 21, 1) * 0.05, 13))
-
-# contains 21 tasks as consequence of batch_assign
-# currently not exactly reproducible over run_time = 0 issue and
-# non-monotonically increasing durations that Braket validation expects
-
-# HW results
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
-os.environ["AWS_ACCESS_KEY_ID"] = ""
-os.environ["AWS_SECRET_ACCESS_KEY"] = ""
-os.environ["AWS_SESSION_TOKEN"] = ""
-
-# Can be saved as JSON and later reloaded when results are available
-hw_job = rabi_oscillation_job.parallelize(24).braket(100).submit()
 
 # Simulation Results
 emu_job = rabi_oscillation_job.braket_local_simulator(10000).submit().report()
