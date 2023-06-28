704794ad6e34b31a1a5094924496523b9c4fb858
Phillip Weinberg <weinbe58@gmail.com>
2023-06-28 11:33:03 -0400

    Adding `apply` method to `ProgramStart` builder.

--- src/bloqade/builder/start.py
+++ src/bloqade/builder/start.py
@@ -1,5 +1,10 @@
 from .base import Builder
 from .coupling import Rydberg, Hyperfine
+import bloqade.ir.control.sequence as SequenceExpr
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .emit import Emit
 
 
 class ProgramStart(Builder):
@@ -10,3 +15,8 @@ class ProgramStart(Builder):
     @property
     def hyperfine(self):
         return Hyperfine(self)
+
+    def apply(self, sequence: SequenceExpr) -> "Emit":
+        from .emit import Emit
+
+        return Emit(self, register=self.register, sequence=sequence)
