e340d12de4bab5207f4ca5ab48195863b87dc0e8
John Long <johnzl@protonmail.ch>
2023-06-28 12:40:29 -0400

    Added attempt to submit to hardware

--- whitepaper/example_1a_rabi.py
+++ whitepaper/example_1a_rabi.py
@@ -1,5 +1,9 @@
-import numpy as np
 from bloqade import start
+
+import numpy as np
+from bokeh.plotting import figure, show
+
+import os
 
 durations = ["ramp_time", "run_time", "ramp_time"]
 
@@ -13,20 +17,45 @@ rabi_oscillations_program = (
     )
 )
 
-
-# for submitting to HW
-# rabi_oscillations_program.parallelize(24).assign(
-#    ramp_time=0.06, rabi_value=15, detuning_value=0.0)
-# .batch_assign(run_time=np.around(np.arange(0, 21, 1) * 0.05, 13)).mock(10, "test.txt")
-
-# for simulation
-emulation_job = (
-    rabi_oscillations_program.assign(ramp_time=0.06, rabi_value=15, detuning_value=0.0)
-    .batch_assign(run_time=np.around(np.arange(0, 21, 1) * 0.05, 13))
-    .braket_local_simulator(10000)  # 10000 shots from whitepaper example
-)
+rabi_oscillation_job = rabi_oscillations_program.assign(
+    ramp_time=0.06, rabi_value=15, detuning_value=0.0
+).batch_assign(run_time=np.around(np.arange(0, 21, 1) * 0.05, 13))
 
 # contains 21 tasks as consequence of batch_assign
 # currently not exactly reproducible over run_time = 0 issue and
 # non-monotonically increasing durations that Braket validation expects
-emulation_job.submit().report().rydberg_densities()
+
+# HW results
+os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+os.environ["AWS_ACCESS_KEY_ID"] = ""
+os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+os.environ["AWS_SESSION_TOKEN"] = ""
+
+# Can be saved as JSON and later reloaded when results are available
+hw_job = rabi_oscillation_job.parallelize(24).braket(100).submit()
+
+# Simulation Results
+emu_job = rabi_oscillation_job.braket_local_simulator(10000).submit().report()
+
+p = figure(
+    x_axis_label="Time (us)",
+    y_axis_label="Rydberg Density",
+    toolbar_location=None,
+    tools="",
+)
+
+p.axis.axis_label_text_font_size = "15pt"
+p.axis.major_label_text_font_size = "10pt"
+
+p.line(
+    np.around(np.arange(0, 21, 1) * 0.05, 13),
+    emu_job.rydberg_densities()[0].to_list(),
+    line_width=2,
+)
+p.cross(
+    np.around(np.arange(0, 21, 1) * 0.05, 13),
+    emu_job.rydberg_densities()[0].to_list(),
+    size=20,
+)
+
+show(p)
