b5a3c5b177ad40ae42e1b5275ca488e1fa7773c8
John Long <johnzl@protonmail.ch>
2023-06-28 18:33:30 -0400

    LP gate plots added, take advantage of .apply for sequences

--- whitepaper/example-2-lp-gate.py
+++ whitepaper/example-2-lp-gate.py
@@ -1,5 +1,8 @@
 from bloqade import start
+
 import numpy as np
+from bokeh.layouts import row
+from bokeh.plotting import figure, show
 
 delta = 0.377371  # Detuning
 xi = 3.90242  # Phase jump
@@ -32,33 +35,75 @@ rabi_wf_values = [
     0.0,
 ]
 
-lp_gate_program = (
-    start.add_position((0, 0))
-    .rydberg.rabi.amplitude.uniform.piecewise_linear(durations, rabi_wf_values)
+lp_gate_sequence = (
+    start.rydberg.rabi.amplitude.uniform.piecewise_linear(durations, rabi_wf_values)
     .slice(0, "t_run")
     .detuning.uniform.constant(detuning_value, sum(durations))
     .slice(0, "t_run")
     .phase.uniform.piecewise_constant(durations, [0.0] * 4 + [xi] * 3)
     .slice(0, "t_run")
+    .sequence
 )
+
+run_times = np.arange(min_time_step, sum(durations), min_time_step)
+
+# create a one atom and two atom example
+atom_positions = [[(0, 0)], [(0, 0), (0, 4.0)]]
+
+lp_gate_programs = []
+for atom_position in atom_positions:
+    lp_gate_programs.append(start.add_positions(atom_position).apply(lp_gate_sequence))
+
+lp_gate_jobs = []
+for lp_gate_program in lp_gate_programs:
+    lp_gate_jobs.append(
+        lp_gate_program.batch_assign(
+            # not happy starting from t_run = 0.0, have to offset a little
+            t_run=run_times
+        )
+    )
 
 # submit to emulator
-lp_gate_job = (
-    lp_gate_program.batch_assign(
-        t_run=np.arange(min_time_step, sum(durations), min_time_step)
-    )  # not happy starting from t_run = 0.0, have to offset a little
-    .braket_local_simulator(10000)
-    .submit()
-    .report()
-    .rydberg_densities()
+emu_jobs = []
+for lp_gate_job in lp_gate_jobs:
+    emu_jobs.append(lp_gate_job.braket_local_simulator(10000).submit().report())
+
+# plot results
+single_atom_lp = figure(
+    title="Single Atom LP Gate",
+    x_axis_label="Time (us)",
+    y_axis_label="Rydberg Density",
+    tools="",
+    toolbar_location=None,
 )
 
-# can plot in terminal via plotext
-# for one atom, remove the second call to plt.plot
-"""
-import plotext as plt
-plt.plot_size(100, 50)
-plt.plot(lp_gate_job.iloc[:,0], color = "red", label = "Atom 1 Density")
-plt.plot(lp_gate_job.iloc[:,1], color = "green", label = "Atom 2 Density")
-plt.show()
-"""
+single_atom_lp.axis.axis_label_text_font_size = "15pt"
+single_atom_lp.axis.major_label_text_font_size = "10pt"
+
+single_atom_lp.line(run_times, emu_jobs[0].rydberg_densities()[0], line_width=2)
+
+dual_atom_lp = figure(
+    title="Dual Atom LP Gate",
+    x_axis_label="Time (us)",
+    y_axis_label="Rydberg Density",
+    tools="",
+    toolbar_location=None,
+)
+
+dual_atom_lp.axis.axis_label_text_font_size = "15pt"
+dual_atom_lp.axis.major_label_text_font_size = "10pt"
+
+dual_atom_lp.line(
+    run_times, emu_jobs[1].rydberg_densities()[0], line_width=2, legend_label="Atom 1"
+)
+dual_atom_lp.line(
+    run_times,
+    emu_jobs[1].rydberg_densities()[1],
+    line_width=2,
+    color="red",
+    legend_label="Atom 2",
+)
+
+p = row(single_atom_lp, dual_atom_lp)
+
+show(p)
