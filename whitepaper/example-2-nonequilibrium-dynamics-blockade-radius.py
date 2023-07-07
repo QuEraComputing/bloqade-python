b110108efc72b6a0a8b9ab414286bcaaa54e6bec
John Long <johnzl@protonmail.ch>
2023-07-07 18:30:43 -0400

    dual ground state plot complete, extract bitstring counts via function

--- whitepaper/example-2-nonequilibrium-dynamics-blockade-radius.py
+++ whitepaper/example-2-nonequilibrium-dynamics-blockade-radius.py
@@ -1,8 +1,11 @@
 from bloqade import start, cast
+from bloqade.task import HardwareFuture
 
 import numpy as np
+
 from bokeh.layouts import row
 from bokeh.plotting import show, figure
+from bokeh.models import HoverTool, CrosshairTool, ColumnDataSource
 
 durations = ["ramp_time", "t_run", "ramp_time"]
 
@@ -27,40 +30,108 @@ emu_job = (
 )
 
 # submit to HW
+"""
 hw_job = (
     noneq_dynamics_blockade_radius_job.parallelize(24)
     .braket(100)
     .submit()
     .save_json("example-2-nonequilibrium-dynamics-blockade-radius-job.json")
 )
+"""
+# get HW results back
+hw_future = HardwareFuture()
+hw_future.load_json("example-2-nonequilibrium-dynamics-blockade-radius-job.json")
+hw_report = hw_future.report()
 
-bitstring_dicts = emu_job.counts
-zero_rydberg_probs = []  # "00"
-one_rydberg_probs = []  # "01" or "10"
-two_rydberg_probs = []  # "11"
 
-for bitstring_dict in bitstring_dicts:
-    zero_rydberg_probs.append(bitstring_dict.get("11", 0) / n_shots)
-    one_rydberg_probs.append(
-        (bitstring_dict.get("01", 0) + bitstring_dict.get("10", 0)) / n_shots
-    )
-    two_rydberg_probs.append(bitstring_dict.get("00", 0) / n_shots)
+def generate_probabilities(report):
+    zero_rydberg_probs = []  # "00"
+    one_rydberg_probs = []  # "01" or "10"
+    two_rydberg_probs = []  # "11"
+
+    for bitstring_counts in report.counts:
+        zero_rydberg_probs.append(bitstring_counts.get("11", 0) / n_shots)
+        one_rydberg_probs.append(
+            (bitstring_counts.get("01", 0) + bitstring_counts.get("10", 0)) / n_shots
+        )
+        two_rydberg_probs.append(bitstring_counts.get("00", 0) / n_shots)
+
+    return zero_rydberg_probs, one_rydberg_probs, two_rydberg_probs
+
+
+# get probabilities of 0, 1, and 2 atoms in Rydberg state for emulator
+emu_zero_probs, emu_one_probs, emu_two_probs = generate_probabilities(emu_job)
+
+# get probabilities of 0, 1, and 2 atoms in Rydberg state for HW
+hw_zero_probs, hw_one_probs, hw_two_probs = generate_probabilities(hw_report)
 
 # make three separate plots
 
 # "00" density
+gg_data = {
+    "times": run_times,
+    "emu_densities": emu_zero_probs,
+    "hw_densities": emu_one_probs,
+}
+gg_source = ColumnDataSource(data=gg_data)
+
 gg_density = figure(
     title="0 Rydberg",
-    x_axis_label="Time (us)",
+    x_axis_label="Time (μs)",
     y_axis_label="Rydberg Density",
-    tools="",
+    tools="pan,wheel_zoom,box_zoom,reset,save",
     toolbar_location=None,
 )
 
 gg_density.axis.axis_label_text_font_size = "15pt"
 gg_density.axis.major_label_text_font_size = "10pt"
 
-gg_density.line(run_times, zero_rydberg_probs, line_width=2, color="green")
+emu_line = gg_density.line(
+    x="times",
+    y="emu_densities",
+    source=gg_source,
+    label="Emulator",
+    color="grey",
+    line_width=2,
+)
+gg_density.circle(x="times", y="emu_densities", source=gg_source, color="grey", size=8)
+hw_line = gg_density.line(
+    x="times",
+    y="hw_densities",
+    source=gg_source,
+    label="Hardware",
+    color="purple",
+    line_width=2,
+)
+gg_density.circle(x="times", y="hw_densities", source=gg_source, color="purple", size=8)
+
+hw_hover_tool = HoverTool(
+    renderers=[hw_line],
+    tooltips=[
+        ("Backend", "Hardware"),
+        ("Density", "@hw_densities"),
+        ("Time", "@times μs"),
+    ],
+    mode="vline",
+    attachment="right",
+)
+gg_density.add_tools(hw_hover_tool)
+emu_hover_tool = HoverTool(
+    renderers=[emu_line],
+    tooltips=[
+        ("Backend", "Emulator"),
+        ("Density", "@emu_densities"),
+        ("Time", "@times μs"),
+    ],
+    mode="vline",
+    attachment="left",
+)
+
+gg_density.add_tools(emu_hover_tool)
+cross_hair_tool = CrosshairTool(dimensions="height")
+gg_density.add_tools(cross_hair_tool)
+
+show(gg_density)
 
 
 gr_rg_density = figure(
@@ -74,7 +145,7 @@ gr_rg_density = figure(
 gr_rg_density.axis.axis_label_text_font_size = "15pt"
 gr_rg_density.axis.major_label_text_font_size = "10pt"
 
-gr_rg_density.line(run_times, one_rydberg_probs, line_width=2, color="blue")
+gr_rg_density.line(run_times, emu_one_probs, line_width=2, color="blue")
 
 
 rr_density = figure(
@@ -88,7 +159,7 @@ rr_density = figure(
 rr_density.axis.axis_label_text_font_size = "15pt"
 rr_density.axis.major_label_text_font_size = "10pt"
 
-rr_density.line(run_times, two_rydberg_probs, line_width=2, color="red")
+rr_density.line(run_times, emu_two_probs, line_width=2, color="red")
 
 p = row(gg_density, gr_rg_density, rr_density)
 
