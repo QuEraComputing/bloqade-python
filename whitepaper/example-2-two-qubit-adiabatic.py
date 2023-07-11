025dd3064b34c44d9d50eecaf958f487c4429e5c
John Long <johnzl@protonmail.ch>
2023-07-11 17:35:20 -0400

    Added finalized bokeh plots

--- whitepaper/example-2-two-qubit-adiabatic.py
+++ whitepaper/example-2-two-qubit-adiabatic.py
@@ -1,7 +1,9 @@
 from bloqade import start
+from bloqade.task import HardwareFuture
 
 import numpy as np
 from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource, CrosshairTool, HoverTool
 
 durations = [1, 2, 1]
 
@@ -16,64 +18,126 @@ two_qubit_adiabatic_job = two_qubit_adiabatic_program.batch_assign(
     distance=np.around(np.arange(4, 11, 1), 13)
 )
 
-n_shots = 10000
+# submit to local emulator
+emu_job = two_qubit_adiabatic_job.braket_local_simulator(10000).submit().report()
 
-emu_job = two_qubit_adiabatic_job.braket_local_simulator(n_shots).submit().report()
-
-hw_job = (
+# submit to HW
+"""
+(
     two_qubit_adiabatic_job.parallelize(24)
     .braket(100)
     .submit()
     .save_json("example-2-two-qubit-adiabatic-job.json")
 )
+"""
+
+# retrieve data from HW
+hw_future = HardwareFuture()
+hw_future.load_json("example-2-two-qubit-adiabatic-job.json")
+hw_report = hw_future.report()
 
 # want to plot the 0 rydberg probability,
 # 1 rydberg probability,
 # and 2 rydberg probabilities
 
-bitstring_dicts = emu_job.counts
-zero_rydberg_probs = []  # "00"
-one_rydberg_probs = []  # "01" or "10"
-two_rydberg_probs = []  # "11"
 
-for bitstring_dict in bitstring_dicts:
-    zero_rydberg_probs.append(bitstring_dict.get("11", 0) / n_shots)
-    one_rydberg_probs.append(
-        (bitstring_dict.get("01", 0) + bitstring_dict.get("10", 0)) / n_shots
-    )
-    two_rydberg_probs.append(bitstring_dict.get("00", 0) / n_shots)
+def get_rydberg_probs(report):
+    bitstring_dicts = report.counts
+    zero_rydberg_probs = []  # "00"
+    one_rydberg_probs = []  # "01" or "10"
+    two_rydberg_probs = []  # "11"
+
+    for bitstring_dict in bitstring_dicts:
+        successful_presort_shots = sum(bitstring_dict.values())
+        zero_rydberg_probs.append(
+            bitstring_dict.get("11", 0) / successful_presort_shots
+        )  # n_shots needs to be dynamically calculated
+        one_rydberg_probs.append(
+            (bitstring_dict.get("01", 0) + bitstring_dict.get("10", 0))
+            / successful_presort_shots
+        )
+        two_rydberg_probs.append(bitstring_dict.get("00", 0) / successful_presort_shots)
+
+    return zero_rydberg_probs, one_rydberg_probs, two_rydberg_probs
+
+
+# format data properly
+emu_data = {
+    "distance": [np.around(np.arange(4, 11, 1), 13)] * 3,
+    "probs": [*get_rydberg_probs(emu_job)],
+}
+emu_source = ColumnDataSource(data=emu_data)
+
+hw_zero_rydberg_probs, hw_one_rydberg_probs, hw_two_rydberg_probs = get_rydberg_probs(
+    hw_report
+)
+hw_data = {
+    "distance": np.around(np.arange(4, 11, 1), 13),
+    "gg_probs": hw_zero_rydberg_probs,
+    "gr_rg_probs": hw_one_rydberg_probs,
+    "rr_probs": hw_two_rydberg_probs,
+}
+hw_source = ColumnDataSource(data=hw_data)
 
 # plot the results
-p = figure(
-    x_axis_label="Distance (um)",
+rydberg_probs_plt = figure(
+    x_axis_label="Distance (μm)",
     y_axis_label="Probability",
-    tools="",
-    toolbar_location=None,
+    toolbar_location="right",
+    tools=["pan,wheel_zoom,box_zoom,reset,save"],
 )
 
-p.axis.axis_label_text_font_size = "15pt"
-p.axis.major_label_text_font_size = "10pt"
+rydberg_probs_plt.axis.axis_label_text_font_size = "15pt"
+rydberg_probs_plt.axis.major_label_text_font_size = "10pt"
 
-p.line(
-    np.arange(4, 11, 1),
-    zero_rydberg_probs,
-    legend_label="0 Rydberg",
-    line_color="green",
+# multiline for emulator
+rydberg_probs_plt.multi_line(
+    xs="distance",
+    ys="probs",
+    source=emu_source,
     line_width=2,
-)
-p.line(
-    np.arange(4, 11, 1),
-    one_rydberg_probs,
-    legend_label="1 Rydberg",
-    line_color="yellow",
-    line_width=2,
-)
-p.line(
-    np.arange(4, 11, 1),
-    two_rydberg_probs,
-    legend_label="2 Rydberg",
-    line_color="red",
-    line_width=2,
+    color="grey",
+    legend_label="Emulator",
 )
 
-show(p)
+# combination of markers and lines for other plots
+# zero rydberg atom case
+hw_data_keys = ["gg_probs", "gr_rg_probs", "rr_probs"]
+legend_labels = ["0 Rydberg", "1 Rydberg", "2 Rydberg"]
+line_colors = ["green", "yellow", "red"]
+
+hw_lines = []
+for data_key, legend_label, line_color in zip(hw_data_keys, legend_labels, line_colors):
+    hw_lines.append(
+        rydberg_probs_plt.line(
+            x="distance",
+            y=data_key,
+            source=hw_source,
+            legend_label=legend_label,
+            color=line_color,
+            line_width=2,
+        )
+    )
+
+    rydberg_probs_plt.circle(
+        x="distance", y=data_key, source=hw_source, color=line_color, size=8
+    )
+
+# add hover tools
+for hw_line, legend_label, hw_data_key in zip(hw_lines, legend_labels, hw_data_keys):
+    rydberg_probs_plt.add_tools(
+        HoverTool(
+            renderers=[hw_line],
+            tooltips=[
+                ("Probability of", legend_label),
+                ("Distance", "@distance μm"),
+                ("Probability", "@" + hw_data_key),
+            ],
+            mode="vline",
+        )
+    )
+
+cross_hair_tool = CrosshairTool(dimensions="height")
+rydberg_probs_plt.add_tools(cross_hair_tool)
+
+show(rydberg_probs_plt)
