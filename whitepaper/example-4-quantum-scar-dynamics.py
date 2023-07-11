ccaeb7273aee0b1707ed417b5dfcc54a45920a1e
John Long <johnzl@protonmail.ch>
2023-07-11 14:55:45 -0400

    removed multiprocessing, added remove_invalid_tasks call to avoid problems with slope being too steep

--- whitepaper/example-4-quantum-scar-dynamics.py
+++ whitepaper/example-4-quantum-scar-dynamics.py
@@ -39,96 +39,91 @@ quantum_scar_job = quantum_scar_program.batch_assign(run_time=run_times)
 n_shots = 10000
 # Run on emulator, taking advantage of multiprocessing
 
-if __name__ == "__main__":
-    print("Submitting Jobs with Multiprocessing Enabled...")
 
-    emu_job = (
-        quantum_scar_job.braket_local_simulator(n_shots)
-        .submit(multiprocessing=True)
-        .report()
-    )
+emu_job = (
+    quantum_scar_job.braket_local_simulator(n_shots)
+    .submit(multiprocessing=True)
+    .report()
+)
 
-    hw_job = (
-        quantum_scar_job.parallelize(24)
-        .braket(100)
-        .submit()
-        .save_json("example-4-quantum-scar-dynamics-job.json")
-    )
+hw_job = (
+    quantum_scar_job.parallelize(24)
+    .braket(100)
+    .remove_invalid_tasks()
+    .submit()
+    .save_json("example-4-quantum-scar-dynamics-job.json")
+)
 
-    print("Plotting Results...")
+# Plot results
+## Mean of Rydberg Densities
+mean_rydberg_densities_plot = figure(
+    title="Mean Rydberg Densities",
+    x_axis_label="Run Time (us)",
+    y_axis_label="Mean Rydberg Density",
+    tools="",
+    toolbar_location=None,
+)
 
-    # Plot results
-    ## Mean of Rydberg Densities
-    mean_rydberg_densities_plot = figure(
-        title="Mean Rydberg Densities",
-        x_axis_label="Run Time (us)",
-        y_axis_label="Mean Rydberg Density",
-        tools="",
-        toolbar_location=None,
-    )
+mean_rydberg_densities_plot.axis.axis_label_text_font_size = "15pt"
+mean_rydberg_densities_plot.axis.major_label_text_font_size = "10pt"
 
-    mean_rydberg_densities_plot.axis.axis_label_text_font_size = "15pt"
-    mean_rydberg_densities_plot.axis.major_label_text_font_size = "10pt"
+mean_rydberg_densities_plot.line(
+    run_times, emu_job.rydberg_densities().mean(axis=1), line_width=2
+)
 
-    mean_rydberg_densities_plot.line(
-        run_times, emu_job.rydberg_densities().mean(axis=1), line_width=2
-    )
+## Probability of Ground State
 
-    ## Probability of Ground State
+ground_state_probabilities = []
+ground_state = "01" * 5 + "0"
 
-    ground_state_probabilities = []
-    ground_state = "01" * 5 + "0"
+for bitstring_counts in emu_job.counts:
+    ground_state_probabilities.append(bitstring_counts.get(ground_state, 0) / n_shots)
 
-    for bitstring_counts in emu_job.counts:
-        ground_state_probabilities.append(
-            bitstring_counts.get(ground_state, 0) / n_shots
-        )
+ground_state_plot = figure(
+    title="Probability of Ground State",
+    x_axis_label="Run Time (us)",
+    y_axis_label="Probability of Ground State",
+    tools="",
+    toolbar_location=None,
+)
 
-    ground_state_plot = figure(
-        title="Probability of Ground State",
-        x_axis_label="Run Time (us)",
-        y_axis_label="Probability of Ground State",
-        tools="",
-        toolbar_location=None,
-    )
+ground_state_plot.line(run_times, ground_state_probabilities, line_width=2)
 
-    ground_state_plot.line(run_times, ground_state_probabilities, line_width=2)
+ground_state_plot.axis.axis_label_text_font_size = "15pt"
+ground_state_plot.axis.major_label_text_font_size = "10pt"
 
-    ground_state_plot.axis.axis_label_text_font_size = "15pt"
-    ground_state_plot.axis.major_label_text_font_size = "10pt"
+## Density Plot over Time w/ Interpolation
 
-    ## Density Plot over Time w/ Interpolation
+density_over_time_plot = figure(
+    title="Density Over Time",
+    x_axis_label="Time (us)",
+    y_axis_label="Site",
+    tools="",
+    toolbar_location=None,
+)
 
-    density_over_time_plot = figure(
-        title="Density Over Time",
-        x_axis_label="Time (us)",
-        y_axis_label="Site",
-        tools="",
-        toolbar_location=None,
-    )
+density_over_time_plot.axis.axis_label_text_font_size = "15pt"
+density_over_time_plot.axis.major_label_text_font_size = "10pt"
 
-    density_over_time_plot.axis.axis_label_text_font_size = "15pt"
-    density_over_time_plot.axis.major_label_text_font_size = "10pt"
+density_over_time_plot.x_range.start = 0
+density_over_time_plot.y_range.start = 0
 
-    density_over_time_plot.x_range.start = 0
-    density_over_time_plot.y_range.start = 0
+densities_array = emu_job.rydberg_densities().to_numpy().transpose()
 
-    densities_array = emu_job.rydberg_densities().to_numpy().transpose()
+color_mapping = LinearColorMapper(palette="Viridis11", low=0, high=1)
+color_bar = ColorBar(color_mapper=color_mapping)
 
-    color_mapping = LinearColorMapper(palette="Viridis11", low=0, high=1)
-    color_bar = ColorBar(color_mapper=color_mapping)
+density_over_time_plot.image(
+    image=[emu_job.rydberg_densities().to_numpy().transpose()],
+    x=0,
+    y=0,
+    dw=densities_array.shape[1],
+    dh=densities_array.shape[0],
+    color_mapper=color_mapping,
+)
 
-    density_over_time_plot.image(
-        image=[emu_job.rydberg_densities().to_numpy().transpose()],
-        x=0,
-        y=0,
-        dw=densities_array.shape[1],
-        dh=densities_array.shape[0],
-        color_mapper=color_mapping,
-    )
+density_over_time_plot.add_layout(color_bar, "right")
 
-    density_over_time_plot.add_layout(color_bar, "right")
+p = row(mean_rydberg_densities_plot, ground_state_plot, density_over_time_plot)
 
-    p = row(mean_rydberg_densities_plot, ground_state_plot, density_over_time_plot)
-
-    show(p)
+show(p)
