2d6bb2b2490070ad5b805a44f4d848afb4232d3b
Kai-Hsin Wu <khwu@KHWus-MacBook-Pro.local>
2023-07-12 14:44:04 -0400

    add test for double parallel

--- tests/test_parallelize.py
+++ tests/test_parallelize.py
@@ -1,31 +1,53 @@
+import pytest
 from bloqade.ir.location import Square
 
 # create lattice
-lattice = Square(3, lattice_spacing=6)
+def test_parallel_task():
+    lattice = Square(3, lattice_spacing=6)
 
-quantum_task = (
-    lattice.rydberg.detuning.uniform.piecewise_linear(
-        durations=["up_time", "anneal_time", "up_time"],
-        values=[
-            "initial_detuning",
-            "initial_detuning",
-            "final_detuning",
-            "final_detuning",
-        ],
+    quantum_task = (
+        lattice.rydberg.detuning.uniform.piecewise_linear(
+            durations=["up_time", "anneal_time", "up_time"],
+            values=[
+                "initial_detuning",
+                "initial_detuning",
+                "final_detuning",
+                "final_detuning",
+            ],
+        )
+        .rydberg.rabi.amplitude.uniform.piecewise_linear(
+            durations=["up_time", "anneal_time", "up_time"],
+            values=[0, "rabi_amplitude_max", "rabi_amplitude_max", 0],
+        )
+        .parallelize(10.0)
+        .assign(
+            initial_detuning=-10,
+            up_time=0.1,
+            final_detuning=15,
+            anneal_time=10,
+            rabi_amplitude_max=15,
+        )
+        .mock(10)
     )
-    .rydberg.rabi.amplitude.uniform.piecewise_linear(
-        durations=["up_time", "anneal_time", "up_time"],
-        values=[0, "rabi_amplitude_max", "rabi_amplitude_max", 0],
-    )
-    .parallelize(10.0)
-    .assign(
-        initial_detuning=-10,
-        up_time=0.1,
-        final_detuning=15,
-        anneal_time=10,
-        rabi_amplitude_max=15,
-    )
-    .mock(10)
-)
 
-assert quantum_task.tasks[0].parallel_decoder
+    assert quantum_task.tasks[0].parallel_decoder
+
+
+def test_error_parallel_doublecall():
+    with pytest.raises(TypeError):
+        lattice = Square(3, lattice_spacing=6)
+        prog = lattice.rydberg.detuning.uniform.piecewise_linear(
+            durations=["up_time", "anneal_time", "up_time"],
+            values=[
+                "initial_detuning",
+                "initial_detuning",
+                "final_detuning",
+                "final_detuning",
+            ],
+        )\
+        .rydberg.rabi.amplitude.uniform.piecewise_linear(
+            durations=["up_time", "anneal_time", "up_time"],
+            values=[0, "rabi_amplitude_max", "rabi_amplitude_max", 0],
+        )\
+        .parallelize(10.0)\
+        .parallelize(4.0)
