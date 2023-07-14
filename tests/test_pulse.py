491b5807311b6779c31fd7a47be4be55fabff0dc
Kai-Hsin Wu <khwu@KHWus-MacBook-Pro.local>
2023-07-14 14:11:29 -0400

    fix missing return for RabiRouter

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -1,5 +1,32 @@
 from bloqade.ir import Field, Uniform, Linear, Pulse, detuning, rabi
 
+from bloqade.ir.control.pulse import (
+    FieldName,
+    RabiFrequencyAmplitude,
+    RabiFrequencyPhase,
+)
+
+
+def test_fieldname_base():
+    x = FieldName()
+
+    assert x.children() == []
+
+
+## from top to bottom test each hierarchy classes:
+# rabi (RabiRouter) |- RabiAmplitude
+#                   |- RabiPhase
+def test_rabi_router():
+    R = rabi
+
+    assert R.print_node() == "RabiRouter"
+    assert R.__repr__() == "rabi (amplitude, phase)"
+    assert R.children() == {
+        "Amplitude": RabiFrequencyAmplitude(),
+        "Phase": RabiFrequencyPhase(),
+    }
+
+
 f = Field({Uniform: Linear(start=1.0, stop="x", duration=3.0)})
 
 print(Pulse({detuning: f}))
