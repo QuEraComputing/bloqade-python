85af957333ba11af207366b49ea038f84254eab4
Kai-Hsin Wu <kaihsinwu@gmail.com>
2023-07-18 10:56:32 -0400

    Fix Sample bugs for eval_decimal and add more tests (#300)
    * add more tests
    
    * add more integration tests for codegen
    
    * fix bug when ConstantCodeGen hit Negative wvfm node.
    
    * tmp
    
    * fix bug when slice produce duplicate time points
    
    * tmp
    
    * tmp
    
    * adding fix for Sample interpolation.
    
    * tmp
    
    * add framework for pretty print testing and fix bugs #297
    
    * fix bug in print with children
    
    * remove [html] from .coveragerc and fix bugs in print testing
    
    * add checking for default filling of global term
    
    ---------
    
    Co-authored-by: Kai-Hsin Wu <khwu@KHWus-MacBook-Pro.local>
    Co-authored-by: Phillip Weinberg <weinbe58@gmail.com>
    Co-authored-by: Kai-Hsin Wu <khwu@KHWus-MBP.hsd1.ma.comcast.net>

---
+++ tests/test_printer.py
@@ -0,0 +1,50 @@
+from IPython.lib.pretty import PrettyPrinter as PP
+from bloqade.ir import Linear, Poly
+from io import StringIO
+from bloqade.ir.tree_print import Printer
+
+
+def test_printer():
+    wf = Linear(start=0, stop=1, duration=1)
+
+    mystdout = StringIO()
+    p = PP(mystdout)
+
+    wf._repr_pretty_(p, True)
+
+    assert (
+        mystdout.getvalue()
+        == "Linear\n├─ start ⇒ Literal: 0\n├─ stop ⇒ Literal: 1\n"
+        + "└─ duration ⇒ Literal: 1"
+    )
+
+
+def test_printer_nodes_list():
+    wf = Linear(start=0, stop=1, duration=1)
+    wf2 = wf + (wf + wf)
+
+    mystdout = StringIO()
+    p = PP(mystdout)
+
+    pr = Printer(p)
+    children = wf2.children().copy()  # children is list so no print children
+    assert pr.should_print_annotation(children) is False
+
+
+def test_printer_nodes_dict():
+    wf = Poly([1, 2, 3], duration=1)
+
+    mystdout = StringIO()
+    p = PP(mystdout)
+
+    pr = Printer(p)
+    children = wf.children().copy()  # children is list so no print children
+
+    assert pr.should_print_annotation(children) is True
+    wf._repr_pretty_(p, 10)
+
+    assert (
+        mystdout.getvalue()
+        == "Poly\n├─ b ⇒ Literal: 1\n├─ t ⇒ Literal: 2\n"
+        + "├─ t^2 ⇒ Literal: 3\n└─ duration ⇒ Literal: 1"
+    )
