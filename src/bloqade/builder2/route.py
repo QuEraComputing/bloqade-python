3f9de2651c725f3ca5c9f2aea78278fbb42360b4
Roger-luo <rogerluo.rl18@gmail.com>
2023-08-04 12:20:16 -0400

    cleanup

--- src/bloqade/builder2/route.py
+++ src/bloqade/builder2/route.py
@@ -17,11 +17,3 @@ class PragmaRoute(Assignable, Parallelizable, BackendRoute):
 
 class WaveformRoute(PulseRoute, Flattenable, PragmaRoute):
     pass
-    # def flatten(self, orders: List[str]) -> "Flatten":
-    #     return Flatten(orders, self)
-
-
-# class Flatten(Parallelizable, BackendRoute):
-#     def __init__(self, orders: List[str], parent: Builder | None = None) -> None:
-#         super().__init__(parent)
-#         self._orders = orders
