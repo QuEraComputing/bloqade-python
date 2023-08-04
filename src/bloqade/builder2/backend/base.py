59cccc46fa5de7be3307e0dd841d9d06f746bf55
Roger-luo <rogerluo.rl18@gmail.com>
2023-08-04 16:21:26 -0400

    add a sketch of compile pipeline

--- src/bloqade/builder2/backend/base.py
+++ src/bloqade/builder2/backend/base.py
@@ -11,5 +11,32 @@ class LocalBackend(Backend):
 
 
 class RemoteBackend(Backend):
+    def __init__(self):
+        self._compile_cache = None
+
+    # NOTE: this is only a sketch, dont use this
+    # def __compile__(self):
+    #     if self._compile_cache:
+    #         return self._compile_cache
+    #     from ..compile import to_bloqade_ir, scan_pragma
+    #     seq = to_bloqade_ir(self) # -> sequence object
+    #     reg = scan_register(self)
+    #     self._compile_cache = (seq, reg)
+    #     return seq, reg
+
+    # def __compile_assign(self):
+    #     assign = self.scan_assign(self) # dict
+    #     [replace(self._compile_cache, assign) for each in assign]
+    #     return
+
     def submit(self):
         raise NotImplementedError
+
+
+# class QuEraAquilia(RemoteBackend):
+
+#     def submit(self):
+#         seq, reg = self.__compile__(self)
+#         target = codegen(seq)
+#         return run(target) # -> Futre
+#         # return super().submit()
