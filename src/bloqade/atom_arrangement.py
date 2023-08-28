de8b4db462b90dbbec9da471bfb0c39e3984a98b
Phillip Weinberg <weinbe58@gmail.com>
2023-08-28 15:19:31 -0400

    re-exporting objects into module just under bloqade. (#460)

---
+++ src/bloqade/atom_arrangement.py
@@ -0,0 +1,24 @@
+from bloqade.ir.location import (
+    AtomArrangement,
+    Chain,
+    Square,
+    Rectangular,
+    Honeycomb,
+    Triangular,
+    Lieb,
+    Kagome,
+    ListOfLocations,
+)
+
+
+__all__ = [
+    "AtomArrangement",
+    "Chain",
+    "Square",
+    "Rectangular",
+    "Honeycomb",
+    "Triangular",
+    "Lieb",
+    "Kagome",
+    "ListOfLocations",
+]
