8ec4552f37ef916d5389d75392a57cc5f4e1fc14
Phillip Weinberg <weinbe58@gmail.com>
2023-08-27 22:17:31 -0400

    debugging lowering from bloqade-IR to emulator IR.

--- src/bloqade/emulator/lower/bloqade_ir.py
+++ src/bloqade/emulator/lower/bloqade_ir.py
@@ -1,3 +1,4 @@
+from bloqade.codegen.common.assignment_scan import AssignmentScan
 from bloqade.ir.location.base import AtomArrangement, SiteFilling
 from bloqade.ir.visitor.program import ProgramVisitor
 from bloqade.ir.visitor.waveform import WaveformVisitor
@@ -8,6 +9,7 @@ from bloqade.ir.control.field import (
     RunTimeVector,
     UniformModulation,
 )
+import bloqade.ir.scalar as scalar
 import bloqade.ir.control.sequence as sequence
 import bloqade.ir.control.pulse as pulse
 import bloqade.ir.control.waveform as waveform
@@ -22,9 +24,121 @@ from bloqade.emulator.ir.emulator_program import (
     RabiTerm,
 )
 
-from typing import Any, Dict, Optional
+import dataclasses
+from typing import Any, Dict, Optional, Union, List
+from numbers import Number, Real
+from decimal import Decimal
 
-from numbers import Number
+ParamType = Union[Real, List[Real]]
+
+
+@dataclasses.dataclass(frozen=True)
+class WaveformScanResults:
+    module_imports: Dict[str, str] = dataclasses.field(default_factory=dict)
+    primative_exprs: Dict[str, str] = dataclasses.field(default_factory=dict)
+    primative_tokens: Dict[str, str] = dataclasses.field(default_factory=dict)
+
+
+@dataclasses.dataclass
+class WaveformScan(WaveformVisitor):
+    var_number: int = 0
+    shift: scalar.Scalar = ir.cast(0.0)
+    assignments: Dict[str, ParamType] = dataclasses.field(default_factory=dict)
+    module_imports: Dict[str, str] = dataclasses.field(default_factory=dict)
+    primative_exprs: Dict[str, str] = dataclasses.field(default_factory=dict)
+    primative_tokens: Dict[str, str] = dataclasses.field(default_factory=dict)
+
+    def gensymm(self):
+        self.var_number += 1
+        return f"var{self.var_number}"
+
+    @property
+    def t(self) -> None:
+        return str(ir.cast("t") + self.shift)
+
+    def gen_token(self, expr: str) -> None:
+        if expr not in self.primative_exprs:
+            token = self.gensymm()
+            self.primative_exprs[expr] = token
+            self.primative_tokens[token] = expr
+
+    def visit_constant(self, ast: waveform.Constant) -> None:
+        pass
+
+    def visit_linear(self, ast: waveform.Linear) -> None:
+        start = ast.start(**self.assignments)
+        stop = ast.stop(**self.assignments)
+        duration = ast.duration(**self.assignments)
+        slope = (stop - start) / duration
+        if slope == 0:
+            expr = f"{start!s}"
+        elif slope == Decimal("1"):
+            expr = f"({self.t!s} + {start!s})"
+        else:
+            expr = f"({slope!s} * {self.t!s} + {start!s})"
+        self.gen_token(expr)
+
+    def visit_poly(self, ast: waveform.Poly) -> None:
+        coeff_str_list = [
+            str(coeff(**self.assignments)) for coeff in ast.checkpoints[::-1]
+        ]
+        coeff_literal = f"[{','.join(coeff_str_list)}]"
+        expr = f"np.polyval({coeff_literal}, {self.t})"
+        self.gen_token(expr)
+
+    def visit_python_fn(self, ast: waveform.PythonFn) -> None:
+        import inspect
+
+        module_name = inspect.getmodule(ast.fn).__name__
+
+        if module_name != "__main__":
+            # if function is coming from an external module that is not in `__main__`.
+            # keep track of where function is imported from to fix any namespace issues
+            # when generating the code for this function.
+            self.module_imports[module_name] = ast.fn.__name__
+
+        expr = f"{ast.fn.__name__}({self.t})"
+        self.gen_token(expr)
+
+    def visit_negative(self, ast: waveform.Negative) -> None:
+        self.visit(ast.waveform)
+
+    def visit_scale(self, ast: waveform.Scale) -> None:
+        self.visit(ast.waveform)
+
+    def visit_add(self, ast: waveform.Add) -> None:
+        self.visit(ast.left)
+        self.visit(ast.right)
+
+    def visit_record(self, ast: waveform.Record) -> None:
+        self.visit(ast.waveform)
+
+    def visit_sample(self, ast: waveform.Sample) -> None:
+        pass
+
+    def visit_append(self, ast: waveform.Append) -> None:
+        original_shift = self.shift
+        for wf in ast.waveforms:
+            self.visit(wf)
+            self.shift += wf.duration(**self.assignments)
+        self.shift = original_shift
+
+    def visit_slice(self, ast: waveform.Slice) -> Any:
+        self.shift += ast.interval.start(**self.assignments)
+        self.visit(ast.waveform)
+        self.shift -= ast.interval.start(**self.assignments)
+
+    @staticmethod
+    def scan(ast: waveform.Waveform, **assignments) -> "WaveformScanResults":
+        assignments = AssignmentScan(assignments).emit(ast)
+        scanner = WaveformScan(assignments=assignments)
+        scanner.visit(ast)
+
+        return WaveformScanResults(
+            module_imports=scanner.module_imports,
+            primative_exprs=scanner.primative_exprs,
+            primative_tokens=scanner.primative_tokens,
+        )
 
 
 class WaveformCompiler(WaveformVisitor):
@@ -38,38 +152,76 @@ class WaveformCompiler(WaveformVisitor):
         return lambda t: ast(t, **self.assignments)
 
 
-class LowerToEmulatorProgram(ProgramVisitor):
-    def __init__(self, assignments: Dict[str, Number], blockade_radius: float = 0.0):
+class EmulatorSpace(ProgramVisitor):
+    def __init__(
+        self,
+        assignments: Dict[str, Number] = {},
+        add_hyperfine: bool = False,
+        blockade_radius: float = 0.0,
+    ):
         self.assignments = assignments
         self.blockade_radius = blockade_radius
+        self.add_hyperfine = add_hyperfine
         self.space = None
-        self.register = None
-        self.duration = None
-        self.rydberg = None
-        self.hyperfine = None
 
     def visit_program(self, ast: ir.Program):
-        self.visit(ast.register)
         self.visit(ast.sequence)
+        self.visit(ast.register)
 
     def visit_register(self, ast: AtomArrangement) -> Any:
         atom_positions = []
         for loc_info in ast.enumerate():
             if loc_info.filling == SiteFilling.filled:
-                atom_positions.append(loc_info.position)
+                positions = tuple(
+                    [pos(**self.assignments) for pos in loc_info.position]
+                )
+                atom_positions.append(positions)
 
-        if self.hyperfine is None:
-            self.space = Space(
-                atom_positions,
-                LocalHilbertSpace.TwoLevel,
-                blockade_radius=self.blockade_radius,
-            )
-        else:
-            self.space = Space(
+        if self.add_hyperfine:
+            self.space = Space.create(
                 atom_positions,
                 LocalHilbertSpace.ThreeLevel,
                 blockade_radius=self.blockade_radius,
             )
+        else:
+            self.space = Space.create(
+                atom_positions,
+                LocalHilbertSpace.TwoLevel,
+                blockade_radius=self.blockade_radius,
+            )
+
+    def visit_sequence(self, ast: sequence.SequenceExpr):
+        match ast:
+            case sequence.Sequence(pulses):
+                if sequence.hyperfine in pulses:
+                    self.hyperfine = True
+
+            case sequence.NamedSequence(sub_sequence, _):
+                self.visit(sub_sequence)
+
+            case _:
+                raise NotImplementedError
+
+    def emit(self, program: ir.Program) -> EmulatorProgram:
+        self.visit(program)
+        return self.space
+
+
+class ToEmulatorProgram(ProgramVisitor):
+    def __init__(
+        self, assignments: Dict[str, Number] = {}, blockade_radius: float = 0.0
+    ):
+        self.assignments = assignments
+        self.waveform_compiler = WaveformCompiler(assignments)
+        self.blockade_radius = blockade_radius
+        self.space = None
+        self.register = None
+        self.duration = 0.0
+        self.rydberg = None
+        self.hyperfine = None
+
+    def visit_program(self, ast: ir.Program):
+        self.visit(ast.sequence)
 
     def visit_sequence(self, ast: sequence.SequenceExpr):
         match ast:
@@ -125,7 +277,7 @@ class LowerToEmulatorProgram(ProgramVisitor):
                     terms.append(
                         DetuningTerm(
                             target_atoms=self.visit_spatial_modulation(sm),
-                            amplitude=WaveformCompiler(self.assignments).emit(wf),
+                            amplitude=self.waveform_compiler.emit(wf),
                         )
                     )
 
@@ -136,15 +288,22 @@ class LowerToEmulatorProgram(ProgramVisitor):
 
                 for atom in range(self.space.n_atoms):
                     wf = sum(
-                        target_atom_dict[sm].get(atom, 0.0) * wf
-                        for sm, wf in value.items()
+                        (
+                            target_atom_dict[sm].get(atom, 0.0) * wf
+                            for sm, wf in value.items()
+                        ),
+                        start=waveform.Constant(0.0, 0.0),
                     )
-
                     self.duration = max(
                         float(wf.duration(**self.assignments)), self.duration
                     )
 
-                    terms.append(DetuningTerm(target_atoms={atom: 1}, amplitude=wf))
+                    terms.append(
+                        DetuningTerm(
+                            target_atoms={atom: 1},
+                            amplitude=self.waveform_compiler.emit(wf),
+                        )
+                    )
 
         return terms
 
@@ -162,7 +321,7 @@ class LowerToEmulatorProgram(ProgramVisitor):
                     terms.append(
                         RabiTerm(
                             target_atoms=self.visit_spatial_modulation(sm),
-                            amplitude=WaveformCompiler(self.assignments).emit(wf),
+                            amplitude=self.waveform_compiler.emit(wf),
                         )
                     )
 
@@ -170,7 +329,7 @@ class LowerToEmulatorProgram(ProgramVisitor):
                 Field(value),
                 Field(value={field.Uniform: phase_waveform}),
             ) if len(value) < self.space.n_atoms:
-                rabi_phase = WaveformCompiler(self.assignments).emit(phase_waveform)
+                rabi_phase = self.waveform_compiler.emit(phase_waveform)
                 self.duration = max(
                     float(phase_waveform.duration(**self.assignments)), self.duration
                 )
@@ -181,7 +340,7 @@ class LowerToEmulatorProgram(ProgramVisitor):
                     terms.append(
                         RabiTerm(
                             target_atoms=self.visit_spatial_modulation(sm),
-                            amplitude=WaveformCompiler(self.assignments).emit(wf),
+                            amplitude=self.waveform_compiler.emit(wf),
                             phase=rabi_phase,
                         )
                     )
@@ -198,12 +357,18 @@ class LowerToEmulatorProgram(ProgramVisitor):
                 terms = []
                 for atom in range(self.space.n_atoms):
                     phase_wf = sum(
-                        phase_target_atoms_dict[sm].get(atom, 0.0) * wf
-                        for sm, wf in phase.value.items()
+                        (
+                            phase_target_atoms_dict[sm].get(atom, 0.0) * wf
+                            for sm, wf in phase.value.items()
+                        ),
+                        start=waveform.Constant(0.0, 0.0),
                     )
                     amplitude_wf = sum(
-                        phase_target_atoms_dict[sm].get(atom, 0.0) * wf
-                        for sm, wf in amplitude.value.items()
+                        (
+                            phase_target_atoms_dict[sm].get(atom, 0.0) * wf
+                            for sm, wf in amplitude.value.items()
+                        ),
+                        start=waveform.Constant(0.0, 0.0),
                     )
 
                     self.duration = max(
@@ -216,10 +381,8 @@ class LowerToEmulatorProgram(ProgramVisitor):
                     terms.append(
                         RabiTerm(
                             target_atoms={atom: 1},
-                            amplitude=WaveformCompiler(self.assignments).emit(
-                                amplitude_wf
-                            ),
-                            phase=WaveformCompiler(self.assignments).emit(phase_wf),
+                            amplitude=self.waveform_compiler.emit(amplitude_wf),
+                            phase=self.waveform_compiler.emit(phase_wf),
                         )
                     )
 
@@ -230,8 +393,8 @@ class LowerToEmulatorProgram(ProgramVisitor):
                 amplitude = fields.get(pulse.rabi.amplitude, None)
                 phase = fields.get(pulse.rabi.phase, None)
 
-                self.rabi_terms = self.visit_detuning(detuning)
-                self.detuning_terms = self.visit_rabi(amplitude, phase)
+                self.detuning_terms = self.visit_detuning(detuning)
+                self.rabi_terms = self.visit_rabi(amplitude, phase)
 
             case pulse.NamedPulse(sub_pulse, _):
                 self.visit(sub_pulse)
@@ -239,11 +402,14 @@ class LowerToEmulatorProgram(ProgramVisitor):
             case _:
                 raise NotImplementedError
 
-    def emit(self, program: ir.Profram) -> EmulatorProgram:
-        self.visit(program)
+    def emit(self, program: ir.Program, add_hyperfine: bool = False) -> EmulatorProgram:
+        self.space = EmulatorSpace(
+            self.assignments, add_hyperfine, self.blockade_radius
+        ).emit(program)
 
+        self.visit(program)
         return EmulatorProgram(
-            register=self.space,
+            space=self.space,
             duration=self.duration,
             rydberg=self.rydberg,
             hyperfine=self.hyperfine,
