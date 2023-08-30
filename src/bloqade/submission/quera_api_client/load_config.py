7787ded72b855e9e548a8ed67c40ca8255c82672
Phillip Weinberg <weinbe58@gmail.com>
2023-08-30 09:15:43 -0400

    fixing configs.

--- src/bloqade/submission/quera_api_client/load_config.py
+++ src/bloqade/submission/quera_api_client/load_config.py
@@ -4,11 +4,17 @@ import json
 
 def load_config(qpu: str):
     real_path = os.path.realpath(__file__)
+    real_path_list = os.path.split(real_path)[:-1]
+    real_path = os.path.join(*real_path_list)
 
     match qpu:
         case "Aquila":
-            with open(os.path.join(real_path, "config", "aquila_api_config.json")) as f:
+            with open(
+                os.path.join(real_path, "config", "aquila_api_config.json"), "r"
+            ) as f:
                 return json.load(f)
         case "Mock":
-            with open(os.path.join(real_path, "config", "mock_api_config.json")) as f:
+            with open(
+                os.path.join(real_path, "config", "mock_api_config.json"), "r"
+            ) as f:
                 return json.load(f)
