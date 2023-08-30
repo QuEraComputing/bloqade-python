1cd0938d8f49df23441b1bc4ccbad24d945c3bce
Phillip Weinberg <weinbe58@gmail.com>
2023-08-30 12:15:26 -0400

    adding fix for missing data

--- src/bloqade/task/base.py
+++ src/bloqade/task/base.py
@@ -27,6 +27,10 @@ class Geometry:
     filling: List[int]
     parallel_decoder: Optional[ParallelDecoder] = None
 
+    @property
+    def n_atoms(self):
+        return len(self.sites)
+
 
 class JSONInterface(BaseModel):
     def json(self, exclude_none=True, by_alias=True, **json_options) -> str:
@@ -166,8 +170,9 @@ class Report:
         filtered_df = self.dataframe[perfect_sorting == pre_sequence]
         task_numbers = filtered_df.index.get_level_values("task_number")
 
-        n_atoms = len(perfect_sorting[0])
-        bitstrings = [np.zeros((0, n_atoms)) for _ in range(task_numbers.max() + 1)]
+        bitstrings = [
+            np.zeros((0, self.geos[i].n_atoms)) for i in range(task_numbers.max() + 1)
+        ]
 
         for task_number in task_numbers.unique():
             bitstrings[task_number] = filtered_df.loc[task_number, ...].to_numpy()
