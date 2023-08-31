94c1471ca007e544bfe59cd1de097b908eb20908
Kai-Hsin Wu <khwu@KHWus-MacBook-Pro.local>
2023-08-31 10:50:42 -0400

    update test

--- tests/test_report.py
+++ tests/test_report.py
@@ -32,7 +32,7 @@ prog = (
 with tempfile.NamedTemporaryFile() as f:
     future = prog.quera.mock(state_file=f.name).submit(shots=100)
     future.pull()
-    future2 = future.remove_tasks(["Completed"])
+    future2 = future.remove_tasks("Completed")
     future2
 
 print(future.report().bitstrings)
