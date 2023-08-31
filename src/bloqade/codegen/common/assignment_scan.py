e78341ff4b1e1ea2555dd05835523e1b41898dc9
Phillip Weinberg <weinbe58@gmail.com>
2023-08-31 14:58:06 -0400

    adding specific visitor for waveforms.

--- src/bloqade/codegen/common/assignment_scan.py
+++ src/bloqade/codegen/common/assignment_scan.py
@@ -1,19 +1,58 @@
-from bloqade.ir.analog_circuit import AnalogCircuit
 from bloqade.ir.visitor.analog_circuit import AnalogCircuitVisitor
+from bloqade.ir.visitor.waveform import WaveformVisitor
+from bloqade.ir.analog_circuit import AnalogCircuit
 import bloqade.ir.control.sequence as sequence
 import bloqade.ir.control.pulse as pulse
 import bloqade.ir.control.field as field
 import bloqade.ir.control.waveform as waveform
-import bloqade.ir.scalar as scalar
 import bloqade.ir.analog_circuit as analog_circuit
 
 import numbers
 from typing import Any, Dict
 
 
+class AssignmentScanRecord(WaveformVisitor):
+    def __init__(self, assignments: Dict[str, numbers.Real] = {}):
+        self.assignments = dict(assignments)
+
+    def visit_record(self, ast: waveform.Record):
+        duration = ast.waveform.duration(**self.assignments)
+        var = ast.var
+        value = ast.waveform.eval_decimal(duration, **self.assignments)
+        self.assignments[var.name] = value
+        self.visit(ast.waveform)
+
+    def visit_append(self, ast: waveform.Append):
+        list(map(self.visit, ast.waveforms))
+
+    def visit_slice(self, ast: waveform.Slice):
+        self.visit(ast.waveform)
+
+    def visit_add(self, ast: waveform.Add):
+        self.visit(ast.left)
+        self.visit(ast.right)
+
+    def visit_negative(self, ast: waveform.Negative):
+        self.visit(ast.waveform)
+
+    def visit_scale(self, ast: waveform.Scale):
+        self.visit(ast.waveform)
+
+    def visit_aligned_waveform(self, ast: waveform.AlignedWaveform):
+        self.visit(ast.waveform)
+
+    def visit_smooth(self, ast: waveform.Smooth):
+        self.visit(ast.waveform)
+
+    def emit(self, ast: waveform.Waveform) -> Dict[str, numbers.Real]:
+        self.visit(ast)
+        return self.assignments
+
+
 class AssignmentScan(AnalogCircuitVisitor):
     def __init__(self, assignments: Dict[str, numbers.Real] = {}):
         self.assignments = dict(assignments)
+        self.waveform_visitor = AssignmentScanRecord(self.assignments)
 
     def visit_analog_circuit(self, ast: AnalogCircuit) -> Any:
         self.visit(ast.sequence)
@@ -50,34 +89,7 @@ class AssignmentScan(AnalogCircuitVisitor):
         pass
 
     def visit_waveform(self, ast: waveform.Waveform):
-        match ast:
-            case waveform.Record(sub_waveform, scalar.Variable(name)):
-                duration = sub_waveform.duration(**self.assignments)
-                value = sub_waveform.eval_decimal(duration, **self.assignments)
-                self.assignments[name] = value
-                self.visit(sub_waveform)
-
-            case waveform.Append(waveforms):
-                list(map(self.visit, waveforms))
-
-            case waveform.Slice(sub_waveform, _):
-                self.visit(sub_waveform)
-
-            case waveform.Add(lhs, rhs):
-                self.visit(lhs)
-                self.visit(rhs)
-
-            case waveform.Negative(sub_waveform):
-                self.visit(sub_waveform)
-
-            case waveform.Scale(_, sub_waveform):
-                self.visit(sub_waveform)
-
-            case waveform.AlignedWaveform(waveform=sub_waveform):
-                self.visit(sub_waveform)
-
-            case waveform.Smooth(_, sub_waveform):
-                self.visit(sub_waveform)
+        self.assignments.update(self.waveform_visitor.emit(ast))
 
     def emit(self, ast: analog_circuit.AnalogCircuit) -> Dict[str, numbers.Real]:
         self.visit(ast)
