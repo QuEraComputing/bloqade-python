3ff60707cb93598a618a1e1d17f9669f3b12d600
Phillip Weinberg <weinbe58@gmail.com>
2023-09-04 18:08:38 -0400

    integration into builder

--- src/bloqade/ir/routine/bloqade.py
+++ src/bloqade/ir/routine/bloqade.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from .base import RoutineBase
 from bloqade.task.batch import LocalBatch
 from typing import Tuple
@@ -7,45 +8,112 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class BloqadeServiceOptions(RoutineBase):
-    def python(
-        self,
-        bloqade_radius: float = 0,
-        atol: float = 1e-7,
-        rtol: float = 1e-14,
-        nsteps: int = 2_147_483_647,
-        solver: str = "dop853",
-    ):
-        return BloqadePythonRoutine(source=self.source, bloqade_radius=bloqade_radius)
+    def python(self):
+        return BloqadePythonRoutine(source=self.source)
 
 
 @dataclass(frozen=True)
 class BloqadePythonRoutine(RoutineBase):
-    bloqade_radius: float
-    atol: float = 1e-7
-    rtol: float = 1e-14
-    nsteps: int = 2_147_483_647
-
     def compile(
-        self, shots: int, args: Tuple[Real, ...] = (), name: str | None = None
+        self,
+        shots: int,
+        args: Tuple[Real, ...] = (),
+        name: str | None = None,
+        blockade_radius: float = 0.0,
+        cache_matrices: bool = False,
     ) -> LocalBatch:
         from bloqade.codegen.common.assignment_scan import AssignmentScan
         from bloqade.codegen.common.assign_variables import AssignAnalogCircuit
         from bloqade.emulate.codegen.emulator_ir import EmulatorProgramCodeGen
-        from bloqade.emulate.codegen.rydberg_hamiltonian import (
-            RydbergHamiltonianCodeGen,
-        )
-        from bloqade.emulate.ir.state_vector import AnalogGate
+        from bloqade.emulate.codegen.rydberg_hamiltonian import CompileCache
+        from bloqade.task.bloqade import BloqadeTask
 
         circuit, params = self.source.parse_source()
 
         circuit = AssignAnalogCircuit(params.static_params).visit(circuit)
 
-        gates = {}
+        if cache_matrices:
+            matrix_cache = CompileCache()
+        else:
+            matrix_cache = None
+
+        tasks = OrderedDict()
         for task_number, batch_param in params.batch_assignments(*args):
             record_params = AssignmentScan(batch_param).emit(circuit)
             final_circuit = AssignAnalogCircuit(record_params).visit(circuit)
-            emulator_ir = EmulatorProgramCodeGen().emit(final_circuit)
-            hamiltonian = RydbergHamiltonianCodeGen().emit(emulator_ir)
+            emulator_ir = EmulatorProgramCodeGen(blockade_radius=blockade_radius).emit(
+                final_circuit
+            )
+            tasks[task_number] = BloqadeTask(shots, emulator_ir, matrix_cache)
 
-            gate = AnalogGate(hamiltonian)
-            gates[task_number] = gate
+        return LocalBatch(tasks, name)
+
+    def run(
+        self,
+        shots: int,
+        args: Tuple[Real, ...] = (),
+        name: str | None = None,
+        blockade_radius: float = 0.0,
+        cache_matrices: bool = False,
+        multiprocesing: bool = False,
+        num_workers: int | None = None,
+        solver_name: str = "dop853",
+        atol: float = 1e-7,
+        rtol: float = 1e-14,
+        nsteps: int = 2_147_483_647,
+    ):
+        if multiprocesing and cache_matrices:
+            raise ValueError(
+                "Cannot use multiprocessing and cache_matrices at the same time."
+            )
+
+        compile_options = dict(
+            shots=shots,
+            args=args,
+            name=name,
+            blockade_radius=blockade_radius,
+            cache_matrices=cache_matrices,
+        )
+
+        solver_options = dict(
+            multiprocesing=multiprocesing,
+            num_workers=num_workers,
+            solver_name=solver_name,
+            atol=atol,
+            rtol=rtol,
+            nsteps=nsteps,
+        )
+
+        batch = self.compile(**compile_options)
+        batch._run(**solver_options)
+
+        return batch
+
+    def __call__(
+        self,
+        *args: Real,
+        shots: int = 1,
+        name: str | None = None,
+        blockade_radius: float = 0.0,
+        multiprocesing: bool = False,
+        num_workers: int | None = None,
+        cache_matrices: bool = False,
+        solver_name: str = "dop853",
+        atol: float = 1e-7,
+        rtol: float = 1e-14,
+        nsteps: int = 2_147_483_647,
+    ):
+        options = dict(
+            shots=shots,
+            args=args,
+            name=name,
+            blockade_radius=blockade_radius,
+            multiprocesing=multiprocesing,
+            num_workers=num_workers,
+            cache_matrices=cache_matrices,
+            solver_name=solver_name,
+            atol=atol,
+            rtol=rtol,
+            nsteps=nsteps,
+        )
+        return self.run(**options)
