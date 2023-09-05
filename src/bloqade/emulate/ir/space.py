5e3cbb0592287e6f238f99a706ee989e6bbb58ae
Phillip Weinberg <weinbe58@gmail.com>
2023-09-05 11:34:08 -0400

    lint

--- src/bloqade/emulate/ir/space.py
+++ src/bloqade/emulate/ir/space.py
@@ -159,6 +159,7 @@ class Space:
     def sample_state_vector(
         self, state_vector: NDArray, n_samples: int, project_hyperfine: bool = True
     ) -> NDArray:
+        from .atom_type import ThreeLevelAtomType
         p = np.abs(state_vector) ** 2
         sampled_configs = np.random.choice(self.configurations, size=n_samples, p=p)
 
