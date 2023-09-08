39efd882302883126413e3c9b03a5bd4a571815e
Phillip Weinberg <weinbe58@gmail.com>
2023-09-08 11:35:36 -0400

    adding integration test with phase.

--- tests/test_python_emulator.py
+++ tests/test_python_emulator.py
@@ -1,8 +1,8 @@
-from bloqade import start
+from bloqade import start, var
 import numpy as np
 
 
-def test_integration():
+def test_integration_1():
     (
         start.add_position((0, 0))
         .add_position((0, 1.0))
@@ -14,7 +14,29 @@ def test_integration():
         .assign(ramp_time=3.0)
         .batch_assign(r=np.linspace(4, 10, 31).tolist())
         .bloqade.python()
-        .run(10000, cache_matrices=True)
+        .run(10000, cache_matrices=True, blockade_radius=6.0)
+        .report()
+        .bitstrings()
+    )
+
+
+def test_integration_2():
+    ramp_time = var("ramp_time")
+    (
+        start.add_position((0, 0))
+        .add_position((0, 1.0))
+        .scale("r")
+        .rydberg.detuning.uniform.piecewise_linear(
+            [0.1, "ramp_time", 0.1], [-100, -100, 100, 100]
+        )
+        .amplitude.uniform.piecewise_linear([0.1, ramp_time, 0.1], [0, 10, 10, 0])
+        .phase.uniform.piecewise_constant(
+            [0.1, ramp_time / 2, ramp_time / 2, 0.1], [0, 0, np.pi, np.pi]
+        )
+        .assign(ramp_time=3.0)
+        .batch_assign(r=np.linspace(4, 10, 31).tolist())
+        .bloqade.python()
+        .run(10000, cache_matrices=True, blockade_radius=6.0)
         .report()
         .bitstrings()
     )
