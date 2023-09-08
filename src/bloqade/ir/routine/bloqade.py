65a6a308abf13915794e03dc89efcb88d352a14a
Phillip Weinberg <weinbe58@gmail.com>
2023-09-08 12:00:32 -0400

    adding docs.

--- src/bloqade/ir/routine/bloqade.py
+++ src/bloqade/ir/routine/bloqade.py
@@ -63,7 +63,38 @@ class BloqadePythonRoutine(RoutineBase):
         atol: float = 1e-14,
         rtol: float = 1e-7,
         nsteps: int = 2_147_483_647,
-    ):
+    ) -> LocalBatch:
+        """Run the current program using bloqade python backend
+
+        Args:
+            shots (int): number of shots after running state vector simulation
+            args (Tuple[Real, ...], optional): The arguments passed in through
+            `flatten`.
+            Defaults to ().
+            name (str | None, optional): Name to give this run. Defaults to None.
+            blockade_radius (float, optional): Use the Blockade subspace given a
+            particular radius. Defaults to 0.0.
+            cache_matrices (bool, optional): Reuse previously evaluated matrcies when
+            possible. Defaults to False.
+            multiprocessing (bool, optional): Use multiple processes to process the
+            batches. Defaults to False.
+            num_workers (int | None, optional): Number of processes to run with
+            multiprocessing. Defaults to None.
+            solver_name (str, optional): Which SciPy Solver to use. Defaults to
+            "dop853".
+            atol (float, optional): Absolute tolerance for ODE solver. Defaults to
+            1e-14.
+            rtol (float, optional): Relative tolerance for adaptive step in ODE solver.
+            Defaults to 1e-7.
+            nsteps (int, optional): Maximum number of steps allowed per integration
+            step. Defaults to 2_147_483_647, the maximum value.
+
+        Raises:
+            ValueError: Cannot use multiprocessing and cache_matrices at the same time.
+
+        Returns:
+            LocalBatch: Batch of local tasks that have been executed.
+        """
         if multiprocessing and cache_matrices:
             raise ValueError(
                 "Cannot use multiprocessing and cache_matrices at the same time."
