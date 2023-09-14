c12574dfeb7219d2fb0ac9d6ff232ca45ee424c1
Phillip Weinberg <weinbe58@gmail.com>
2023-09-14 08:50:57 -0400

    fixing bugs in impl

--- src/bloqade/serialize.py
+++ src/bloqade/serialize.py
@@ -1,6 +1,6 @@
 import json
 from typing import Any
-from beartype.typing import Dict, Type, Union, List
+from beartype.typing import Type, Callable
 from beartype import beartype
 
 
@@ -15,62 +15,60 @@ class Serializer(json.JSONEncoder):
     def register(cls: Type):
         @beartype
         def set_serializer(f: Callable):
-            type_name = f"{cls.__module__}.{cls.__name__}"
             Serializer.serializers[cls] = f
-            Serializer.type_to_str[cls] = type_name
-            Serializer.str_to_type[type_name] = cls
-
             return f
-        
+
         @beartype
         def set_deserializer(f: Callable):
             Serializer.deserializers[cls] = f
-
             return f
 
-        Serializer.methods[cls] = None
-
-        setattr(cls, "set_serializer", set_serializer)
-        setattr(cls, "set_deserializer", set_deserializer)
+        type_name = f"{cls.__module__}.{cls.__name__}"
+        Serializer.type_to_str[cls] = type_name
+        Serializer.str_to_type[type_name] = cls
+        setattr(cls, "set_serializer", staticmethod(beartype(set_serializer)))
+        setattr(cls, "set_deserializer", staticmethod(beartype(set_deserializer)))
 
         return cls
-    
-    def object_hook(self, d: Any) -> Any:
+
+    @classmethod
+    def object_hook(cls, d: Any) -> Any:
         if isinstance(d, dict) and len(d) == 1:
             ((key, value),) = d.items()
-            if key in self.str_to_type:
-                return self.str_to_type[key](**value)
-            
+            if key in cls.str_to_type:
+                obj_cls = cls.str_to_type[key]
+                deserialize = cls.deserializers.get(obj_cls)
 
+                if deserialize is None:
+                    raise TypeError(f"no deserializer registered for type {cls}")
+
+                return deserialize(value)
+
+        return d
 
     def default(self, o: Any) -> Any:
-        if type(o) in self.methods:
+        if type(o) in self.serializers:
             cls = type(o)
-            if self.methods[cls] is None:
+            serializer = self.serializers.get(cls)
+            if serializer is None:
                 raise TypeError(f"no serializer registered for type {cls}")
-            
 
-            method = getattr(o, self.methods[type(o)])
-            return {self.type_to_str[cls]: method(o)}
+            return {self.type_to_str[cls]: serializer(o)}
 
         return super().default(o)
 
 
-def loads(s, use_decimal=True, **json_kwargs):
-    return json.loads(
-        s, object_hook=Serializer.object_hook, use_decimal=use_decimal, **json_kwargs
-    )
+def loads(s, **json_kwargs):
+    return json.loads(s, object_hook=Serializer.object_hook, **json_kwargs)
 
 
-def load(fp, use_decimal=True, **json_kwargs):
-    return json.load(
-        fp, object_hook=Serializer.object_hook, use_decimal=use_decimal, **json_kwargs
-    )
+def load(fp, **json_kwargs):
+    return json.load(fp, object_hook=Serializer.object_hook, **json_kwargs)
 
 
-def dumps(o, use_decimal=True, **json_kwargs):
-    return json.dumps(o, cls=Serializer, use_decimal=use_decimal, **json_kwargs)
+def dumps(o, **json_kwargs):
+    return json.dumps(o, cls=Serializer, **json_kwargs)
 
 
-def dump(o, fp, use_decimal=True, **json_kwargs):
-    return json.dump(o, fp, cls=Serializer, use_decimal=use_decimal, **json_kwargs)
+def dump(o, fp, **json_kwargs):
+    return json.dump(o, fp, cls=Serializer, **json_kwargs)
