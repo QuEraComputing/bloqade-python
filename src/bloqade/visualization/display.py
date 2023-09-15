bcc5759e9d3ca7a8f2a6b82a13675b95e064736a
kaihsin <kaihsinwu@gmail.com>
2023-09-15 14:53:03 -0400

    add tests for builder

--- src/bloqade/visualization/display.py
+++ src/bloqade/visualization/display.py
@@ -36,11 +36,37 @@ def display_ir(obj, assignments):
         raise NotImplementedError(f"not supported IR for display, got {type(obj)}")
 
 
+def figure_ir(obj, assignments):
+    from bloqade.ir.analog_circuit import AnalogCircuit
+    from bloqade.ir.control.pulse import PulseExpr
+    from bloqade.ir.control.sequence import SequenceExpr
+    from bloqade.ir.control.field import Field, SpatialModulation
+    from bloqade.ir.control.waveform import Waveform
+    from bloqade.ir.location import AtomArrangement
+
+    if isinstance(obj, AnalogCircuit):
+        return analog_circuit_figure(obj, assignments)
+    elif isinstance(obj, PulseExpr):
+        return obj.figure(**assignments)
+    elif isinstance(obj, SequenceExpr):
+        return obj.figure(**assignments)
+    elif isinstance(obj, Field):
+        return obj.figure(**assignments)
+    elif isinstance(obj, SpatialModulation):
+        return obj.figure(**assignments)
+    elif isinstance(obj, Waveform):
+        return obj.figure(**assignments)
+    elif isinstance(obj, AtomArrangement):
+        return atom_arrangement_figure(obj, assignments)
+    else:
+        raise NotImplementedError(f"not supported IR for display, got {type(obj)}")
+
+
 def liner(txt):
     return f"<p>{txt}</p>"
 
 
-def display_builder(builder, batch_id):
+def builder_figure(builder, batch_id):
     from bloqade.builder.parse.builder import Parser
 
     analog_circ, metas = Parser().parse_source(builder)
@@ -55,13 +81,22 @@ def display_builder(builder, batch_id):
     field, reg = analog_circ.figure(**kwargs)
 
     div = Div(text=out, width=200, height=200)
+    return row(field, column(reg, div))
 
-    show(row(field, column(reg, div)))
+
+def display_builder(builder, batch_id):
+    fig = builder_figure(builder, batch_id)
+    show(fig)
 
 
 ## ir
+def analog_circuit_figure(analog_circuit, assignments):
+    return row(*analog_circuit.figure(**assignments))
+
+
 def display_analog_circuit(analog_circuit, assignments):
-    show(row(*analog_circuit.figure(**assignments)))
+    fig = analog_circuit_figure(analog_circuit, assignments)
+    show(fig)
 
 
 def display_pulse(pulse, assignments):
@@ -84,7 +119,7 @@ def display_waveform(wvfm_ir, assignments):
     show(wvfm_ir.figure(**assignments))
 
 
-def display_atom_arrangement(atom_arrangement, assignments):
+def atom_arrangement_figure(atom_arrangement, assignments):
     """show the register."""
     p = atom_arrangement.figure(None, **assignments)
 
@@ -111,14 +146,26 @@ def display_atom_arrangement(atom_arrangement, assignments):
 
     full = column(p, row(Brad_input, toggle_button))
     # full.sizing_mode="scale_both"
-    show(full)
+    return full
+
+
+def display_atom_arrangement(atom_arrangement, assignments):
+    fig = atom_arrangement_figure(atom_arrangement, assignments)
+    show(fig)
 
 
 ## report
-def display_report(report):
+
+
+def report_figure(report):
     dat = report_visualize.format_report_data(report)
     p = report_visualize.report_visual(*dat)
-    show(p)
+    return p
+
+
+def display_report(report):
+    fig = report_figure(report)
+    show(fig)
 
 
 ## task_ir
