edb6ab091da43513a590945e5a65e70b3ab07868
Phillip Weinberg <weinbe58@gmail.com>
2023-09-21 12:34:40 -0400

    add integration test comparing braket to bloqade emulator.

--- tests/test_python_emulator.py
+++ tests/test_python_emulator.py
@@ -1,6 +1,8 @@
 from bloqade import start, var
+from bloqade.atom_arrangement import Chain
 from bloqade.serialize import dumps, loads
 import numpy as np
+from beartype.typing import Dict
 
 
 def test_integration_1():
@@ -159,3 +161,46 @@ def test_serialization():
     obj_str = dumps(batch)
     batch2 = loads(obj_str)
     assert isinstance(batch2, type(batch))
+
+
+def KS_test(
+    lhs_counts: Dict[str, int], rhs_counts: Dict[str, int], alpha: float = 0.05
+) -> None:
+    # statistical test to check of two empirical distributions are the same
+    # https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test#Two-sample_Kolmogorov%E2%80%93Smirnov_test
+    m = sum(lhs_counts.values())
+    n = sum(rhs_counts.values())
+    lhs_cdf = []
+    rhs_cdf = []
+    bitstrings = sorted(list(set([*lhs_counts.keys(), *rhs_counts.keys()])))
+    for bitstring in bitstrings:
+        lhs_cdf.append(lhs_counts.get(bitstring, 0) / n)
+        rhs_cdf.append(rhs_counts.get(bitstring, 0) / m)
+
+    lhs_cdf = np.cumsum(sorted(lhs_cdf, reverse=True))
+    rhs_cdf = np.cumsum(sorted(rhs_cdf, reverse=True))
+
+    c_alpha = np.sqrt(-0.5 * np.log(alpha / 2))
+
+    assert np.linalg.norm(lhs_cdf - rhs_cdf, ord=np.inf) <= c_alpha * np.sqrt(
+        (m + n) / (m * n)
+    )
+
+
+def test_bloqade_against_braket():
+    prog = (
+        Chain(5, lattice_spacing=6.1)
+        .rydberg.detuning.uniform.piecewise_linear(
+            [0.1, 3.0, 0.1], [-20, -20, "d", "d"]
+        )
+        .amplitude.uniform.piecewise_linear([0.1, 3.0, 0.1], [0, 15, 15, 0])
+        .phase.uniform.constant(0.3, 3.2)
+        .batch_assign(d=[0, 10, 20, 30, 40])
+    )
+
+    nshots = 1000
+    a = prog.bloqade.python().run(nshots, cache_matrices=True).report().counts
+    b = prog.braket.local_emulator().run(nshots).report().counts
+
+    for lhs, rhs in zip(a, b):
+        KS_test(lhs, rhs)
