494c586b789d93791f791988a721790326badbe5
Phillip Weinberg <weinbe58@gmail.com>
2023-09-22 16:30:18 -0400

    moving codgen modules into appropriate locations

--- tests/test_emulator_codegen.py
+++ tests/test_emulator_codegen.py
@@ -9,7 +9,7 @@ from bloqade.emulate.ir.emulator import (
     DetuningOperatorData,
     RabiOperatorType,
 )
-from bloqade.emulate.codegen.emulator_ir import (
+from bloqade.codegen.emulator_ir import (
     EmulatorProgramCodeGen,
     CompiledWaveform,
     LevelCoupling,
