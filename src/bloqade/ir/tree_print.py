c4f5b6b231a44681379cae140351908da0e27726
Phillip Weinberg <weinbe58@gmail.com>
2023-09-25 20:46:44 -0400

    cleaning up if statement

--- src/bloqade/ir/tree_print.py
+++ src/bloqade/ir/tree_print.py
@@ -174,9 +174,8 @@ class Printer:
             if not (self.state.last and len(children) == 0):
                 self.p.text("\n")
 
-        if (
-            self.state.depth > self.max_tree_depth or self.state.depth > cycle
-        ):  # need to set this variable dynamically
+        if self.state.depth > min(cycle, self.max_tree_depth):
+            # need to set this variable dynamically
             self.p.text(self.charset.trunc)
             self.p.text("\n")
             return
