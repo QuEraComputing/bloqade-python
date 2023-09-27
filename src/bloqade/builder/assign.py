6df9f35ca5d40ac8a2db044c354aeb3bcf77ff62
Phillip Weinberg <weinbe58@gmail.com>
2023-09-27 10:36:58 -0400

    debugging new casting.

--- src/bloqade/builder/assign.py
+++ src/bloqade/builder/assign.py
@@ -1,5 +1,5 @@
-from itertools import repeat, starmap
-from beartype.typing import Optional, List
+from itertools import repeat
+from beartype.typing import Optional, List, Dict, Set
 from bloqade.builder.typing import ParamType
 from bloqade.builder.base import Builder
 from bloqade.builder.pragmas import Parallelizable, AddArgs, BatchAssignable
@@ -9,50 +9,55 @@ from decimal import Decimal
 import numpy as np
 
 
-def cast_scalar_param(value: ParamType, name: str) -> Decimal:
-    if isinstance(value, (Real, Decimal)):
-        return Decimal(str(value))
+class CastParams:
+    def __init__(self, n_sites: int, scalar_vars: Set[str], vector_vars: Set[str]):
+        self.n_sites = n_sites
+        self.scalar_vars = scalar_vars
+        self.vector_vars = vector_vars
 
-    raise TypeError(
-        f"assign parameter '{name}' must be a real number, "
-        f"found type: {type(value)}"
-    )
+    def cast_scalar_param(self, value: ParamType, name: str) -> Decimal:
+        if isinstance(value, (Real, Decimal)):
+            return Decimal(str(value))
 
+        raise TypeError(
+            f"assign parameter '{name}' must be a real number, "
+            f"found type: {type(value)}"
+        )
 
-def cast_vector_param(value: List[ParamType], name: str) -> List[Decimal]:
-    if isinstance(value, np.ndarray):
-        value = value.tolist()
+    def cast_vector_param(
+        self,
+        value: List[ParamType],
+        name: str,
+    ) -> List[Decimal]:
+        if isinstance(value, np.ndarray):
+            value = value.tolist()
 
-    if isinstance(value, (list, tuple)):
-        return list(starmap(cast_scalar_param, zip(value, repeat(name))))
+        if isinstance(value, (list, tuple)):
+            if len(value) != self.n_sites:
+                raise ValueError(
+                    f"assign parameter '{name}' must be a list of length "
+                    f"{self.n_sites}, found length: {len(value)}"
+                )
+            return list(map(self.cast_scalar_param, value, repeat(name, len(value))))
 
-    raise TypeError(
-        f"assign parameter '{name}' must be a list of real numbers, "
-        f"found type: {type(value)}"
-    )
+        raise TypeError(
+            f"assign parameter '{name}' must be a list of real numbers, "
+            f"found type: {type(value)}"
+        )
 
+    def cast_params(self, params: Dict[str, ParamType]) -> Dict[str, ParamType]:
+        checked_params = {}
+        for name, value in params.items():
+            if name not in self.scalar_vars and name not in self.vector_vars:
+                raise ValueError(
+                    f"assign parameter '{name}' is not found in analog circuit."
+                )
+            if name in self.vector_vars:
+                checked_params[name] = self.cast_vector_param(value, name)
+            else:
+                checked_params[name] = self.cast_scalar_param(value, name)
 
-def cast_batch_scalar_param(value: List[ParamType], name: str) -> List[Decimal]:
-    if isinstance(value, np.ndarray):
-        value = value.tolist()
-
-    if isinstance(value, (list, tuple)):
-        return list(starmap(cast_scalar_param, zip(value, repeat(name))))
-
-    raise TypeError(
-        f"batch_assign parameter '{name}' must be a list of real numbers, "
-        f"found type: {type(value)}"
-    )
-
-
-def cast_batch_vector_param(value: List[ParamType], name: str) -> List[List[Decimal]]:
-    if isinstance(value, (list, tuple)):
-        return list(starmap(cast_vector_param, zip(value, repeat(name))))
-
-    raise TypeError(
-        f"batch_assign parameter '{name}' must be a list of lists of real numbers, "
-        f"found type: {type(value)}"
-    )
+        return checked_params
 
 
 class AssignBase(Builder):
@@ -62,49 +67,64 @@ class AssignBase(Builder):
 class Assign(BatchAssignable, AddArgs, Parallelizable, BackendRoute, AssignBase):
     __match_args__ = ("_assignments", "__parent__")
 
-    def __init__(self, parent: Optional[Builder] = None, **assignments) -> None:
+    def __init__(
+        self, assignments: Dict[str, ParamType], parent: Optional[Builder] = None
+    ) -> None:
         from bloqade.ir.analysis.scan_variables import ScanVariablesAnalogCircuit
 
         super().__init__(parent)
 
         circuit = self.parse_circuit()
-        vars = ScanVariablesAnalogCircuit().emit(circuit)
+        variables = ScanVariablesAnalogCircuit().emit(circuit)
 
-        self._assignments = {}
-        for name, value in assignments.items():
-            if name not in vars.scalar_vars and name not in vars.vector_vars:
-                raise ValueError(
-                    f"batch_assign parameter '{name}' is not found in analog circuit."
-                )
-            if name in vars.vector_vars:
-                self._assignments[name] = cast_vector_param(value, name)
-            else:
-                self._assignments[name] = cast_scalar_param(value, name)
+        self._static_params = CastParams(
+            circuit.register.n_sites, variables.scalar_vars, variables.vector_vars
+        ).cast_params(assignments)
 
 
 class BatchAssign(AddArgs, Parallelizable, BackendRoute, AssignBase):
     __match_args__ = ("_assignments", "__parent__")
 
-    def __init__(self, parent: Optional[Builder] = None, **assignments) -> None:
+    def __init__(
+        self, assignments: Dict[str, ParamType], parent: Optional[Builder] = None
+    ) -> None:
         from bloqade.ir.analysis.scan_variables import ScanVariablesAnalogCircuit
 
         super().__init__(parent)
 
         circuit = self.parse_circuit()
-        vars = ScanVariablesAnalogCircuit().emit(circuit)
-
-        self._assignments = {}
-        for name, values in assignments.items():
-            if name not in vars.scalar_vars and name not in vars.vector_vars:
-                raise ValueError(
-                    f"batch_assign parameter '{name}' is not found in analog circuit."
-                )
-            if name in vars.vector_vars:
-                self._assignments[name] = cast_batch_vector_param(values, name)
-            else:
-                self._assignments[name] = cast_batch_scalar_param(values, name)
+        variables = ScanVariablesAnalogCircuit().emit(circuit)
 
         if not len(np.unique(list(map(len, assignments.values())))) == 1:
             raise ValueError(
                 "all the assignment variables need to have same number of elements."
             )
+
+        tuple_iterators = [
+            zip(repeat(name), values) for name, values in assignments.items()
+        ]
+
+        caster = CastParams(
+            circuit.register.n_sites, variables.scalar_vars, variables.vector_vars
+        )
+
+        self._batch_params = list(
+            map(caster.cast_params, map(dict, zip(*tuple_iterators)))
+        )
+
+
+class ListAssign(AddArgs, Parallelizable, BackendRoute, AssignBase):
+    def __init__(
+        self, batch_params: List[Dict[str, ParamType]], parent: Optional[Builder] = None
+    ) -> None:
+        from bloqade.ir.analysis.scan_variables import ScanVariablesAnalogCircuit
+
+        super().__init__(parent)
+
+        circuit = self.parse_circuit()
+        variables = ScanVariablesAnalogCircuit().emit(circuit)
+        caster = CastParams(
+            circuit.register.n_sites, variables.scalar_vars, variables.vector_vars
+        )
+
+        self._batch_params = list(map(caster.cast_params, batch_params))
