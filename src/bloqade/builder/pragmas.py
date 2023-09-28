604f07caab523d8078624ad343bcea6dc35aa6ef
John Long <johnzl@protonmail.ch>
2023-09-27 22:04:00 -0400

    WIP pragmas/device() docstring

--- src/bloqade/builder/pragmas.py
+++ src/bloqade/builder/pragmas.py
@@ -18,23 +18,33 @@ class AddArgs:
 class Assignable:
     def assign(self, **assignments) -> "Assign":
         """
-        Assign values to variables declared previously in the program.
+        - assign values to variables declared previously in the program.
+        - This is reserved for variables that should only take single values OR
+        for spatial modulations that were created with `.var` in which case you can
+        pass in a list. This is the ONLY circumstance in which multiple values are allowed.
+        - You can:
+            - |_ ...assign(assignments).bloqade: select the bloqade local emulator backend
+            - |_ ...assign(assignments).braket: select braket local emulator or QuEra hardware
+            - |_ ...assign(assignments).device(specifier_string): select backend by specifying a string
+        - Assign multiple values to a single variable for a parameter sweep:
+            - |_ ...assign(assignments).batch_assign(assignments): 
+        - Parallelize the program register, duplicating the geometry and waveform sequence to take advantage of all available
+          space/qubits on the QPU:
+            - |_ ...assign(assignments).parallelize(cluster_spacing)
+        - Defer value assignment of certain variables to runtime:
+            - |_ ...assign(assignments).args([previously_defined_vars])
 
-        Args:
-            assignments (Dict[str, Union[Number]]):
-            The assignments, which should be a kwargs
-            where the key is the variable name and the
-            value is the value to assign to the variable.
-
-        Examples:
-            - Assign the value 0.0 to the variable "ival"
-            and 0.5 to the variable "span_time".
-
-            >>> reg = bloqade.start
-            ...       .add_position([(0,0),(1,1),(2,2),(3,3)])
-            >>> seq = reg.rydberg.detuning.uniform
-            ...       .linear(start="ival",stop=1,duration="span_time")
-            >>> seq = seq.assign(span_time = 0.5, ival = 0.0)
+        Usage Examples:
+        ```
+        # define geometry
+        >>> reg = bloqade.start
+        ...       .add_position([(0,0),(1,1),(2,2),(3,3)])
+        # define variables in program
+        >>> seq = reg.rydberg.detuning.uniform
+        ...       .linear(start="ival",stop=1,duration="span_time")
+        # assign values to variables
+        >>> seq = seq.assign(span_time = 0.5, ival = 0.0)
+        ```
 
         """
         from bloqade.builder.assign import Assign
@@ -44,6 +54,32 @@ class Assignable:
 
 class BatchAssignable:
     def batch_assign(self, **assignments: ParamType) -> "BatchAssign":
+        """
+
+        - Assign multiple values to a single variable to create a parameter sweep. 
+        - Bloqade automatically handles the multiple programs this would generate and treats it as 
+          object with unified results for easy post-processing. 
+        - NOTE: if you assign multiple values to multiple variables in your program, the values must 
+          be of the same length. Bloqade will NOT do a Cartesian product (e.g. if "var1" is assigned
+          [1,2,3] and "var2" is assigned [4,5,6] then the resulting programs will have assignments [1,4], [2,5], [3,6]).
+        - Next steps are:
+            - |_ ...batch_assign(assignments).bloqade: select the bloqade local emulator backend
+            - |_ ...batch_assign(assignments).braket: select braket local emulator or QuEra hardware
+            - |_ ...batch_assign(assignments).device(specifier_string): select backend by specifying a string
+        - Parallelize the program register, duplicating the geometry and waveform sequence to take advantage of all available
+          space/qubits on the QPU:
+            - |_ ...batch_assign(assignments).parallelize(cluster_spacing)
+        - Defer value assignment of certain variables to runtime:
+            - |_ ...batch_assign(assignments).args([previously_defined_vars])
+
+        Usage Example:
+        ```
+        >>> reg = start.add_position([(0,0), (0, "atom_distance")])
+        >>> prog = reg.rydberg.rabi.amplitude.uniform.constant("value", 5.0)
+        >>> var_assigned_prog = prog.batch_assign(value = [1.0, 2.0, 3.0], atom_distance = [1.0, 2.0, 3.0])
+        ```
+
+        """
         from bloqade.builder.assign import BatchAssign
 
         return BatchAssign(parent=self, **assignments)
@@ -52,22 +88,20 @@ class BatchAssignable:
 class Parallelizable:
     def parallelize(self, cluster_spacing: LiteralType) -> "Parallelize":
         """
-        Parallelize the current problem (register & sequnece) to fill entire FOV
-        with the given cluster spacing.
+        - Parallelize the current problem (register and sequence) by duplicating
+        the geometry to take advantage of all available space/qubits on hardware.
+        - The singular argument lets you specify how far apart the clusters should be in micrometers.
+        - Your next steps are: 
+            |_ `...parallelize(cluster_spacing).bloqade`: select the bloqade local emulator backend
+            |_ `...parallelize(cluster_spacing).braket`: select braket local emulator or QuEra hardware on the cloud
+            |_ `...parallelize(cluster_spacing).device(specifier_string)`: select backend by specifying a string
 
-        Args:
-            cluster_spacing (Real | Decimal):
-            the spacing between parallel clusters.
-
-        Examples:
-            - Parallelize the current problem with cluster spacing 7.2 um.
-
-            >>> prob = (
-                    bloqade.start.add_position([(0,0),(1,1),(2,2),(3,3)])
-                    .rydberg.detuning.uniform
-                    .linear(start=0,stop=1,duration=1)
-                    )
-            >>> prob = prob.parallelize(7.2)
+        Usage Example:
+        ```
+        >>> reg = start.add_position((0,0)).rydberg.rabi.uniform.amplitude.constant(1.0, 1.0)
+        # copy-paste the geometry and waveforms 
+        >>> parallelized_prog = reg.parallelize(24)
+        ```
 
         """
         from bloqade.builder.parallelize import Parallelize
