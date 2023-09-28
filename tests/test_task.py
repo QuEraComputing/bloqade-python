bb61cfe735f27e087b22cde4f05a3ed709e55efb
Phillip Weinberg <weinbe58@gmail.com>
2023-09-28 17:11:15 -0400

    adding test for warning.

--- tests/test_task.py
+++ tests/test_task.py
@@ -20,7 +20,33 @@ def test_batch_error(*args):
             Chain(5, 6.1)
             .rydberg.detuning.uniform.linear(-10, 10, 3.0)
             .quera.mock()
-            .run(100)
+            .run_async(100)
+        )
+
+    error_files = glob.glob("partial-batch-errors-*")
+    batch_files = glob.glob("partial-batch-future-*")
+
+    assert len(error_files) == 1
+    assert len(batch_files) == 1
+
+    for error_file, batch_file in zip(error_files, batch_files):
+        os.remove(error_file)
+        os.remove(batch_file)
+
+
+@patch("bloqade.ir.routine.quera.MockBackend")
+def test_batch_warn(*args):
+    backend = bloqade.ir.routine.quera.MockBackend()
+
+    backend.submit_task.side_effect = ValueError("some random error")
+    backend.dict.return_value = {"state_file": ".mock_state.txt"}
+
+    with pytest.warns():
+        (
+            Chain(5, 6.1)
+            .rydberg.detuning.uniform.linear(-10, 10, 3.0)
+            .quera.mock()
+            .run_async(100, ignore_submission_error=True)
         )
 
     error_files = glob.glob("partial-batch-errors-*")
