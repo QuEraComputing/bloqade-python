2f1a96a6bec2e1a0bd6d6a5fb253f898fd1780c5
Phillip Weinberg <weinbe58@gmail.com>
2023-10-06 13:40:37 -0400

    adding coverate to numba impl.

--- tests/test_sparse_operator.py
+++ tests/test_sparse_operator.py
@@ -2,6 +2,12 @@ from bloqade.emulate.sparse_operator import (
     SparseMatrixCSC,
     SparseMatrixCSR,
     IndexMapping,
+    _csc_matvec_impl,
+    _csr_matvec_impl,
+    _index_mapping_col_sliced,
+    _index_mapping_row_sliced,
+    _index_mapping_impl,
+    _index_mapping_row_col_sliced,
 )
 from scipy.sparse import random
 import numpy as np
@@ -23,6 +29,12 @@ def test_csr():
 
     assert np.allclose(result, expected_result)
 
+    result = v.copy()
+
+    _csr_matvec_impl.py_func(B.shape[1], B.data, B.indices, B.indptr, a, v, result)
+
+    assert np.allclose(result, expected_result)
+
 
 def test_csc():
     A = random(10, 10, density=0.5, format="csc")
@@ -38,6 +50,12 @@ def test_csc():
 
     assert np.allclose(result, expected_result)
 
+    result = v.copy()
+
+    _csc_matvec_impl.py_func(B.shape[1], B.data, B.indices, B.indptr, a, v, result)
+
+    assert np.allclose(result, expected_result)
+
 
 def test_index_mapping():
     B = IndexMapping(10, slice(0, None, 2), slice(1, None, 2))
@@ -50,6 +68,20 @@ def test_index_mapping():
 
     result = v.copy()
     B.matvec(v, out=result, scale=a)
+
+    print(result)
+    print(expected_result)
+    print("---------------")
+
+    assert np.allclose(result, expected_result)
+
+    result = v.copy()
+
+    _index_mapping_row_col_sliced.py_func(
+        a,
+        v[B.col_indices],
+        result[B.row_indices],
+    )
 
     print(result)
     print(expected_result)
@@ -74,6 +106,21 @@ def test_index_mapping_2():
 
     print(result)
     print(expected_result)
+    print("---------------")
+
+    assert np.allclose(result, expected_result)
+
+    result = v.copy()
+
+    _index_mapping_col_sliced.py_func(
+        B.row_indices,
+        a,
+        v[B.col_indices],
+        result,
+    )
+
+    print(result)
+    print(expected_result)
 
     assert np.allclose(result, expected_result)
 
@@ -84,6 +131,58 @@ def test_index_mapping_2():
 
     result = v.copy()
     B.matvec(v, out=result, scale=a)
+
+    print(result)
+    print(expected_result)
+    print("---------------")
+
+    assert np.allclose(result, expected_result)
+
+    result = v.copy()
+
+    _index_mapping_row_sliced.py_func(
+        B.col_indices,
+        a,
+        v,
+        result[B.row_indices],
+    )
+
+    print(result)
+    print(expected_result)
+
+    assert np.allclose(result, expected_result)
+
+
+def test_index_mapping_3():
+    input = np.random.permutation(np.arange(10))
+    output = np.random.permutation(np.arange(10))
+
+    B = IndexMapping(10, input, output)
+    A = B.tocsr().toarray()
+
+    v = np.random.normal(size=10)
+    a = 3
+
+    expected_result = a * A.dot(v) + v
+
+    result = v.copy()
+    B.matvec(v, out=result, scale=a)
+
+    print(result)
+    print(expected_result)
+    print("---------------")
+
+    assert np.allclose(result, expected_result)
+
+    result = v.copy()
+
+    _index_mapping_impl.py_func(
+        B.col_indices,
+        B.row_indices,
+        a,
+        v,
+        result,
+    )
 
     print(result)
     print(expected_result)
