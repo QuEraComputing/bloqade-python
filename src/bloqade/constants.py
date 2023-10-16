6c10741dbb1db5471064abcd9c941ac8dc6f29ec
Phillip Weinberg <weinbe58@gmail.com>
2023-10-16 09:32:10 -0400

    adding doc string for `RB_C6`

--- src/bloqade/constants.py
+++ src/bloqade/constants.py
@@ -1,5 +1,5 @@
 from math import pi
 
 RB_C6 = 2 * pi * 862690
-"""The C6 constant for the Rydberg Interaction of two Rubidium in units of:
+"""The C6 constant for the Rydberg Interaction of two Rubidium atoms in units of:
 rad μm^6/μs"""
