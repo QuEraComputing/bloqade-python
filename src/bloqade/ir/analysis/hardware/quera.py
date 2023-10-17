6096a964a3556dadef399afb76d849ecf27e691e
Phillip Weinberg <weinbe58@gmail.com>
2023-10-17 09:16:35 -0400

    fixing typo

--- src/bloqade/ir/analysis/hardware/quera.py
+++ src/bloqade/ir/analysis/hardware/quera.py
@@ -5,15 +5,13 @@ import bloqade.ir.control.sequence as sequence
 import bloqade.ir.control.pulse as pulse
 import bloqade.ir.control.field as field
 import bloqade.ir.control.waveform as waveform
-from builder.typing import LiteralType
-from beartype.typeing import Dict
-
+from bloqade.builder.typing import LiteralType
+from beartype.typing import Dict
 
 
 class AHSValidator(AnalogCircuitVisitor):
     def __init__(self, assignments: Dict[str, LiteralType] = {}):
         self.assignments = dict(assignments)
-
 
     def validate_detuning(self, detuning: field.Field):
         pass
@@ -23,6 +21,3 @@ class AHSValidator(AnalogCircuitVisitor):
 
     def validate_rabi_phase(self, rabi_freq: field.Field):
         pass
-
-
-    
