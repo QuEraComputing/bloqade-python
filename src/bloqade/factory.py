79d1f4d76b548670e3e2e90a5ee1f7d47a4ec0c0
John Long <johnzl@protonmail.ch>
2023-10-24 16:45:50 -0400

    fix formatting

--- src/bloqade/factory.py
+++ src/bloqade/factory.py
@@ -20,7 +20,7 @@ def get_capabilities() -> "QuEraCapabilities":
         Units of time, distance, and energy are microseconds (us),
         micrometers (um), and rad / us, respectively.
 
-        For a comprehensive list of capabilities, 
+        For a comprehensive list of capabilities,
         see the [Hardware Reference](../../hardware-reference/hardware-capabilities.md) page
     """
 
