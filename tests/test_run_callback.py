16c029d7b4b7a5d1ce6daaccb73ef1ecc4e8ea42
Phillip Weinberg <weinbe58@gmail.com>
2023-11-01 21:01:29 +0100

    adding tests for exception handles

--- tests/test_run_callback.py
+++ tests/test_run_callback.py
@@ -3,6 +3,7 @@ from bloqade.codegen.emulator_ir import EmulatorProgramCodeGen
 from bloqade.emulate.codegen.hamiltonian import RydbergHamiltonianCodeGen
 from bloqade.emulate.ir.state_vector import AnalogGate
 import numpy as np
+import pytest
 
 
 def callback(register, *_):
@@ -13,8 +14,8 @@ def test_run_callback():
     program = (
         start.add_position((0, 0))
         .add_position((0, 6.1))
-        .rydberg.detuning.uniform.constant(15, 1)
-        .rabi.amplitude.uniform.constant(15, 1)
+        .rydberg.detuning.uniform.constant(15, 0.1)
+        .rabi.amplitude.uniform.constant(15, 0.1)
     )
 
     bloqade_ir = program.parse_circuit()
@@ -22,7 +23,7 @@ def test_run_callback():
     hamiltonian = RydbergHamiltonianCodeGen().emit(emulatir_ir)
 
     state = hamiltonian.space.zero_state(np.complex128)
-    (expected_result,) = AnalogGate(hamiltonian).apply(state, atol=1e-14, rtol=1e-7)
+    (expected_result,) = AnalogGate(hamiltonian).apply(state)
 
     (result_single,) = program.bloqade.python().run_callback(callback)
     (result_multi,) = program.bloqade.python().run_callback(
@@ -33,5 +34,39 @@ def test_run_callback():
     np.testing.assert_equal(expected_result, result_multi)
 
 
+def callback_exception(*args):
+    raise ValueError
+
+
+def test_run_callback_exception():
+    program = (
+        start.add_position((0, 0))
+        .add_position((0, 6.1))
+        .rydberg.detuning.uniform.constant(15, 1)
+        .rabi.amplitude.uniform.constant(15, 1)
+    )
+
+    # program.bloqade.python().run_callback(callback_exception)
+
+    with pytest.raises(RuntimeError):
+        program.bloqade.python().run_callback(callback_exception)
+
+    with pytest.raises(RuntimeError):
+        program.bloqade.python().run_callback(
+            callback_exception, multiprocessing=True, num_workers=1
+        )
+
+    (result_single,) = program.bloqade.python().run_callback(
+        callback_exception, ignore_exceptions=True
+    )
+    (result_multi,) = program.bloqade.python().run_callback(
+        callback_exception, multiprocessing=True, num_workers=1, ignore_exceptions=True
+    )
+
+    assert isinstance(result_single, ValueError)
+    assert isinstance(result_multi, ValueError)
+
+
 if __name__ == "__main__":
     test_run_callback()
+    test_run_callback_exception()
