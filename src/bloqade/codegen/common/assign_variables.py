eaa7a7d3628caf4f038d370c93909e5f2a342fee
Phillip Weinberg <weinbe58@gmail.com>
2023-11-17 16:24:26 -0500

    fixing impl for assignment transformer.

--- src/bloqade/codegen/common/assign_variables.py
+++ src/bloqade/codegen/common/assign_variables.py
@@ -1,4 +1,5 @@
 import bloqade.ir.control.field as field
+import bloqade.ir.control.waveform as waveform
 import bloqade.ir.scalar as scalar
 from bloqade.builder.typing import LiteralType
 from bloqade.ir.visitor.base import BloqadeIRTransformer
@@ -26,6 +27,12 @@ class AssignBloqadeIR(BloqadeIRTransformer):
             return field.AssignedRunTimeVector(node.name, self.mapping[node.name])
 
         return node
+
+    def visit_waveform_Record(self, node: waveform.Record):
+        if node.var.name in self.mapping:
+            return self.visit(node.waveform)
+
+        return waveform.Record(self.visit(node.waveform), node.var)
 
     def visit_field_AssignedRunTimeVector(self, node: field.AssignedRunTimeVector):
         if node.name in self.mapping:
