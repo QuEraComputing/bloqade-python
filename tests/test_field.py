2a5c9e6387b26485959374b6cc9d5c6c3f77b39d
Phillip Weinberg <weinbe58@gmail.com>
2023-11-23 08:28:08 -0500

    bump test coverage

--- tests/test_field.py
+++ tests/test_field.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from bloqade.ir import (
     Field,
     Uniform,
@@ -6,6 +7,7 @@ from bloqade.ir import (
     Location,
     SpatialModulation,
     RunTimeVector,
+    AssignedRunTimeVector,
 )
 from bloqade.ir.control.field import Drive
 import pytest
@@ -14,8 +16,11 @@ from io import StringIO
 from IPython.lib.pretty import PrettyPrinter as PP
 
 import bloqade.ir.tree_print as trp
+from bloqade import tree_depth
 
 trp.color_enabled = False
+
+tree_depth(10)
 
 
 def test_location():
@@ -33,12 +38,13 @@ def test_spacmod_base():
         hash(x)
 
 
-def test_unform():
+def test_uniform():
     x = Uniform
 
     assert str(x) == "UniformModulation\n"
     assert x.print_node() == "UniformModulation"
     assert x.children() == []
+    assert x._get_data() == (["uni"], ["all"])
 
     mystdout = StringIO()
     p = PP(mystdout)
@@ -54,6 +60,7 @@ def test_runtime_vec():
     assert str(x) == "RunTimeVector: sss\n"
     assert x.print_node() == "RunTimeVector: sss"
     assert x.children() == []
+    assert x._get_data() == (["sss"], ["vec"])
 
     mystdout = StringIO()
     p = PP(mystdout)
@@ -61,6 +68,28 @@ def test_runtime_vec():
     x._repr_pretty_(p, 0)
 
     assert mystdout.getvalue() == "RunTimeVector: sss\n"
+
+
+def test_assigned_runtime_vec():
+    x = AssignedRunTimeVector("sss", [Decimal("1.0"), Decimal("2.0")])
+
+    x_str = str(x)
+
+    assert x_str == (
+        "AssignedRunTimeVector: sss\n" "├─ Literal: 1.0\n" "└─ Literal: 2.0"
+    )
+    assert x.print_node() == "AssignedRunTimeVector: sss"
+    assert x.children() == cast([Decimal("1.0"), Decimal("2.0")])
+    assert x._get_data() == (["sss"], ["vec"])
+
+    mystdout = StringIO()
+    p = PP(mystdout)
+
+    x._repr_pretty_(p, 0)
+
+    assert mystdout.getvalue() == (
+        "AssignedRunTimeVector: sss\n" "├─ Literal: 1.0\n" "⋮\n" "└─ Literal: 2.0⋮\n"
+    )
 
 
 def test_scal_loc():
@@ -71,6 +100,12 @@ def test_scal_loc():
 
     assert x.print_node() == "ScaledLocations"
     assert x.children() == {"Location(1)": cast(1.0), "Location(2)": cast(2.0)}
+    assert x._get_data() == (["loc[1]", "loc[2]"], ["1.0", "2.0"])
+
+    x[Location(0)] = 1.0
+
+    assert x[Location(0)] == cast(1.0)
+    assert x
 
     mystdout = StringIO()
     p = PP(mystdout)
@@ -82,8 +117,11 @@ def test_scal_loc():
         "├─ Location(1)\n"
         "│  ⇒ Literal: 1.0\n"
         "⋮\n"
-        "└─ Location(2)\n"
-        "   ⇒ Literal: 2.0⋮\n"
+        "├─ Location(2)\n"
+        "│  ⇒ Literal: 2.0\n"
+        "⋮\n"
+        "└─ Location(0)\n"
+        "   ⇒ Literal: 1.0⋮\n"
     )
 
 
@@ -95,10 +133,14 @@ def test_field_scaled_locations():
     f2 = Field({Loc: Linear(start=1.0, stop="x", duration=3.0)})
     f3 = Field({Loc2: Linear(start=1.0, stop="x", duration=3.0)})
     f4 = Field({Loc3: Linear(start="y", stop="x", duration=4.0)})
+    f5 = Field({})
 
     # add with non field
     with pytest.raises(ValueError):
         f1.add(Loc)
+
+    assert f5.duration == cast(0)
+    assert f1.duration == cast(3.0)
 
     mystdout = StringIO()
     p = PP(mystdout)
