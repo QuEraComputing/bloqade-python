48c0ba83106c8c2414020e3f4bf5e48d7c74ddd6
Phillip Weinberg <weinbe58@gmail.com>
2023-11-27 15:53:35 -0500

    updating flatten so that Append is pulled to top level of waveform AST.

--- src/bloqade/transform/common/flatten_sequence.py
+++ src/bloqade/transform/common/flatten_sequence.py
@@ -3,7 +3,14 @@ import bloqade.ir.control.pulse as pulse
 import bloqade.ir.control.field as field
 import bloqade.ir.control.waveform as waveform
 import bloqade.ir.scalar as scalar
+
+
 from bloqade.ir.visitor import BloqadeIRTransformer
+from bloqade.analysis.common.scan_variables import ScanVariables
+from bloqade.factory import piecewise_constant, piecewise_linear
+from bisect import bisect_left
+from decimal import Decimal
+from beartype.typing import Union
 
 
 class FillMissingWaveforms(BloqadeIRTransformer):
@@ -134,6 +141,67 @@ class FlattenBloqadeIR(BloqadeIRTransformer):
         self.spatial_modulations = spatial_modulations
         self.duration = None
 
+    def get_times(self, wf: Union[waveform.Append, waveform.Waveform]):
+        if isinstance(wf, waveform.Append):
+            times = []
+            time = Decimal("0")
+            for wf in wf.waveforms:
+                times.append(time)
+                time += wf.duration()
+            return times
+        else:
+            return [Decimal("0"), wf.duration()]
+
+    def move_slice_into_append(
+        self, wf: waveform.Append, start_time: Decimal, stop_time: Decimal
+    ):
+        if start_time == stop_time:
+            return waveform.Constant(0, 0)
+
+        times = self.get_times(wf)
+
+        start_index = bisect_left(times[1:], start_time)
+        stop_index = bisect_left(times[1:], stop_time)
+
+        if start_index == stop_index:
+            return wf.waveforms[start_index][start_time:stop_time]
+
+        if start_time == times[start_index + 1]:
+            wf_start = waveform.Constant(0, 0)
+        else:
+            start_time = start_time - times[start_index]
+
+            wf_start = wf.waveforms[start_index][start_time:]
+
+        if stop_time == times[stop_index + 1]:
+            wf_stop = waveform.Constant(0, 0)
+        else:
+            stop_time = stop_time - times[stop_index]
+
+            wf_stop = wf.waveforms[stop_index][:stop_time]
+
+        return waveform.Append(
+            [wf_start] + wf.waveforms[start_index + 1 : stop_index] + [wf_stop]
+        )
+
+    def move_add_inside_append(
+        self,
+        left: Union[waveform.Append, waveform.Waveform],
+        right: Union[waveform.Append, waveform.Waveform],
+    ):
+        times = sorted(list(set(self.get_times(left) + self.get_times(right))))
+
+        new_wf = None
+        for start, stop in zip(times[:-1], times[1:]):
+            next_wf = left[start:stop] + right[start:stop]
+
+            if new_wf is None:
+                new_wf = next_wf
+            else:
+                new_wf = new_wf.append(next_wf)
+
+        return new_wf
+
     #######################
     # Visitor definitions #
     #######################
@@ -179,7 +247,7 @@ class FlattenBloqadeIR(BloqadeIRTransformer):
 
             fields[fn] = field.Field(drives)
 
-        return pulse.Pulse(fields)
+        return self.visit(pulse.Pulse(fields))
 
     def visit_pulse_Append(self, node: pulse.Append) -> pulse.Pulse:
         pulses = list(map(self.visit, node.pulses))
@@ -191,7 +259,103 @@ class FlattenBloqadeIR(BloqadeIRTransformer):
                     curr_wf = fields[fn].drives[sm]
                     fields[fn].drives[sm] = curr_wf.append(wf)
 
-        return pulse.Pulse(fields)
+        return self.visit(pulse.Pulse(fields))
 
     def visit_pulse_NamedPulse(self, node: pulse.NamedPulse) -> pulse.Pulse:
         return self.visit(node)
+
+    def visit_waveform_Scale(self, node: waveform.Scale) -> waveform.Waveform:
+        wf = self.visit(node.waveform)
+
+        if isinstance(wf, waveform.Append):
+            wf = node.scalar * wf.waveforms[0]
+            for next_wf in wf.waveforms[1:]:
+                wf = wf.append(node.scalar * next_wf)
+
+            return wf
+        else:
+            return node.scalar * wf
+
+    def visit_waveform_Negative(self, node: waveform.Negative) -> waveform.Waveform:
+        wf = self.visit(node.waveform)
+
+        if isinstance(wf, waveform.Append):
+            wf = -wf.waveforms[0]
+            for next_wf in wf.waveforms[1:]:
+                wf = wf.append(-next_wf)
+
+            return wf
+        else:
+            return -wf
+
+    def visit_waveform_Sample(self, node: waveform.Sample) -> waveform.Waveform:
+        wf = self.visit(node.waveform)
+
+        variable_scan_wf = ScanVariables().emit(wf)
+        variable_scan_dt = ScanVariables().emit(node.dt)
+
+        scalar_vars = variable_scan_wf.scalar_vars.union(variable_scan_dt.scalar_vars)
+
+        if scalar_vars:
+            return waveform.Sample(wf, node.interpolation, node.dt)
+
+        times, values = node.samples()
+
+        if node.interpolation is waveform.Interpolation.Linear:
+            return piecewise_linear(times, values)
+        elif node.interpolation is waveform.Interpolation.Constant:
+            return piecewise_constant(times, values)
+
+    def visit_waveform_Slice(self, node: waveform.Slice) -> waveform.Waveform:
+        wf = self.visit(node.waveform)
+
+        variable_scan_wf = ScanVariables().emit(wf)
+        variable_scan_interval = ScanVariables().emit(node.interval)
+
+        scalar_vars = variable_scan_wf.scalar_vars.union(
+            variable_scan_interval.scalar_vars
+        )
+
+        # if waveform still has unassigned variables, we can't move the slice inside
+        # the append so we just return the node
+        if scalar_vars:
+            return wf[node.interval.start : node.interval.stop]
+
+        wf = self.visit(node.waveform)
+
+        # if waveform is an append, we don't need to move the slice inside
+        if not isinstance(wf, waveform.Append):
+            return waveform.Slice(wf, node.interval)
+
+        start = node.start()
+        stop = node.stop()
+
+        # no slice needed
+        if start == Decimal("0") and stop == wf.duration():
+            return wf
+
+        # move slice inside append
+        return self.move_slice_into_append(wf, start, stop)
+
+    def visit_waveform_Add(self, node: waveform.Add) -> waveform.Add:
+        # visit left and right side of add
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+
+        variable_scan_left = ScanVariables().emit(left)
+        variable_scan_right = ScanVariables().emit(right)
+
+        scalar_vars = variable_scan_left.scalar_vars.union(
+            variable_scan_right.scalar_vars
+        )
+
+        # if waveforms still have unassigned variables, we can't move the add inside
+        # the append so we just return the visited node
+        if scalar_vars:
+            return left + right
+
+        # if either side is an append, we need to move the add inside the append
+        if isinstance(left, waveform.Append) or isinstance(right, waveform.Append):
+            return self.move_add_inside_append(left, right)
+
+        return left + right
