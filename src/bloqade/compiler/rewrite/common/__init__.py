aa4e92d6e96e07e3ca1994c6a99806bace810269
Phillip Weinberg <weinbe58@gmail.com>
2023-12-11 16:37:25 -0500

    fixing typo

--- src/bloqade/compiler/rewrite/common/__init__.py
+++ src/bloqade/compiler/rewrite/common/__init__.py
@@ -2,12 +2,12 @@ from .add_padding import AddPadding
 from .assign_to_literal import AssignToLiteral
 from .assign_variables import AssignBloqadeIR
 from .canonicalize import Canonicalizer
-from .flatten import Flatten
+from .flatten import FlattenCircuit
 
 __all__ = [
     "AddPadding",
     "AssignToLiteral",
     "AssignBloqadeIR",
     "Canonicalizer",
-    "Flatten",
+    "FlattenCircuit",
 ]
