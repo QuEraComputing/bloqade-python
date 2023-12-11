e42a97c435e7042b3f41073157a5e3836e389b96
Phillip Weinberg <weinbe58@gmail.com>
2023-12-11 11:27:55 -0500

    putting imports inside pass functions.

--- src/bloqade/compiler/hardware/passes.py
+++ src/bloqade/compiler/hardware/passes.py
@@ -1,58 +1,29 @@
-# TODO: import common passes from bloqade.analysis.common
-from bloqade.analysis.common.assignment_scan import AssignmentScan
-from bloqade.analysis.common.scan_channels import ScanChannels
-from bloqade.analysis.common.scan_variables import ScanVariables
-from bloqade.analysis.hardware.channels import ValidateChannels
-from bloqade.analysis.hardware.piecewise_constant import (
-    ValidatePiecewiseConstantChannel,
-)
-from bloqade.analysis.hardware.piecewise_linear import (
-    ValidatePiecewiseLinearChannel,
-)
 from bloqade.builder.typing import ParamType
 
-# TODO: import hardware_v2 passes from bloqade.codegen.hardware_v2
-from bloqade.codegen.hardware_v2.lattice import GenerateLattice
-from bloqade.codegen.hardware_v2.lattice_site_coefficients import (
-    GenerateLatticeSiteCoefficients,
-)
-from bloqade.codegen.hardware_v2.piecewise_linear import (
-    GeneratePiecewiseLinearChannel,
-)
-from bloqade.codegen.hardware_v2.piecewise_constant import (
-    GeneratePiecewiseConstantChannel,
-)
 from bloqade.compiler.hardware.components import AHSComponents
 from bloqade.ir import analog_circuit
 from bloqade.ir.control import pulse, sequence, field
 
-# TODO: import common passes from bloqade.rewrite.common
-from bloqade.rewrite.common.assign_variables import AssignBloqadeIR
-from bloqade.rewrite.common.add_padding import AddPadding
-from bloqade.rewrite.common.assign_to_literal import AssignToLiteral
-from bloqade.rewrite.common.canonicalize import Canonicalizer
 from bloqade.submission.ir.braket import BraketTaskSpecification
 
 from bloqade.submission.ir.capabilities import QuEraCapabilities
 from bloqade.submission.ir.task_specification import QuEraTaskSpecification
-
-from bloqade.compiler.hardware.units import (
-    convert_time_units,
-    convert_energy_units,
-    convert_coordinate_units,
-)
 
 from beartype.typing import Dict, Optional
 
 
 def analyze_channels(circuit: analog_circuit.AnalogCircuit) -> Dict:
     """1. Scan channels"""
+    from bloqade.analysis.hardware.channels import ValidateChannels
+    from bloqade.analysis.common.scan_channels import ScanChannels
+
     ValidateChannels().scan(circuit)
     level_couplings = ScanChannels().scan(circuit)
 
     # add missing channels
     fields = level_couplings[sequence.rydberg]
-
+    # detuning, phase and amplitude are required
+    # to have at least a uniform field
     updated_fields = {
         field_name: fields.get(field_name, {field.Uniform})
         for field_name in [pulse.detuning, pulse.rabi.amplitude, pulse.rabi.phase]
@@ -64,6 +35,8 @@ def analyze_channels(circuit: analog_circuit.AnalogCircuit) -> Dict:
 def add_padding(
     circuit: analog_circuit.AnalogCircuit, level_couplings: Dict
 ) -> analog_circuit.AnalogCircuit:
+    from bloqade.rewrite.common.add_padding import AddPadding
+
     """2. Insert zero waveform in the explicit time intervals missing a waveform"""
     return AddPadding(level_couplings=level_couplings).visit(circuit)
 
@@ -72,6 +45,10 @@ def assign_circuit(
     circuit: analog_circuit.AnalogCircuit, assignments: Dict[str, ParamType]
 ) -> analog_circuit.AnalogCircuit:
     """3. Assign variables, validate here"""
+
+    from bloqade.analysis.common.assignment_scan import AssignmentScan
+    from bloqade.analysis.common.scan_variables import ScanVariables
+    from bloqade.rewrite.common.assign_variables import AssignBloqadeIR
 
     final_assignments = AssignmentScan(assignments).emit(circuit)
 
@@ -93,6 +70,13 @@ def validate_waveforms(
     circuit: analog_circuit.AnalogCircuit, level_couplings: Dict
 ) -> None:
     """4. validate piecewise linear and piecewise constant pieces of pulses"""
+    from bloqade.analysis.hardware.piecewise_constant import (
+        ValidatePiecewiseConstantChannel,
+    )
+    from bloqade.analysis.hardware.piecewise_linear import (
+        ValidatePiecewiseLinearChannel,
+    )
+
     channel_iter = (
         (level_coupling, field_name, sm)
         for level_coupling, fields in level_couplings.items()
@@ -110,6 +94,9 @@ def to_literal_and_canonicalize(
     circuit: analog_circuit.AnalogCircuit,
 ) -> analog_circuit.AnalogCircuit:
     """5. convert to literals and canonicalize"""
+    from bloqade.rewrite.common.assign_to_literal import AssignToLiteral
+    from bloqade.rewrite.common.canonicalize import Canonicalizer
+
     return Canonicalizer().visit(AssignToLiteral().visit(circuit))
 
 
@@ -119,6 +106,16 @@ def generate_ahs_code(
     circuit: analog_circuit.AnalogCircuit,
 ) -> AHSComponents:
     """6. generate ahs code"""
+    from bloqade.codegen.hardware_v2.lattice import GenerateLattice
+    from bloqade.codegen.hardware_v2.lattice_site_coefficients import (
+        GenerateLatticeSiteCoefficients,
+    )
+    from bloqade.codegen.hardware_v2.piecewise_linear import (
+        GeneratePiecewiseLinearChannel,
+    )
+    from bloqade.codegen.hardware_v2.piecewise_constant import (
+        GeneratePiecewiseConstantChannel,
+    )
 
     ahs_lattice_data = GenerateLattice(capabilities).emit(circuit)
 
@@ -164,6 +161,11 @@ def generate_quera_ir(
     ahs_components: AHSComponents, shots: int
 ) -> QuEraTaskSpecification:
     import bloqade.submission.ir.task_specification as task_spec
+    from bloqade.compiler.hardware.units import (
+        convert_time_units,
+        convert_energy_units,
+        convert_coordinate_units,
+    )
 
     lattice = task_spec.Lattice(
         sites=list(
@@ -235,7 +237,11 @@ def generate_braket_ir(
     ahs_components: AHSComponents, shots: int
 ) -> BraketTaskSpecification:
     import braket.ir.ahs as ahs
-    from bloqade.submission.ir.braket import BraketTaskSpecification
+    from bloqade.compiler.hardware.units import (
+        convert_time_units,
+        convert_energy_units,
+        convert_coordinate_units,
+    )
 
     ahs_register = ahs.AtomArrangement(
         sites=list(map(convert_coordinate_units, ahs_components.lattice_data.sites)),
