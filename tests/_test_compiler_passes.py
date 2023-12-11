80323b0dbecf73f66bf6271d64ec70457445bd9d
Phillip Weinberg <weinbe58@gmail.com>
2023-12-11 10:19:59 -0500

    updating compiler passes

--- tests/_test_compiler_passes.py
+++ tests/_test_compiler_passes.py
@@ -10,7 +10,7 @@ from bloqade.analysis.hardware.piecewise_constant import (
 from bloqade.analysis.hardware.piecewise_linear import (
     ValidatePiecewiseLinearChannel,
 )
-from bloqade.codegen.hardware_v2.lattice import GenerateLattice
+from bloqade.codegen.hardware_v2.lattice import GenerateLattice, AHSLatticeData
 from bloqade.codegen.hardware_v2.lattice_site_coefficients import (
     GenerateLatticeSiteCoefficients,
 )
@@ -88,9 +88,10 @@ def assign_program(circuit, assignments) -> sequence.SequenceExpr:
 def validate_waveforms(level_couplings, circuit) -> None:
     """4. validate piecewise linear and piecewise constant pieces of pulses"""
     channel_iter = (
-        (level_coupling, field_name, spatial_modulations)
+        (level_coupling, field_name, sm)
         for level_coupling, fields in level_couplings.items()
         for field_name, spatial_modulations in fields.items()
+        for sm in spatial_modulations
     )
     for channel in channel_iter:
         if channel[1] in [pulse.detuning, pulse.rabi.amplitude]:
@@ -106,13 +107,6 @@ def to_literal_and_canonicalize(circuit):
 
 
 @dataclass
-class AHSLatticeData:
-    sites: List[Tuple[Decimal, Decimal]]
-    filling: List[int]
-    parallel_decoder: Optional[ParallelDecoder]
-
-
-@dataclass
 class AHSComponents:
     lattice_data: AHSLatticeData
     global_detuning: PiecewiseLinear
@@ -121,15 +115,163 @@ class AHSComponents:
     local_detuning: Optional[PiecewiseLinear]
     lattice_site_coefficients: Optional[List[Decimal]]
 
+    @staticmethod
+    def convert_time_units(time: Decimal) -> Decimal:
+        return time * Decimal("1e-6")
+
+    @staticmethod
+    def convert_energy_units(energy: Decimal) -> Decimal:
+        return energy * Decimal("1e6")
+
+    @staticmethod
+    def convert_coordinate_units(
+        length: Tuple[Decimal, Decimal]
+    ) -> Tuple[Decimal, Decimal]:
+        return (length[0] * Decimal("1e-6"), length[1] * Decimal("1e-6"))
+
     def generate_quera_ir(
         self, shots: int
     ) -> Tuple[QuEraTaskSpecification, Optional[ParallelDecoder]]:
-        pass
+        import bloqade.submission.ir.task_specification as task_spec
+
+        lattice = task_spec.Lattice(
+            sites=list(map(self.convert_coordinate_units, self.lattice_data.sites)),
+            filling=self.lattice_data.filling,
+        )
+
+        global_detuning = task_spec.GlobalField(
+            times=list(map(self.convert_time_units, self.global_detuning.times)),
+            values=list(map(self.convert_energy_units, self.global_detuning.values)),
+        )
+
+        local_detuning = None
+
+        if self.lattice_site_coefficients is not None:
+            local_detuning = task_spec.LocalField(
+                times=list(map(self.convert_time_units, self.local_detuning.times)),
+                values=list(map(self.convert_energy_units, self.local_detuning.values)),
+                lattice_site_coefficients=self.lattice_site_coefficients,
+            )
+
+        rabi_frequency_amplitude_field = task_spec.GlobalField(
+            times=list(map(self.convert_time_units, self.global_amplitude.times)),
+            values=list(map(self.convert_energy_units, self.global_amplitude.values)),
+        )
+
+        rabi_frequency_phase_field = task_spec.GlobalField(
+            times=list(map(self.convert_time_units, self.global_phase.times)),
+            values=self.global_phase.values,
+        )
+
+        detuning = task_spec.Detuning(
+            global_=global_detuning,
+            local=local_detuning,
+        )
+
+        rabi_frequency_amplitude = task_spec.RabiFrequencyAmplitude(
+            global_=rabi_frequency_amplitude_field,
+        )
+
+        rabi_frequency_phase = task_spec.RabiFrequencyPhase(
+            global_=rabi_frequency_phase_field,
+        )
+
+        rydberg = task_spec.RydbergHamiltonian(
+            rabi_frequency_amplitude=rabi_frequency_amplitude,
+            rabi_frequency_phase=rabi_frequency_phase,
+            detuning=detuning,
+        )
+
+        effective_hamiltonian = task_spec.EffectiveHamiltonian(
+            rydberg=rydberg,
+        )
+
+        return (
+            task_spec.QuEraTaskSpecification(
+                nshots=shots,
+                lattice=lattice,
+                effective_hamiltonian=effective_hamiltonian,
+            ),
+            self.lattice_data.parallel_decoder,
+        )
 
     def generate_braket_ir(
         self, shots: int
     ) -> Tuple[BraketTaskSpecification, Optional[ParallelDecoder]]:
-        pass
+        import braket.ir.ahs as ahs
+        from bloqade.submission.ir.braket import BraketTaskSpecification
+
+        ahs_register = ahs.AtomArrangement(
+            sites=list(map(self.convert_coordinate_units, self.lattice_data.sites)),
+            filling=self.lattice_data.filling,
+        )
+
+        global_detuning_time_series = ahs.TimeSeries(
+            times=list(map(self.convert_time_units, self.global_detuning.times)),
+            values=list(map(self.convert_energy_units, self.global_detuning.values)),
+        )
+
+        local_detuning_time_series = None
+        if self.lattice_site_coefficients is not None:
+            local_detuning_time_series = ahs.TimeSeries(
+                times=list(map(self.convert_time_units, self.local_detuning.times)),
+                values=list(map(self.convert_energy_units, self.local_detuning.values)),
+            )
+
+        amplitude_time_series = ahs.TimeSeries(
+            times=list(map(self.convert_time_units, self.global_amplitude.times)),
+            values=list(map(self.convert_energy_units, self.global_amplitude.values)),
+        )
+
+        phase_time_series = ahs.TimeSeries(
+            times=list(map(self.convert_time_units, self.global_phase.times)),
+            values=self.global_phase.values,
+        )
+
+        detuning = ahs.PhysicalField(
+            time_series=global_detuning_time_series,
+            pattern="uniform",
+        )
+
+        amplitude = ahs.PhysicalField(
+            time_series=amplitude_time_series,
+            pattern="uniform",
+        )
+
+        phase = ahs.PhysicalField(
+            time_series=phase_time_series,
+            pattern="uniform",
+        )
+
+        local_detuning = None
+        if self.lattice_site_coefficients is not None:
+            local_detuning = ahs.PhysicalField(
+                time_series=local_detuning_time_series,
+                pattern=self.lattice_site_coefficients,
+            )
+
+        driving_field = ahs.DrivingField(
+            detuning=detuning,
+            amplitude=amplitude,
+            phase=phase,
+        )
+
+        shiftingFields = []
+        if self.lattice_site_coefficients is not None:
+            shiftingFields = [ahs.ShiftingField(magnitude=local_detuning)]
+
+        program = ahs.Program(
+            setup=ahs.Setup(ahs_register=ahs_register),
+            hamiltonian=ahs.Hamiltonian(
+                drivingFields=[driving_field],
+                shiftingFields=shiftingFields,
+            ),
+        )
+
+        return (
+            BraketTaskSpecification(nshots=shots, program=program),
+            self.lattice_data.parallel_decoder,
+        )
 
 
 def generate_ahs_code(capabilities, level_couplings, final_circuit) -> AHSComponents:
@@ -152,9 +294,7 @@ def generate_ahs_code(capabilities, level_couplings, final_circuit) -> AHSCompon
     local_detuning = None
     lattice_site_coefficients = None
 
-    extra_sm = level_couplings[sequence.rydberg][pulse.detuning].keys() - {
-        field.Uniform
-    }
+    extra_sm = level_couplings[sequence.rydberg][pulse.detuning] - {field.Uniform}
 
     if extra_sm:
         (sm,) = extra_sm
@@ -202,3 +342,4 @@ ahs_code = generate_ahs_code(capabilities, level_couplings, circuit)
 # 7. specialize to QuEra/Braket IR
 quera_ir, parallel_decoder = ahs_code.generate_quera_ir(shots=100)
 braket_ir, parallel_decoder = ahs_code.generate_braket_ir(shots=100)
+print(quera_ir.effective_hamiltonian.rydberg.rabi_frequency_amplitude.global_)
