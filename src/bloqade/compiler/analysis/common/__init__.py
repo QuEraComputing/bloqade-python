7e324a6497b65bb4146c4485b2e7152deab8bd62
Phillip Weinberg <weinbe58@gmail.com>
2023-12-27 09:49:01 -0500

    removing unnecessary namespace declaration.

--- src/bloqade/compiler/analysis/common/__init__.py
+++ src/bloqade/compiler/analysis/common/__init__.py
@@ -1,8 +1,3 @@
-try:
-    __import__("pkg_resources").declare_namespace(__name__)
-except ImportError:
-    __path__ = __import__("pkgutil").extend_path(__path__, __name__)
-
 from .assignment_scan import AssignmentScan
 from .check_slices import CheckSlices
 from .is_constant import IsConstant
