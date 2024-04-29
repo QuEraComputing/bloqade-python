4be8f5891cdee4ddfb4477f73159016adfad162d
John Long <johnzl@protonmail.ch>
2024-04-29 16:05:46 -0400

    Update typing docstrings

--- src/bloqade/builder/typing.py
+++ src/bloqade/builder/typing.py
@@ -5,5 +5,17 @@ from beartype.typing import List, Union
 
 
 ScalarType = Union[Real, Decimal, str, Scalar]
+""" 
+A type union representing Python-native types (`Real`, `Decimal`, and `str`) and Bloqade's own [`scalar`][bloqade.ir.scalar.Scalar] type
+used to represent and manipulate variables within programs.
+"""
+
 LiteralType = Union[Real, Decimal]
+""" 
+A type union representing the `Real` and `Decimal` types used in certain Bloqade methods.
+"""
+
 ParamType = Union[LiteralType, List[LiteralType]]
+""" 
+A type union representing the [`LiteralType`][bloqade.builder.typing.LiteralType] and `List[LiteralType]` types used in certain Bloqade methods.
+"""
