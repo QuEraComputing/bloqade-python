d7e9b8fbdc5bc1748198c90562651937d5cdd683
John Long <johnzl@protonmail.ch>
2024-05-08 12:01:14 -0400

    remove unused imports

--- tests/test_hamiltonian_codegen.py
+++ tests/test_hamiltonian_codegen.py
@@ -1,8 +1,6 @@
 from decimal import Decimal
 from itertools import combinations
 from bloqade.atom_arrangement import Chain
-from bloqade.compiler.codegen.python.emulator_ir import EmulatorProgramCodeGen
-from bloqade.emulate.codegen.hamiltonian import CompileCache, RydbergHamiltonianCodeGen
 from functools import reduce
 import numpy as np
 import pytest
