9be86541fcb8e4135d17a7f477abf84d6503930c
John Long <johnzl@protonmail.ch>
2024-05-29 15:54:00 -0400

    Add experimental capabilities to get_capabilities

--- src/bloqade/submission/capabilities.py
+++ src/bloqade/submission/capabilities.py
@@ -4,8 +4,11 @@ from bloqade.submission.ir.capabilities import QuEraCapabilities
 
 
 # TODO: Create unit converter for capabilities
-def get_capabilities() -> QuEraCapabilities:
+def get_capabilities(use_experimental=False) -> QuEraCapabilities:
     base_path = os.path.dirname(__file__)
-    full_path = os.path.join(base_path, "config", "capabilities.json")
+    if use_experimental:
+        full_path = os.path.join(base_path, "config", "experimental_capabilities.json")
+    else:
+        full_path = os.path.join(base_path, "config", "capabilities.json")
     with open(full_path, "r") as io:
         return QuEraCapabilities(**json.load(io))
