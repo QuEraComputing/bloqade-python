197dea16cb6813f6c857548f24562c5bc7523327
Jason Han <98669315+jasonhan3@users.noreply.github.com>
2026-08-05 09:28:18 -0400

    fix: removed polyfill js (#1109)
    * fix: removed polyfill js
    
    * fix: fixed CI issues

--- src/bloqade/analog/visualization/__init__.py
+++ src/bloqade/analog/visualization/__init__.py
@@ -1,66 +1,31 @@
-Use_bokeh = True
+from .display import (
+    figure_ir,
+    display_ir,
+    report_figure,
+    builder_figure,
+    display_report,
+    display_builder,
+    display_task_ir,
+)
+from .ir_visualize import get_ir_figure, get_field_figure, get_pulse_figure
+from .task_visualize import get_task_ir_figure
+from .atom_arrangement_visualize import (
+    get_atom_arrangement_figure,
+    assemble_atom_arrangement_panel,
+)
 
-if Use_bokeh:
-    from typing import List
-
-    from .display import (
-        figure_ir,
-        display_ir,
-        report_figure,
-        builder_figure,
-        display_report,
-        display_builder,
-        display_task_ir,
-    )
-    from .ir_visualize import get_ir_figure, get_field_figure, get_pulse_figure
-    from .task_visualize import get_task_ir_figure
-    from .atom_arrangement_visualize import (
-        get_atom_arrangement_figure,
-        assemble_atom_arrangement_panel,
-    )
-else:
-    # display
-    def display_ir(obj, assignemnts):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def display_report(report):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def display_task_ir(task_ir):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def display_builder(builder, batch_id):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    # visualization
-    def get_task_ir_figure(task_ir, **fig_kwargs):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    # atom arrangement
-    def get_atom_arrangement_figure(
-        atom_arng_ir, colors=(), fig_kwargs=None, **assignments
-    ):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def assemble_atom_arrangement_panel(atom_arrangement_plots, keys: List[str]):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    # ir
-    def get_ir_figure(ir, **assignments):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def get_field_figure(ir_field, title, indicator, **assignments):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def get_pulse_figure(ir_pulse, title: str = None, **assginments):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    # figure:
-    def figure_ir(obj, assignemnts):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def builder_figure(builder, batch_id):
-        raise Warning("Bokeh not installed", UserWarning)
-
-    def report_figure(report):
-        raise Warning("Bokeh not installed", UserWarning)
+__all__ = [
+    "assemble_atom_arrangement_panel",
+    "builder_figure",
+    "display_builder",
+    "display_ir",
+    "display_report",
+    "display_task_ir",
+    "figure_ir",
+    "get_atom_arrangement_figure",
+    "get_field_figure",
+    "get_ir_figure",
+    "get_pulse_figure",
+    "get_task_ir_figure",
+    "report_figure",
+]
